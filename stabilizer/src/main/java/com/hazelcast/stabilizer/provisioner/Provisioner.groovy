e862a5aea4db5dce2c481bd12f64547ac03506ef
Peter Veentjer <alarmnummer@gmail.com>
2014-04-29 21:20:07 +0300

    Stuff

--- stabilizer/src/main/java/com/hazelcast/stabilizer/provisioner/Provisioner.groovy
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/provisioner/Provisioner.groovy
@@ -275,7 +275,8 @@ public class Provisioner {
         overrides.setProperty(POLL_INITIAL_PERIOD, config.CLOUD_POLL_INITIAL_PERIOD);
         overrides.setProperty(POLL_MAX_PERIOD, config.CLOUD_POLL_MAX_PERIOD);
 
-        String credentials = (String)config.get("CLOUD_CREDENTIALS");
+        String credentials = (String)config.CLOUD_CREDENTIALS;
+        println credentials.class
         File file = new File(credentials);
         if(file.exists()){
             credentials = Utils.fileAsText(file);
