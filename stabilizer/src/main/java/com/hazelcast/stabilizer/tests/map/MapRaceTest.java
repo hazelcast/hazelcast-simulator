a5eea867efa1f54d62c494c9306bd9df9f778357
Peter Veentjer <alarmnummer@gmail.com>
2014-04-30 11:30:23 +0300

    Stuff

---
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/tests/map/MapRaceTest.java
@@ -0,0 +1,138 @@
+package com.hazelcast.stabilizer.tests.map;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
+import com.hazelcast.stabilizer.performance.OperationsPerSecond;
+import com.hazelcast.stabilizer.performance.Performance;
+import com.hazelcast.stabilizer.tests.AbstractTest;
+import com.hazelcast.stabilizer.tests.TestRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MapRaceTest extends AbstractTest {
+
+    private final static ILogger log = Logger.getLogger(MapRaceTest.class);
+
+    private IMap<Integer, Long> map;
+    private final AtomicLong operations = new AtomicLong();
+    private IMap<String, Map<Integer, Long>> resultsPerWorker;
+
+    //properties
+    public int threadCount = 10;
+    public int keyCount = 1000;
+    public int logFrequency = 10000;
+    public int performanceUpdateFrequency = 10000;
+    public String basename = "map";
+
+    @Override
+    public void localSetup() throws Exception {
+        HazelcastInstance targetInstance = getTargetInstance();
+
+        map = targetInstance.getMap(basename + "-" + getTestId());
+
+        for (int k = 0; k < threadCount; k++) {
+            spawn(new Worker());
+        }
+
+        resultsPerWorker = targetInstance.getMap("ResultMap" + getTestId());
+    }
+
+    @Override
+    public void globalSetup() throws Exception {
+        super.globalSetup();
+
+        for (int k = 0; k < keyCount; k++) {
+            map.put(k, 0l);
+        }
+    }
+
+    @Override
+    public void globalTearDown() throws Exception {
+        map.destroy();
+        resultsPerWorker.destroy();
+    }
+
+    @Override
+    public void globalVerify() throws Exception {
+        long[] amount = new long[keyCount];
+
+        for (Map<Integer, Long> map : resultsPerWorker.values()) {
+            for (Map.Entry<Integer, Long> entry : map.entrySet()) {
+                amount[entry.getKey()] += entry.getValue();
+            }
+        }
+
+        int failures = 0;
+        for (int k = 0; k < keyCount; k++) {
+            long expected = amount[k];
+            long found = map.get(k);
+            if (expected != found) {
+                failures++;
+            }
+        }
+
+        if (failures > 0) {
+            throw new IllegalStateException("Failures found:" + failures);
+        }
+    }
+
+    @Override
+    public Performance calcPerformance() {
+        OperationsPerSecond performance = new OperationsPerSecond();
+        performance.setStartMs(getStartTimeMs());
+        performance.setEndMs(getCurrentTimeMs());
+        performance.setOperations(operations.get());
+        return performance;
+    }
+
+    private class Worker implements Runnable {
+        private final Random random = new Random();
+        private final Map<Integer, Long> result = new HashMap<Integer, Long>();
+
+        @Override
+        public void run() {
+            for (int k = 0; k < keyCount; k++) {
+                result.put(k, 0L);
+            }
+
+            long iteration = 0;
+            while (!stopped()) {
+                Integer key = random.nextInt(keyCount);
+                long increment = random.nextInt(100);
+
+                Long current = map.get(key);
+                Long update = current + increment;
+                map.put(key, update);
+
+                increment(key, increment);
+
+                if (iteration % logFrequency == 0) {
+                    log.info(Thread.currentThread().getName() + " At iteration: " + iteration);
+                }
+
+                if (iteration % performanceUpdateFrequency == 0) {
+                    operations.addAndGet(performanceUpdateFrequency);
+                }
+
+                iteration++;
+            }
+
+            resultsPerWorker.put(UUID.randomUUID().toString(), result);
+        }
+
+        private void increment(int key, long increment) {
+            result.put(key, result.get(key) + increment);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        MapRaceTest test = new MapRaceTest();
+        new TestRunner().run(test, 20);
+    }
+}
