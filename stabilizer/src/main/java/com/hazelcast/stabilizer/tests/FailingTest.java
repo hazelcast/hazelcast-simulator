2ea852bcf951656307dae1faef2ff535a1d0d99f
Peter Veentjer <alarmnummer@gmail.com>
2014-05-02 07:47:01 +0300

    oome test improvement

--- stabilizer/src/main/java/com/hazelcast/stabilizer/tests/FailingTest.java
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/tests/FailingTest.java
@@ -21,9 +21,14 @@ public class FailingTest extends AbstractTest {
             if (failure.equals("Exception")) {
                 ExceptionReporter.report(new RuntimeException("Wanted exception"));
             } else if (failure.equals("OOME")) {
+
                 List list = new LinkedList();
                 for (; ; ) {
-                    list.add(new byte[100 * 1000 * 1000]);
+                    try {
+                        list.add(new byte[100 * 1000 * 1000]);
+                    } catch (OutOfMemoryError error) {
+
+                    }
                 }
             } else if (failure.equals("Exit")) {
                 System.exit(1);
