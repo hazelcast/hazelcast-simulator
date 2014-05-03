e59a326b6726d9d32ed3d0b870bdcb8c2da6924c
Peter Veentjer <alarmnummer@gmail.com>
2014-05-03 22:30:19 +0300

    Added support for ibm 8

---
+++ stabilizer/conf/jdk-ibm-8.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+function prepend {
+    echo $1|cat - $2 > /tmp/out && mv /tmp/out $2
+}
+
+cd ~
+wget http://ec2-54-87-52-100.compute-1.amazonaws.com/ibm-java-x86_64-sdk-ea-8.0-0.03.bin
+chmod +x ibm-java-x86_64-sdk-ea-8.0-0.03.bin
+sudo ./ibm-java-x86_64-sdk-ea-8.0-0.03.bin -i silent
+
+prepend 'export PATH=$JAVA_HOME/bin:$PATH'  ~/.bashrc
+prepend "export JAVA_HOME=/opt/ibm/java-x86_64-80/"  ~/.bashrc
