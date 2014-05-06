33d8220d0f6ff0ffc69e4710d3cc625bfaafb157
Peter Veentjer <alarmnummer@gmail.com>
2014-05-06 11:01:41 +0300

    Some cleanup

---
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/provisioner/HazelcastJars.java
@@ -0,0 +1,122 @@
+package com.hazelcast.stabilizer.provisioner;
+
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
+import com.hazelcast.stabilizer.Utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.String.format;
+
+public class HazelcastJars {
+
+    private final static ILogger log = Logger.getLogger(HazelcastJars.class);
+    private final Shell shell;
+    private final String versionSpec;
+
+    private File hazelcastJarsDir;
+
+    public HazelcastJars(Shell shell, String versionSpec){
+        this.shell = shell;
+        this.versionSpec = versionSpec;
+    }
+
+    public String getAbsolutePath(){
+        return hazelcastJarsDir.getAbsolutePath();
+    }
+
+    public void prepare() {
+        File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+        hazelcastJarsDir = new File(tmpDir, "hazelcastjars-" + UUID.randomUUID().toString());
+        hazelcastJarsDir.mkdirs();
+
+        if (versionSpec.equals("outofthebox")) {
+            log.info("Using Hazelcast version-spec: outofthebox");
+        } else if (versionSpec.startsWith("path=")) {
+            String path = versionSpec.substring(5);
+            log.info("Using Hazelcast version-spec: path=" + path);
+            File file = new File(path);
+            if (!file.exists()) {
+                log.severe("Directory :" + path + " does not exist");
+                System.exit(1);
+            }
+
+            if (!file.isDirectory()) {
+                log.severe("File :" + path + " is not a directory");
+                System.exit(1);
+            }
+
+            shell.bash(format("cp %s/* %s", path, hazelcastJarsDir.getAbsolutePath()));
+        } else if (versionSpec.equals("none")) {
+            log.info("Using Hazelcast version-spec: none");
+            //we don't need to do anything
+        } else if (versionSpec.startsWith("maven=")) {
+            String version = versionSpec.substring(6);
+            log.info("Using Hazelcast version-spec: maven=" + version);
+            mavenRetrieve("hazelcast", version);
+            mavenRetrieve("hazelcast-client", version);
+        } else {
+            log.severe("Unrecognized version spec:" + versionSpec);
+            System.exit(1);
+        }
+    }
+
+    private void mavenRetrieve(String artifact, String version) {
+        File userhome = new File(System.getProperty("user.home"));
+        File repositoryDir = Utils.toFile(userhome, ".m2", "repository");
+        File artifactFile = Utils.toFile(repositoryDir, "com", "hazelcast",
+                artifact, version, format("%s-%s.jar", artifact, version));
+        if (artifactFile.exists()) {
+            log.finest("Using artifact: " + artifactFile + " from local maven repository");
+            shell.bash(format("cp %s %s", artifactFile.getAbsolutePath(), hazelcastJarsDir.getAbsolutePath()));
+        } else {
+            log.finest("Artifact: " + artifactFile + " is not found in local maven repository, trying online one");
+
+            String url;
+            if (version.endsWith("-SNAPSHOT")) {
+                String baseUrl = "https://oss.sonatype.org/content/repositories/snapshots";
+                String mavenMetadataUrl = format("%s/com/hazelcast/%s/%s/maven-metadata.xml", baseUrl, artifact, version);
+                log.finest("Loading: " + mavenMetadataUrl);
+                String mavenMetadata = null;
+                try {
+                    mavenMetadata = Utils.getText(mavenMetadataUrl);
+                } catch (FileNotFoundException e) {
+                    log.severe("Failed to load " + artifact + "-" + version + ", because :"
+                            + mavenMetadataUrl + " was not found");
+                    System.exit(1);
+                } catch (IOException e) {
+                    log.severe("Could not load:" + mavenMetadataUrl);
+                    System.exit(1);
+                }
+
+                log.finest(mavenMetadata);
+                String timestamp = getTagValue(mavenMetadata, "timestamp");
+                String buildnumber = getTagValue(mavenMetadata, "buildNumber");
+                String shortVersion = version.replace("-SNAPSHOT", "");
+                url = format("%s/com/hazelcast/%s/%s/%s-%s-%s-%s.jar",
+                        baseUrl, artifact, version, artifact, shortVersion, timestamp, buildnumber);
+            } else {
+                String baseUrl = "http://repo1.maven.org/maven2";
+                url = format("%s/com/hazelcast/%s/%s/%s-%s.jar", baseUrl, artifact, version, artifact, version);
+            }
+
+            shell.bash(format("wget --no-verbose --directory-prefix=%s %s", hazelcastJarsDir.getAbsolutePath(), url));
+        }
+    }
+
+    private String getTagValue(String mavenMetadata, String tag) {
+        final Pattern pattern = Pattern.compile("<" + tag + ">(.+?)</" + tag + ">");
+        final Matcher matcher = pattern.matcher(mavenMetadata);
+
+        if (!matcher.find()) {
+            throw new RuntimeException("Could not find " + tag + " in:" + mavenMetadata);
+        }
+
+        return matcher.group(1);
+    }
+}
--- stabilizer/src/main/java/com/hazelcast/stabilizer/provisioner/Provisioner.java
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/provisioner/Provisioner.java
@@ -23,7 +23,6 @@ import org.jclouds.sshj.config.SshjSshClientModule;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -34,14 +33,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static com.hazelcast.stabilizer.Utils.appendText;
 import static com.hazelcast.stabilizer.Utils.getVersion;
@@ -70,6 +66,7 @@ public class Provisioner {
 
     private final List<AgentAddress> addresses = Collections.synchronizedList(new LinkedList<AgentAddress>());
     private final Shell shell;
+    private final HazelcastJars hazelcastJars;
 
     public Provisioner() throws Exception {
         log.info("Hazelcast Stabilizer Provisioner");
@@ -82,6 +79,7 @@ public class Provisioner {
 
         addresses.addAll(AgentsFile.load(agentsFile));
         shell = new Shell(stabilizerProperties);
+        hazelcastJars = new HazelcastJars(shell,    getProperty("HAZELCAST_VERSION_SPEC", "outofthebox"));
     }
 
     private Properties loadStabilizerProperties() {
@@ -113,7 +111,7 @@ public class Provisioner {
             //remove the hazelcast jars, they will be copied from the 'hazelcastJarsDir'.
             shell.ssh(ip, format("rm hazelcast-stabilizer-%s/lib/hazelcast-*.jar", VERSION));
             //copy the actual hazelcast jars that are going to be used by the worker.
-            shell.scpToRemote(ip, hazelcastJarsDir.getAbsolutePath() + "/*.jar", format("hazelcast-stabilizer-%s/lib", VERSION));
+            shell.scpToRemote(ip, hazelcastJars.getAbsolutePath() + "/*.jar", format("hazelcast-stabilizer-%s/lib", VERSION));
         }
     }
 
@@ -150,7 +148,7 @@ public class Provisioner {
     }
 
     public void restart() {
-        prepareHazelcastJars();
+        hazelcastJars.prepare();
         for (AgentAddress address : addresses) {
             installAgent(address.publicAddress);
         }
@@ -199,7 +197,7 @@ public class Provisioner {
             log.info(format("Machines will use Java: %s %s", jdkFlavor, getProperty("JDK_VERSION")));
         }
 
-        prepareHazelcastJars();
+        hazelcastJars.prepare();
 
         ComputeService compute = getComputeService();
 
@@ -255,99 +253,6 @@ public class Provisioner {
         echo("Duration: " + secondsToHuman(TimeUnit.MILLISECONDS.toSeconds(durationMs)));
         echoImportant(format("Successfully provisioned %s %s machines",
                 delta, getProperty("CLOUD_PROVIDER")));
-    }
-
-    private File hazelcastJarsDir;
-
-    private void prepareHazelcastJars() {
-        File tmpDir = new File(System.getProperty("java.io.tmpdir"));
-        hazelcastJarsDir = new File(tmpDir, "hazelcastjars-" + UUID.randomUUID().toString());
-        hazelcastJarsDir.mkdirs();
-
-        String versionSpec = getProperty("HAZELCAST_VERSION_SPEC", "outofthebox");
-        if (versionSpec.equals("outofthebox")) {
-            log.info("Using Hazelcast version-spec: outofthebox");
-        } else if (versionSpec.startsWith("path=")) {
-            String path = versionSpec.substring(5);
-            log.info("Using Hazelcast version-spec: path=" + path);
-            File file = new File(path);
-            if (!file.exists()) {
-                log.severe("Directory :" + path + " does not exist");
-                System.exit(1);
-            }
-
-            if (!file.isDirectory()) {
-                log.severe("File :" + path + " is not a directory");
-                System.exit(1);
-            }
-
-            shell.bash(format("cp %s/* %s", path, hazelcastJarsDir.getAbsolutePath()));
-        } else if (versionSpec.equals("none")) {
-            log.info("Using Hazelcast version-spec: none");
-            //we don't need to do anything
-        } else if (versionSpec.startsWith("maven=")) {
-            String version = versionSpec.substring(6);
-            log.info("Using Hazelcast version-spec: maven=" + version);
-            mavenRetrieve("hazelcast", version);
-            mavenRetrieve("hazelcast-client", version);
-        } else {
-            log.severe("Unrecognized version spec:" + versionSpec);
-            System.exit(1);
-        }
-    }
-
-    private void mavenRetrieve(String artifact, String version) {
-        File userhome = new File(System.getProperty("user.home"));
-        File repositoryDir = Utils.toFile(userhome, ".m2", "repository");
-        File artifactFile = Utils.toFile(repositoryDir, "com", "hazelcast",
-                artifact, version, format("%s-%s.jar", artifact, version));
-        if (artifactFile.exists()) {
-            log.finest("Using artifact: " + artifactFile + " from local maven repository");
-            shell.bash(format("cp %s %s", artifactFile.getAbsolutePath(), hazelcastJarsDir.getAbsolutePath()));
-        } else {
-            log.finest("Artifact: " + artifactFile + " is not found in local maven repository, trying online one");
-
-            String url;
-            if (version.endsWith("-SNAPSHOT")) {
-                String baseUrl = "https://oss.sonatype.org/content/repositories/snapshots";
-                String mavenMetadataUrl = format("%s/com/hazelcast/%s/%s/maven-metadata.xml", baseUrl, artifact, version);
-                log.finest("Loading: " + mavenMetadataUrl);
-                String mavenMetadata = null;
-                try {
-                    mavenMetadata = Utils.getText(mavenMetadataUrl);
-                } catch (FileNotFoundException e) {
-                    log.severe("Failed to load " + artifact + "-" + version + ", because :"
-                            + mavenMetadataUrl + " was not found");
-                    System.exit(1);
-                } catch (IOException e) {
-                    log.severe("Could not load:" + mavenMetadataUrl);
-                    System.exit(1);
-                }
-
-                log.finest(mavenMetadata);
-                String timestamp = getTagValue(mavenMetadata, "timestamp");
-                String buildnumber = getTagValue(mavenMetadata, "buildNumber");
-                String shortVersion = version.replace("-SNAPSHOT", "");
-                url = format("%s/com/hazelcast/%s/%s/%s-%s-%s-%s.jar",
-                        baseUrl, artifact, version, artifact, shortVersion, timestamp, buildnumber);
-            } else {
-                String baseUrl = "http://repo1.maven.org/maven2";
-                url = format("%s/com/hazelcast/%s/%s/%s-%s.jar", baseUrl, artifact, version, artifact, version);
-            }
-
-            shell.bash(format("wget --no-verbose --directory-prefix=%s %s", hazelcastJarsDir.getAbsolutePath(), url));
-        }
-    }
-
-    private String getTagValue(String mavenMetadata, String tag) {
-        final Pattern pattern = Pattern.compile("<" + tag + ">(.+?)</" + tag + ">");
-        final Matcher matcher = pattern.matcher(mavenMetadata);
-
-        if (!matcher.find()) {
-            throw new RuntimeException("Could not find " + tag + " in:" + mavenMetadata);
-        }
-
-        return matcher.group(1);
     }
 
     private class InstallNodeTask implements Runnable {
