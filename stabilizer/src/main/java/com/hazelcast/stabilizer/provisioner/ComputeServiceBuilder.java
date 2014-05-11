b9e6bc986f201e4d8a605d5ff4eeed57efbec99b
Greg Luck <gluck@gregluck.com>
2014-05-11 15:45:34 +1000

    Added additional locationId information.

--- stabilizer/src/main/java/com/hazelcast/stabilizer/provisioner/ComputeServiceBuilder.java
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/provisioner/ComputeServiceBuilder.java
@@ -48,6 +48,7 @@ public class ComputeServiceBuilder {
                 .credentials(identity, credential)
                 .modules(getModules())
                 .buildView(ComputeServiceContext.class)
+                .
                 .getComputeService();
     }
 
