08ef8e5cdb5e3a8947a4e0a7309a1877db365ad1
danny <danny@hazelcast.com>
2014-06-17 11:05:09 +0300

    verfy all most there

--- stabilizer/src/main/java/com/hazelcast/stabilizer/tests/map/MapUsageStressTest.java
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/tests/map/MapUsageStressTest.java
@@ -34,6 +34,7 @@ import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static org.junit.Assert.assertEquals;
 
@@ -47,34 +48,28 @@ public class MapUsageStressTest {
     public int valueLength = 100;
     public int keyCount = 1000;
     public int valueCount = 1000;
-    public int maxMaps = 1;
 
     public boolean randomDistributionUniform=true;
 
     //check these add up to 1
     public double writeProb = 0.4;
-    public double getProb = 0.2;
+    public double evictProb = 0.2;
     public double removeProb = 0.2;
     public double deleteProb = 0.2;
     //
 
     //check these add up to 1   (writeProb is split up into sub styles)
-    public double writeUsingPutProb = 0.25;
-    public double writeUsingPutIfAbsent = 0.15;
-    public double writeUsingPutExpireProb = 0.5;
-    public double replaceProb = 0.1;
+    public double writeUsingPutProb = 0.5;
+    public double writeUsingPutIfAbsent = 0.25;
+    public double replaceProb = 0.25;
     //
 
-    public int minExpireMillis = 200;
-    public int maxExpireMillis = 8000;
-
-    public int localMapEntryListenerCount = 1;
     public int mapEntryListenerCount = 1;
 
-    private final AtomicInteger localAddCount = new AtomicInteger(0);
-    private final AtomicInteger localRemoveCount = new AtomicInteger(0);
-    private final AtomicInteger localUpdateCount = new AtomicInteger(0);
-    private final AtomicInteger localEvictCount = new AtomicInteger(0);
+    private final AtomicLong localAddCount = new AtomicLong(0);
+    private final AtomicLong localRemoveCount = new AtomicLong(0);
+    private final AtomicLong localUpdateCount = new AtomicLong(0);
+    private final AtomicLong localEvictCount = new AtomicLong(0);
 
 
     private String[] values;
@@ -83,7 +78,6 @@ public class MapUsageStressTest {
 
     private Map<String, EntryListenerImpl> listeners = new HashMap();
 
-    ScrambledZipfianGenerator mapsZipfian = new ScrambledZipfianGenerator(maxMaps);
     ScrambledZipfianGenerator kesyZipfian = new ScrambledZipfianGenerator(keyCount);
 
     @Setup
@@ -100,28 +94,19 @@ public class MapUsageStressTest {
             values[k] = makeString(valueLength);
         }
 
-        for (int i = 0; i < maxMaps; i++) {
-            String name = basename + i;
-            IMap map = targetInstance.getMap(name);
+        IMap map = targetInstance.getMap(basename);
 
-            for (int count = 0; count < mapEntryListenerCount; count++) {
-                EntryListenerImpl l = new EntryListenerImpl();
-                listeners.put(name, l);
-                map.addEntryListener(l, true);
-            }
+        for (int count = 0; count < mapEntryListenerCount; count++) {
+            EntryListenerImpl l = new EntryListenerImpl();
+            listeners.put(basename, l);
+            map.addEntryListener(l, true);
+        }
 
-            for (int count = 0; count < localMapEntryListenerCount; count++) {
-                EntryListenerImpl l = new EntryListenerImpl();
-                listeners.put(name+"local", l);
-                map.addLocalEntryListener(l);
-            }
-
-            int v = 0;
-            for (int k = 0; k < keyCount; k++) {
-                map.put(k, values[v]);
-                localAddCount.getAndIncrement();
-                v = (v + 1 == values.length ? 0 : v + 1);
-            }
+        int v = 0;
+        for (int k = 0; k < keyCount; k++) {
+            map.put(k, values[v]);
+            localAddCount.getAndIncrement();
+            v = (v + 1 == values.length ? 0 : v + 1);
         }
     }
 
@@ -148,10 +133,8 @@ public class MapUsageStressTest {
 
     @Teardown(global = true)
     public void tearDown() throws Exception {
-        for (int i = 0; i < maxMaps; i++) {
-            IMap map = targetInstance.getMap(basename + i);
-            map.destroy();
-        }
+        IMap map = targetInstance.getMap(basename);
+        map.destroy();
     }
 
     @Performance
@@ -159,64 +142,40 @@ public class MapUsageStressTest {
         return 1;
     }
 
-
     @Verify(global = false)
     public void verify() throws Exception {
+        Thread.sleep(5000);
 
-        Thread.sleep(10000);
+        IMap map = targetInstance.getMap(basename);
+        EntryListenerImpl e = listeners.get(basename);
 
-        /*
-        for (int i = 0; i < maxMaps; i++) {
-            String name = basename + i;
-            IMap map = targetInstance.getMap(name);
-            EntryListenerImpl e = listeners.get(name);
+        System.out.println("add = "+localAddCount.get() +" "+ e.addCount.get());
+        System.out.println("update = "+localUpdateCount.get() +" "+ e.updateCount.get());
+        System.out.println("remove = "+localRemoveCount.get() +" "+ e.removeCount.get());
+        System.out.println("evict = "+localEvictCount.get() +" "+ e.evictCount.get());
 
-            log.info(e.toString());
+        long expectedMapSz = e.addCount.get() - (e.evictCount.get() + e.removeCount.get());
+        assertEquals(expectedMapSz, map.size());
 
-
-            int expectedMapSz = e.addCount.get() - (e.evictCount.get() + e.removeCount.get());
-            assertEquals(expectedMapSz, map.size());
-        }
-        */
-
-        for (int i = 0; i < localMapEntryListenerCount; i++) {
-            String name = basename + i;
-            EntryListenerImpl e = listeners.get(name+"local");
-
-            while(true){
-
-
-                System.out.println("add = "+localAddCount.get() +" "+ e.addCount.get());
-                System.out.println("update = "+localUpdateCount.get() +" "+ e.updateCount.get());
-
-                System.out.println("remove = "+localRemoveCount.get() +" "+ e.removeCount.get());
-                System.out.println("evict = "+localEvictCount.get() +" "+ e.evictCount.get());
-
-
-                Thread.sleep(3000);
-                //assertEquals(localAddCount.get(), e.addCount.get());
-                //assertEquals(localUpdateCount.get(), e.updateCount.get());
-            }
-        }
+        //assertEquals(localAddCount.get(), e.addCount.get());
+        //assertEquals(localUpdateCount.get(), e.updateCount.get());
     }
 
     private class Worker implements Runnable {
         private final Random random = new Random();
-        int mapIdx, key;
+        int key;
 
         public void run() {
 
             while (!testContext.isStopped()) {
 
                 if(randomDistributionUniform){
-                    mapIdx = random.nextInt(maxMaps);
                     key = random.nextInt(keyCount);
                 }else{
-                    mapIdx = mapsZipfian.nextInt();
                     key = kesyZipfian.nextInt();
                 }
 
-                IMap map = targetInstance.getMap(basename + mapIdx);
+                IMap map = targetInstance.getMap(basename);
 
                 double chance = random.nextDouble();
                 if (chance < writeProb) {
@@ -225,7 +184,6 @@ public class MapUsageStressTest {
 
                     chance = random.nextDouble();
                     if (chance < writeUsingPutProb) {
-
                         map.lock(key);
                         try{
                             if(map.containsKey(key)){
@@ -239,7 +197,6 @@ public class MapUsageStressTest {
                         }
                     }
                     else if(chance < writeUsingPutIfAbsent + writeUsingPutProb ){
-
                         map.lock(key);
                         try{
                             if(map.putIfAbsent(key, value) == null ){
@@ -248,44 +205,31 @@ public class MapUsageStressTest {
                         }finally {
                             map.unlock(key);
                         }
-
                     }
-                    else if ( chance <  writeUsingPutExpireProb + writeUsingPutIfAbsent + writeUsingPutProb) {
-                        int expire = random.nextInt(maxExpireMillis) + minExpireMillis;
-
-                        map.lock(key);
-                        try{
-                            if(map.containsKey(key)){
-                                localUpdateCount.getAndIncrement();
-                            }else {
-                                localAddCount.getAndIncrement();
-                            }
-                            map.put(key, value, expire, TimeUnit.MILLISECONDS);
-                        }
-                        finally {
-                            map.unlock(key);
-                        }
-                    }
-                    else if(chance < replaceProb + writeUsingPutExpireProb + writeUsingPutIfAbsent + writeUsingPutProb){
-
+                    else if(chance < replaceProb + writeUsingPutIfAbsent + writeUsingPutProb){
                         Object orig = map.get(key);
                         if ( orig !=null && map.replace(key, orig, value) ){
-                            localUpdateCount.getAndIncrement();
+                            localAddCount.getAndIncrement();
                         }
                     }
-
-
-                }else if(chance < getProb + writeProb){
-                    map.get(key);
+                }else if(chance < evictProb + writeProb){
+                    map.lock(key);
+                    try{
+                        if(map.containsKey(key)){
+                            localEvictCount.getAndIncrement();
+                        }
+                        map.evict(key);
+                    }finally {
+                        map.unlock(key);
+                    }
                 }
-                else if(chance < removeProb + getProb + writeProb){
+                else if(chance < removeProb + evictProb + writeProb){
                     Object o = map.remove(key);
                     if(o != null){
                         localRemoveCount.getAndIncrement();
                     }
                 }
-                else if (chance < deleteProb + removeProb + getProb + writeProb ){
-
+                else if (chance < deleteProb + removeProb + evictProb + writeProb ){
                     map.lock(key);
                     try{
                         if(map.containsKey(key)){
@@ -295,9 +239,7 @@ public class MapUsageStressTest {
                     }finally {
                         map.unlock(key);
                     }
-
                 }
-
             }
         }
     }
@@ -305,14 +247,12 @@ public class MapUsageStressTest {
 
     public class EntryListenerImpl implements EntryListener<Object, Object> {
 
-        public final AtomicInteger addCount = new AtomicInteger();
-        public final AtomicInteger removeCount = new AtomicInteger();
-        public final AtomicInteger updateCount = new AtomicInteger();
-        public final AtomicInteger evictCount = new AtomicInteger();
+        public final AtomicLong addCount = new AtomicLong();
+        public final AtomicLong removeCount = new AtomicLong();
+        public final AtomicLong updateCount = new AtomicLong();
+        public final AtomicLong evictCount = new AtomicLong();
 
-        public EntryListenerImpl( ) {
-
-        }
+        public EntryListenerImpl( ) { }
 
         @Override
         public void entryAdded(EntryEvent<Object, Object> objectObjectEntryEvent) {
