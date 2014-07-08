1b97b8f1c910bdf442ec533e80a2bd521b75fb6c
danny <danny@hazelcast.com>
2014-07-08 16:12:33 +0300

    M

---
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/tests/map/MapStoreTest.java
@@ -0,0 +1,133 @@
+package com.hazelcast.stabilizer.tests.map;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.stabilizer.tests.TestContext;
+import com.hazelcast.stabilizer.tests.TestRunner;
+import com.hazelcast.stabilizer.tests.annotations.Run;
+import com.hazelcast.stabilizer.tests.annotations.Setup;
+import com.hazelcast.stabilizer.tests.annotations.Verify;
+import com.hazelcast.stabilizer.tests.utils.ThreadSpawner;
+
+import java.util.Random;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by danny on 6/27/14.
+ */
+public class MapStoreTest {
+
+    public String basename = this.getClass().getName();
+    public int threadCount = 3;
+    public int keyCount = 10;
+
+    //check these add up to 1
+    public double writeProb = 0.4;
+    public double evictProb = 0.2;
+    public double removeProb = 0.2;
+    public double deleteProb = 0.2;
+    //
+
+    //check these add up to 1   (writeProb is split up into sub styles)
+    public double writeUsingPutProb = 0.5;
+    public double writeUsingPutIfAbsent = 0.25;
+    public double replaceProb = 0.25;
+    //
+
+
+    private TestContext testContext;
+    private HazelcastInstance targetInstance;
+
+
+    public MapStoreTest(){
+
+    }
+
+    @Setup
+    public void setup(TestContext testContext) throws Exception {
+        this.testContext = testContext;
+        targetInstance = testContext.getTargetInstance();
+    }
+
+
+    @Run
+    public void run() {
+        ThreadSpawner spawner = new ThreadSpawner(testContext.getTestId());
+        for (int k = 0; k < threadCount; k++) {
+            spawner.spawn(new Worker());
+        }
+        spawner.awaitCompletion();
+    }
+
+
+    private class Worker implements Runnable {
+        private final Random random = new Random();
+        int key;
+
+        @Override
+        public void run() {
+
+            final IMap map = targetInstance.getMap(basename);
+            map.size();
+
+            while (!testContext.isStopped()) {
+                key = random.nextInt(keyCount);
+
+                double chance = random.nextDouble();
+                if (chance < writeProb) {
+
+                    final Object value = random.nextInt(keyCount);
+
+                    chance = random.nextDouble();
+                    if (chance < writeUsingPutProb) {
+                        map.put(key, value);
+                    }
+                    else if(chance < writeUsingPutIfAbsent + writeUsingPutProb ){
+                        //map.putIfAbsent(key, value);
+                    }
+                    else if(chance < replaceProb + writeUsingPutIfAbsent + writeUsingPutProb){
+                        //Object orig = map.get(key);
+                        //if ( orig !=null ){
+                        //    map.replace(key, orig, value);
+                        //}
+                    }
+                }else if(chance < evictProb + writeProb){
+                    //map.evict(key);
+                }
+                else if(chance < removeProb + evictProb + writeProb){
+                    //map.remove(key);
+                }
+                else if (chance < deleteProb + removeProb + evictProb + writeProb ){
+                    //map.delete(key);
+                }
+            }
+        }
+    }
+
+    @Verify(global = false)
+    public void verify() throws Exception {
+        System.out.println("verify "+basename+" !!");
+
+        final IMap map = targetInstance.getMap(basename);
+        MapStoreWithCounter mapStore = (MapStoreWithCounter) targetInstance.getConfig().getMapConfig(basename).getMapStoreConfig().getImplementation();
+
+
+        System.out.println("map local keys =" + map.localKeySet());
+        System.out.println("map local      =" + map.getAll(map.localKeySet()).entrySet());
+        System.out.println("map Store      =" + mapStore.store.entrySet());
+
+        //this is still wrong as some other node is putting to the keys you own.
+        //how to do it 1) real DB,  TimeStamp on all events, and check the last ones
+        for(Object k: map.localKeySet()){
+            Object storeValue = mapStore.store.get(k);
+            assertEquals( map.get(k), storeValue );
+        }
+
+    }
+
+    public static void main(String[] args) throws Throwable {
+        new TestRunner(new MapStoreTest()).run();
+    }
+
+}
---
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/tests/map/MapStoreWithCounter.java
@@ -0,0 +1,114 @@
+package com.hazelcast.stabilizer.tests.map;
+
+import com.hazelcast.core.MapStore;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MapStoreWithCounter implements MapStore<Object, Object> {
+
+    protected final Map store = new ConcurrentHashMap();
+
+    protected AtomicInteger countStore = new AtomicInteger(0);
+    protected AtomicInteger countDelete = new AtomicInteger(0);
+    protected List<AtomicInteger> batchOpCountList = Collections.synchronizedList(new ArrayList<AtomicInteger>());
+
+
+    public MapStoreWithCounter(){
+
+        System.out.println("===>>"+this.getClass().getName()+" INSTANCE !!");
+
+
+    }
+
+
+    @Override
+    public void store(Object key, Object value) {
+
+        System.out.println("store "+key+" "+value);
+        countStore.incrementAndGet();
+        store.put(key, value);
+    }
+
+    @Override
+    public void storeAll(Map<Object, Object> map) {
+        batchOpCountList.add(new AtomicInteger(map.size()));
+
+        countStore.addAndGet(map.size());
+        for (Map.Entry<Object, Object> kvp : map.entrySet()) {
+            store.put(kvp.getKey(), kvp.getValue());
+        }
+    }
+
+    @Override
+    public void delete(Object key) {
+        countDelete.incrementAndGet();
+        store.remove(key);
+    }
+
+    @Override
+    public void deleteAll(Collection<Object> keys) {
+        countDelete.addAndGet(keys.size());
+        for (Object key : keys) {
+            store.remove(key);
+        }
+    }
+
+    @Override
+    public Object load(Object key) {
+        return store.get(key);
+    }
+
+    @Override
+    public Map<Object, Object> loadAll(Collection<Object> keys) {
+        Map result = new HashMap();
+        for (Object key : keys) {
+            final Object v = store.get(key);
+            if (v != null) {
+                result.put(key, v);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Set<Object> loadAllKeys() {
+        return store.keySet();
+    }
+
+    public int getStoreOpCount() {
+        return countStore.intValue();
+    }
+
+    public int getDeleteOpCount() {
+        return countDelete.intValue();
+    }
+
+    public List<AtomicInteger> getBatchStoreOpCount() {
+        return batchOpCountList;
+    }
+
+    public int size() {
+        return store.size();
+    }
+
+
+    public int findNumberOfBatchsEqualWriteBatchSize(int writeBatchsize) {
+        int count = 0;
+        final List<AtomicInteger> batchStoreOpCount = getBatchStoreOpCount();
+        for (AtomicInteger atomicInteger : batchStoreOpCount) {
+            final int value = atomicInteger.intValue();
+            if (value == writeBatchsize) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
