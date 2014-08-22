a557abb39bc8509b86e73197121673d0c0680c43
danny <danny@hazelcast.com>
2014-08-22 23:53:33 +0300

    testing

--- dist/src/main/dist/stabilizer-tests/sandbox/killall.sh
+++ dist/src/main/dist/stabilizer-tests/sandbox/killall.sh
@@ -2,11 +2,8 @@
 
 list=$(cat agents.txt | cut -d',' -f1)
 
-#Set the field separator to new line
-IFS=$'\n'
+readarray -t array <<<"$list"
 
-for item in $list
-do
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${item} "jps | grep Worker | cut -d ' ' -f1 | xargs -L 1  kill -9 $1"
-	break
-done
+box=${array[2]}
+
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${item} "jps | grep Worker | cut -d ' ' -f1 | xargs -L 1  kill -9 $1"
