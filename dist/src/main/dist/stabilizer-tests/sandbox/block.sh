41cd6c051e1d08676724d1eaebb17ceb48660807
danny <danny@hazelcast.com>
2014-08-22 17:56:20 +0300

    testing

--- dist/src/main/dist/stabilizer-tests/sandbox/block.sh
+++ dist/src/main/dist/stabilizer-tests/sandbox/block.sh
@@ -7,5 +7,11 @@ echo "Blocking interface $INF"
 sudo iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
+sudo iptables -A INPUT -p tcp --dport 9000 -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 9001 -m state --state ESTABLISHED -j ACCEPT
+
+sudo iptables -A INPUT -p tcp --dport 9000 -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 9001 -m state --state ESTABLISHED -j ACCEPT
+
 sudo iptables -A INPUT -i $INF -j DROP
 sudo iptables -A OUTPUT -o $INF -j DROP
