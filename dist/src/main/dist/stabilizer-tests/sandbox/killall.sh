2df9fc8d18c45f8e3ac543f785da3b536f8aae35
danny <danny@hazelcast.com>
2014-08-22 23:59:14 +0300

    testing

--- dist/src/main/dist/stabilizer-tests/sandbox/killall.sh
+++ dist/src/main/dist/stabilizer-tests/sandbox/killall.sh
@@ -6,4 +6,4 @@ readarray -t array <<<"$list"
 
 box=${array[2]}
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${item} "jps | grep Worker | cut -d ' ' -f1 | xargs -L 1  kill -9 $1"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${box} "jps | grep Worker | cut -d ' ' -f1 | xargs -L 1  kill -9 $1"
