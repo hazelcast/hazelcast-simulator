c85ccbbf3129ffc1c90e6d479be6289f7468e044
danny <danny@hazelcast.com>
2014-08-22 18:50:52 +0300

    testing

--- dist/src/main/dist/stabilizer-tests/sandbox/block.sh
+++ dist/src/main/dist/stabilizer-tests/sandbox/block.sh
@@ -4,14 +4,14 @@ INF=$1
 
 echo "Blocking interface $INF"
 
-sudo iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-sudo iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+sudo /sbin/iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
-sudo iptables -A INPUT -p tcp --dport 9000 -m state --state NEW,ESTABLISHED -j ACCEPT
-sudo iptables -A OUTPUT -p tcp --sport 9000 -m state --state ESTABLISHED -j ACCEPT
+sudo /sbin/iptables -A INPUT -p tcp --dport 9000 -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -p tcp --sport 9000 -m state --state ESTABLISHED -j ACCEPT
 
-sudo iptables -A INPUT -p tcp --dport 9001 -m state --state NEW,ESTABLISHED -j ACCEPT
-sudo iptables -A OUTPUT -p tcp --sport 9001 -m state --state ESTABLISHED -j ACCEPT
+sudo /sbin/iptables -A INPUT -p tcp --dport 9001 -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -p tcp --sport 9001 -m state --state ESTABLISHED -j ACCEPT
 
-sudo iptables -A INPUT -i $INF -j DROP
-sudo iptables -A OUTPUT -o $INF -j DROP
+sudo /sbin/iptables -A INPUT -i $INF -j DROP
+sudo /sbin/iptables -A OUTPUT -o $INF -j DROP
