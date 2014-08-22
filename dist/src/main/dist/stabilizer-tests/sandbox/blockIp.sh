19a78d213f97c5b640ccc307d012dfb0b5daa3e4
danny <danny@hazelcast.com>
2014-08-22 20:50:35 +0300

    testing

--- dist/src/main/dist/stabilizer-tests/sandbox/blockIp.sh
+++ dist/src/main/dist/stabilizer-tests/sandbox/blockIp.sh
@@ -5,9 +5,9 @@ list=$(cat agents.txt | cut -d',' -f1)
 #Set the field separator to new line
 IFS=$'\n'
 
-for box in $list
-do
-    scp block.sh stabilizer@${box}:~
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${box} "chmod +x block.sh; ./block.sh eth0"
-	break
-done
+box=${list[2]}
+
+scp block.sh stabilizer@${box}:~
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${box} "chmod +x block.sh; ./block.sh eth0"
+
+
