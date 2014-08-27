007d002baeb0a1cb8a23974e9f1ea059dff7b7dc
danny <danny@hazelcast.com>
2014-08-27 12:14:53 +0300

    testing

--- dist/src/main/dist/stabilizer-tests/sandbox/clean.sh
+++ dist/src/main/dist/stabilizer-tests/sandbox/clean.sh
@@ -2,10 +2,16 @@
 
 rm -rf workers/*
 
-rm -f failures-[0-9]*.txt
-
 rm -f logs/*
+
+rm -f failures-[0-9]*.txt
 
 rm -f nohup-[0-9]*.out
 
 rm -f nohup.out
+
+rm -f nohupRun*.out
+
+rm -f results.zip
+
+rm -f tempNotifyFifo
