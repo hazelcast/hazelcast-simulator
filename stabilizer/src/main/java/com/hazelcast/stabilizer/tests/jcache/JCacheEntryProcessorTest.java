34861567dc9a57f524bc5b83493dcfc986e87031
Peter Veentjer <alarmnummer@gmail.com>
2014-09-03 15:41:51 +0300

    JCache WIP

---
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/tests/jcache/JCacheEntryProcessorTest.java
@@ -0,0 +1,200 @@
+package com.hazelcast.stabilizer.tests.jcache;
+
+import com.hazelcast.cache.HazelcastCacheManager;
+import com.hazelcast.cache.HazelcastCachingProvider;
+import com.hazelcast.cache.ICache;
+import com.hazelcast.config.CacheConfig;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IList;
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
+import com.hazelcast.stabilizer.tests.TestContext;
+import com.hazelcast.stabilizer.tests.TestRunner;
+import com.hazelcast.stabilizer.tests.annotations.Performance;
+import com.hazelcast.stabilizer.tests.annotations.Run;
+import com.hazelcast.stabilizer.tests.annotations.Setup;
+import com.hazelcast.stabilizer.tests.annotations.Teardown;
+import com.hazelcast.stabilizer.tests.annotations.Verify;
+import com.hazelcast.stabilizer.tests.annotations.Warmup;
+import com.hazelcast.stabilizer.tests.utils.ThreadSpawner;
+
+import javax.cache.processor.EntryProcessor;
+import javax.cache.processor.EntryProcessorException;
+import javax.cache.processor.MutableEntry;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.junit.Assert.assertEquals;
+
+public class JCacheEntryProcessorTest {
+
+    private final static ILogger log = Logger.getLogger(JCacheEntryProcessorTest.class);
+
+    //props
+    public String basename = this.getClass().getName();
+    public int threadCount = 10;
+    public int keyCount = 1000;
+    public int minProcessorDelayMs = 0;
+    public int maxProcessorDelayMs = 0;
+    public int logFrequency = 10000;
+    public int performanceUpdateFrequency = 10000;
+
+    private final AtomicLong operations = new AtomicLong();
+    private ICache<Integer, Long> cache;
+    private IList<Map<Integer, Long>> resultsPerWorker;
+    private TestContext testContext;
+    private HazelcastInstance targetInstance;
+
+    @Setup
+    public void setup(TestContext testContext) throws Exception {
+        this.testContext = testContext;
+        targetInstance = testContext.getTargetInstance();
+        HazelcastCachingProvider hcp = new HazelcastCachingProvider();
+
+        HazelcastCacheManager cacheManager = new HazelcastCacheManager(
+                hcp, targetInstance, hcp.getDefaultURI(), hcp.getDefaultClassLoader(), null);
+
+        CacheConfig<Integer, String> config = new CacheConfig<Integer, String>();
+        config.setName("SimpleCache");
+        config.setInMemoryFormat(InMemoryFormat.OBJECT);
+
+        cache = cacheManager.getCache("simpleCache");
+        resultsPerWorker = targetInstance.getList(basename + "ResultMap" + testContext.getTestId());
+    }
+
+    @Teardown
+    public void teardown() throws Exception {
+        cache.destroy();
+        resultsPerWorker.destroy();
+    }
+
+    @Warmup(global = true)
+    public void warmup() throws Exception {
+        for (int k = 0; k < keyCount; k++) {
+            cache.put(k, 0l);
+        }
+        System.out.println(basename + " cache size ==>" + cache.size());
+    }
+
+    @Run
+    public void run() {
+        ThreadSpawner spawner = new ThreadSpawner(testContext.getTestId());
+        for (int k = 0; k < threadCount; k++) {
+            spawner.spawn(new Worker());
+        }
+        spawner.awaitCompletion();
+    }
+
+    @Performance
+    public long getOperationCount() {
+        return operations.get();
+    }
+
+    @Verify
+    public void verify() throws Exception {
+        long[] amount = new long[keyCount];
+
+        for (Map<Integer, Long> map : resultsPerWorker) {
+            for (Map.Entry<Integer, Long> entry : map.entrySet()) {
+                amount[entry.getKey()] += entry.getValue();
+            }
+        }
+
+        int failures = 0;
+        for (int k = 0; k < keyCount; k++) {
+            long expected = amount[k];
+            long found = cache.get(k);
+            if (expected != found) {
+                failures++;
+            }
+        }
+
+        assertEquals("Failures have been found", 0, failures);
+    }
+
+    private class Worker implements Runnable {
+        private final Random random = new Random();
+        private final Map<Integer, Long> result = new HashMap<Integer, Long>();
+
+        public Worker() {
+            for (int k = 0; k < keyCount; k++) {
+                result.put(k, 0L);
+            }
+        }
+
+        @Override
+        public void run() {
+            long iteration = 0;
+            while (!testContext.isStopped()) {
+                int key = random.nextInt(keyCount);
+                long increment = random.nextInt(100);
+
+                int delayMs = 0;
+                if (maxProcessorDelayMs != 0) {
+                    delayMs = minProcessorDelayMs + random.nextInt(maxProcessorDelayMs);
+                }
+
+                cache.invoke(key, new IncrementEntryProcessor(increment, delayMs));
+                increment(key, increment);
+
+
+                if (iteration % logFrequency == 0) {
+                    log.info(Thread.currentThread().getName() + " At iteration: " + iteration);
+                }
+
+                if (iteration % performanceUpdateFrequency == 0) {
+                    operations.addAndGet(performanceUpdateFrequency);
+                }
+            }
+
+            //sleep to give time for the last EntryProcessor tasks to complete.
+            try {
+                Thread.sleep(maxProcessorDelayMs * 2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            resultsPerWorker.add(result);
+        }
+
+        private void increment(int key, long increment) {
+            result.put(key, result.get(key) + increment);
+        }
+    }
+
+    private static class IncrementEntryProcessor implements EntryProcessor<Integer, Long, Object> {
+        private final long increment;
+        private final long delayMs;
+
+        private IncrementEntryProcessor(long increment, long delayMs) {
+            this.increment = increment;
+            this.delayMs = delayMs;
+        }
+
+        @Override
+        public Object process(MutableEntry<Integer, Long> entry, Object... arguments) throws EntryProcessorException {
+            delay();
+            long newValue = entry.getValue() + increment;
+            entry.setValue(newValue);
+            return null;
+        }
+
+        private void delay() {
+            if (delayMs != 0) {
+                try {
+                    Thread.sleep(delayMs);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Throwable {
+        JCacheEntryProcessorTest test = new JCacheEntryProcessorTest();
+        new TestRunner(test).run();
+    }
+}
+
