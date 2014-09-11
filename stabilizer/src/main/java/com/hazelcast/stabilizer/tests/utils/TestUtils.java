d4351a5ab2b94a4ccc86c70297f2d9b4d07a0f23
Peter Veentjer <alarmnummer@gmail.com>
2014-09-11 13:30:28 +0300

    Added the AtomicReferenceTest

--- stabilizer/src/main/java/com/hazelcast/stabilizer/tests/utils/TestUtils.java
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/tests/utils/TestUtils.java
@@ -29,12 +29,21 @@ import com.hazelcast.stabilizer.tests.BindException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Map;
+import java.util.Random;
 
 import static java.lang.String.format;
 
 public class TestUtils {
 
     public static final String TEST_INSTANCE = "testInstance";
+
+    public static byte[] randomByteArray(Random random, int length) {
+        byte[] result = new byte[length];
+        for (int k = 0; k < length; k++) {
+            result[k] = (byte) random.nextInt();
+        }
+        return result;
+    }
 
     public static void warmupPartitions(ILogger logger, HazelcastInstance hz) {
         PartitionService partitionService = hz.getPartitionService();
