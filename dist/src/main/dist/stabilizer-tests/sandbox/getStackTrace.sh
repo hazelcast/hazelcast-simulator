640360ee7d6821b5afc532228f238c39238a7db3
danny <danny@hazelcast.com>
2014-09-11 11:38:56 +0300

    map Reduce test stabilizer

--- dist/src/main/dist/stabilizer-tests/sandbox/getStackTrace.sh
+++ dist/src/main/dist/stabilizer-tests/sandbox/getStackTrace.sh
@@ -7,7 +7,7 @@ IFS=$'\n'
 
 for item in $list
 do
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${item} "rm -f jstackTrack.txt; jps | grep Worker | cut -d ' ' -f1 | xargs -L 1  jstack $1 >> jstackTrack.txt"
+	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${item} "rm -f jstackTrack.txt; jps | grep .*Worker | cut -d ' ' -f1 | xargs -L 1  jstack $1 >> jstackTrack.txt"
 	scp stabilizer@${item}:jstackTrack.txt ${item}temp
 done
 
