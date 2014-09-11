9d311e71cd65f40f4fc512a96251a4a60889caa8
danny <danny@hazelcast.com>
2014-09-11 11:58:30 +0300

    map Reduce test stabilizer

--- dist/src/main/dist/stabilizer-tests/sandbox/getStackTrace.sh
+++ dist/src/main/dist/stabilizer-tests/sandbox/getStackTrace.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 
-list=$(cat agents.txt | cut -d',' -f1)
+everySeconds=10
+max=12
+
+ips=$(cat agents.txt | cut -d',' -f1)
 
 #Set the field separator to new line
 IFS=$'\n'
 
-for item in $list
+
+for i in {1..${max}}
 do
-	ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${item} "rm -f jstackTrack.txt; jps | grep .*Worker | cut -d ' ' -f1 | xargs -L 1  jstack $1 >> jstackTrack.txt"
-	scp stabilizer@${item}:jstackTrack.txt ${item}temp
+
+    for box in $ips
+    do
+        ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no stabilizer@${box} "rm -f jstackTrack.txt; jps | grep .*Worker | cut -d ' ' -f1 | xargs -L 1  jstack $1 >> jstackTrack.txt"
+        scp stabilizer@${box}:jstackTrack.txt ${box}temp
+    done
+
+    for box in $ips
+    do
+        cat ${box}temp >> jstackTrack${i}.txt
+        rm -f ${box}temp
+    done
+
+    sleep ${everySeconds}
+
 done
 
-for item in $list
-do
-  cat ${item}temp >> jstackTrack.txt
-  rm -f ${item}temp
-done
+echo "The End"
+
