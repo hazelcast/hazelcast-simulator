7d1c656bf44806d8c6302d089ef7ffebff964a78
Peter Veentjer <alarmnummer@gmail.com>
2014-09-12 12:25:50 +0300

    Removed the println that was added for experiment

--- stabilizer/src/main/java/com/hazelcast/stabilizer/tests/concurrent/atomiclong/AtomicLongTest.java
+++ stabilizer/src/main/java/com/hazelcast/stabilizer/tests/concurrent/atomiclong/AtomicLongTest.java
@@ -57,9 +57,7 @@ public class AtomicLongTest {
 
     @Setup
     public void setup(TestContext context) throws Exception {
-        System.out.println("Temporary sys out call see if it is captured correctly");
-
-        this.context = context;
+         this.context = context;
 
         if (writePercentage < 0) {
             throw new IllegalArgumentException("Write percentage can't be smaller than 0");
