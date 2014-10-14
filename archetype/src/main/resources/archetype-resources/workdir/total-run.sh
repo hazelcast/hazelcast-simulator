ccd94dfc94242b4ff88cca5c29534e801bf397c1
Peter Veentjer <alarmnummer@gmail.com>
2014-10-14 14:46:44 +0200

    Some archetype cleanup

--- archetype/src/main/resources/archetype-resources/workdir/total-run.sh
+++ archetype/src/main/resources/archetype-resources/workdir/total-run.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+provisioner --scale 4
+
 coordinator     --workerVmOptions "-ea -server -Xms2G -Xmx2G -XX:+PrintGC -XX:+HeapDumpOnOutOfMemoryError" \
                 --clientHzFile      client-hazelcast.xml \
                 --hzFile            conf/hazelcast.xml \
@@ -12,4 +14,6 @@ coordinator     --workerVmOptions "-ea -server -Xms2G -Xmx2G -XX:+PrintGC -XX:+H
                 test.properties
 
 provisioner --download
+
+provisioner --terminate
 
