3ae16f1e084ed97f819ade1db47cd4d725a2b322
danny <danny@hazelcast.com>
2015-02-24 12:07:29 +0200

    MapPerfMetrics

---
+++ tests/src/main/java/com/hazelcast/stabilizer/tests/map/MapPerfMetrics.java
@@ -0,0 +1,127 @@
+package com.hazelcast.stabilizer.tests.map;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+import java.io.File;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.CsvReporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import com.hazelcast.stabilizer.test.TestContext;
+import com.hazelcast.stabilizer.test.annotations.Run;
+import com.hazelcast.stabilizer.test.annotations.Setup;
+import com.hazelcast.stabilizer.test.annotations.Warmup;
+import com.hazelcast.stabilizer.test.utils.ThreadSpawner;
+
+public class MapPerfMetrics {
+
+    public String cvsDirPath = "~/";
+
+    public String baseMapName = "map";
+
+    public int threadCount = 8;
+    public int totalMaps = 10;
+    public int totalKeys = 10;
+    public int valueByteArraySize = 1000;
+
+    public double putProb= 1.0;
+    public double getProb= 0.0;
+    public double setProb= 0.0;
+
+    public int reportSecondsInterval=5;
+
+    private File cvsDir;
+    private byte[] value;
+    private TestContext testContext;
+    private HazelcastInstance targetInstance;
+
+    @Setup
+    public void setup(TestContext testContext) throws Exception {
+        this.testContext = testContext;
+        targetInstance = testContext.getTargetInstance();
+
+        value = new byte[valueByteArraySize];
+        Random random = new Random();
+        random.nextBytes(value);
+
+        cvsDir = new File(cvsDirPath);
+    }
+
+    @Warmup(global = true)
+    public void warmup() throws Exception {
+        for(int m =0; m<totalMaps; m++){
+            IMap map = targetInstance.getMap(baseMapName+m);
+            for (int k = 0; k < totalKeys; k++) {
+                map.put(k, value);
+            }
+        }
+    }
+
+    @Run
+    public void run() {
+
+        MetricRegistry metrics = new MetricRegistry();
+
+        ThreadSpawner spawner = new ThreadSpawner(testContext.getTestId());
+        for (int k = 0; k < threadCount; k++) {
+            spawner.spawn(new LoadProducer(metrics));
+        }
+
+
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics).build();
+        CsvReporter csv = CsvReporter.forRegistry(metrics).build(cvsDir);
+
+
+        csv.report(reportSecondsInterval, TimeUnit.SECONDS);
+
+        spawner.awaitCompletion();
+
+        reporter.report();
+    }
+
+    private class LoadProducer implements Runnable {
+
+        private Random random = new Random();
+        private MetricRegistry metrics;
+        private Timer getTimer;
+        private Timer putTimer;
+        private Timer setTimer;
+
+        public LoadProducer(MetricRegistry metrics){
+            this.metrics = metrics;
+            getTimer = metrics.timer("getTimer");
+            putTimer = metrics.timer("putTimer");
+            setTimer = metrics.timer("setTimer");
+        }
+
+        public void run() {
+            while (!testContext.isStopped()) {
+
+                IMap map = targetInstance.getMap(baseMapName+random.nextInt(totalMaps));
+
+                double chance = random.nextDouble();
+
+                if((chance -= putProb)<=0) {
+                    Timer.Context context =  putTimer.time();
+                    Object o = map.put(random.nextInt(totalKeys), value);
+                    context.stop();
+
+                }else if((chance-=getProb)<=0){
+                    Timer.Context context =  getTimer.time();
+                    Object o = map.get(random.nextInt(totalKeys));
+                    context.stop();
+
+                }else{
+                    Timer.Context context =  setTimer.time();
+                    map.set(random.nextInt(totalKeys), value);
+                    context.stop();
+                }
+            }
+        }
+    }
+}
