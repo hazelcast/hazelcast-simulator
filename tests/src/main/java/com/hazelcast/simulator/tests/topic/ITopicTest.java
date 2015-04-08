be6a28d8402fd2444a2c50e3299f0b26ea78fe5c
Donnerbart <github@thunderphreak.de>
2015-04-08 03:16:04 +0200

    Refactored test for ITopic. Added configuration and feature check for reliable ITopic (backed by Ringbuffer).

--- tests/src/main/java/com/hazelcast/simulator/tests/topic/ITopicTest.java
+++ tests/src/main/java/com/hazelcast/simulator/tests/topic/ITopicTest.java
@@ -9,35 +9,37 @@ import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
 import com.hazelcast.simulator.test.TestContext;
 import com.hazelcast.simulator.test.TestRunner;
-import com.hazelcast.simulator.test.annotations.Performance;
-import com.hazelcast.simulator.test.annotations.Run;
+import com.hazelcast.simulator.test.annotations.RunWithWorker;
 import com.hazelcast.simulator.test.annotations.Setup;
 import com.hazelcast.simulator.test.annotations.Teardown;
 import com.hazelcast.simulator.test.annotations.Verify;
 import com.hazelcast.simulator.utils.AssertTask;
-import com.hazelcast.simulator.utils.ThreadSpawner;
+import com.hazelcast.simulator.worker.tasks.AbstractMonotonicWorker;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
 
 import static com.hazelcast.simulator.utils.CommonUtils.sleepRandomNanos;
 import static com.hazelcast.simulator.utils.TestUtils.assertTrueEventually;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Tests the ITopic. It test creates a number of topic and a number of listeners per topic and each member
- * publishes messages to every topic.
+ * Tests the {@link ITopic}. It creates a number of topics and a number of listeners per topic and each member publishes messages
+ * to every topic.
  *
- * This test is inherently unreliable because the ITopic relies on the event system which is unreliable.
- * When messages are published with a too high rate, eventually the event system will drop incoming events.
+ * With the default properties this test is inherently unreliable because the {@link ITopic} relies on the event system which is
+ * unreliable. When messages are published with a too high rate, eventually the event system will drop incoming events.
+ *
+ * You can configure this test with reliable configuration (backed by a ringbuffer) by changing the basename to match the wildcard
+ * <tt>ReliableITopic*</tt>.
  */
 public class ITopicTest {
 
     private static final ILogger LOGGER = Logger.getLogger(ITopicTest.class);
 
     // properties
+    public String basename = ITopicTest.class.getSimpleName();
     public int topicCount = 1000;
     public int threadCount = 5;
     public int listenersPerTopic = 1;
@@ -45,33 +47,33 @@ public class ITopicTest {
     public int performanceUpdateFrequency = 100000;
     public int maxProcessingDelayNanos = 0;
     public int maxPublicationDelayNanos = 1000;
-    // the maximum period the verification process is going to wait till the correct number of messags
-    // have been received. A negative value indicates that no verification should be done.
+    // the maximum period the verification process is going to wait till the correct number of messages have been received
+    // a negative value indicates that no verification should be done
     public int maxVerificationTimeSeconds = 60;
-    public String basename = "topic";
+
+    @SuppressWarnings("unchecked")
+    private final ITopic<Long>[] topics = new ITopic[topicCount];
+    private final List<TopicListener> listeners = new LinkedList<TopicListener>();
 
     private IAtomicLong totalExpectedCounter;
     private IAtomicLong totalFoundCounter;
-    private ITopic[] topics;
-    private AtomicLong operations = new AtomicLong();
-    private TestContext testContext;
-    private HazelcastInstance hz;
-    private List<TopicListener> listeners;
 
     @Setup
-    public void setup(TestContext testContext) throws Exception {
-        this.testContext = testContext;
-        hz = testContext.getTargetInstance();
+    public void setUp(TestContext testContext) throws Exception {
+        checkForRingbufferFeature();
 
-        totalExpectedCounter = hz.getAtomicLong(testContext.getTestId() + ":TotalExpectedCounter");
-        totalFoundCounter = hz.getAtomicLong(testContext.getTestId() + ":TotalFoundCounter");
-        topics = new ITopic[topicCount];
-        listeners = new LinkedList<TopicListener>();
-        for (int k = 0; k < topics.length; k++) {
-            ITopic<Long> topic = hz.getTopic(basename + "-" + testContext.getTestId() + "-" + k);
-            topics[k] = topic;
+        HazelcastInstance hz = testContext.getTargetInstance();
+        String testId = testContext.getTestId();
 
-            for (int l = 0; l < listenersPerTopic; l++) {
+        totalExpectedCounter = hz.getAtomicLong(testId + ":TotalExpectedCounter");
+        totalFoundCounter = hz.getAtomicLong(testId + ":TotalFoundCounter");
+
+        String topicBaseName = basename + "-" + testId + "-";
+        for (int i = 0; i < topics.length; i++) {
+            ITopic<Long> topic = hz.getTopic(topicBaseName + i);
+            topics[i] = topic;
+
+            for (int j = 0; j < listenersPerTopic; j++) {
                 TopicListener topicListener = new TopicListener();
                 topic.addMessageListener(topicListener);
                 listeners.add(topicListener);
@@ -79,13 +81,33 @@ public class ITopicTest {
         }
     }
 
-    @Run
-    public void run() {
-        ThreadSpawner spawner = new ThreadSpawner(testContext.getTestId());
-        for (int k = 0; k < threadCount; k++) {
-            spawner.spawn(new Worker());
+    private void checkForRingbufferFeature() {
+        Class classType = null;
+        try {
+            classType = Class.forName("com.hazelcast.config.RingbufferConfig");
+        } catch (ClassNotFoundException e) {
+            LOGGER.warning("Feature is not enabled in this version of Hazelcast!", e);
         }
-        spawner.awaitCompletion();
+
+        try {
+            if (classType != null) {
+                int capacity = (Integer) classType.getDeclaredField("DEFAULT_CAPACITY").get(null);
+                LOGGER.info(basename + ": Ringbuffer capacity: " + capacity);
+            }
+        } catch (NoSuchFieldException e) {
+            LOGGER.severe("Could not find expected field!", e);
+        } catch (IllegalAccessException e) {
+            LOGGER.severe("Could not read expected field!", e);
+        }
+    }
+
+    @Teardown
+    public void tearDown() throws Exception {
+        for (ITopic topic : topics) {
+            topic.destroy();
+        }
+        totalExpectedCounter.destroy();
+        totalFoundCounter.destroy();
     }
 
     @Verify(global = true)
@@ -95,68 +117,49 @@ public class ITopicTest {
         }
 
         final long expectedCount = totalExpectedCounter.get();
-
         assertTrueEventually(new AssertTask() {
             @Override
             public void run() throws Exception {
                 long actualCount = 0;
                 for (TopicListener topicListener : listeners) {
-                    actualCount += topicListener.count;
+                    actualCount += topicListener.receivedMessages;
                 }
-                assertEquals("published messages don't match received messages", expectedCount, actualCount);
+                assertEquals("Number of published messages don't match received messages", expectedCount, actualCount);
             }
         }, maxVerificationTimeSeconds);
     }
 
-    @Teardown
-    public void teardown() throws Exception {
-        for (ITopic topic : topics) {
-            topic.destroy();
-        }
-        totalExpectedCounter.destroy();
-        totalFoundCounter.destroy();
+    @RunWithWorker
+    public Worker createWorker() {
+        return new Worker();
     }
 
-    @Performance
-    public long getOperationCount() {
-        return operations.get();
-    }
+    private class Worker extends AbstractMonotonicWorker {
 
-    private class Worker implements Runnable {
-        private final Random random = new Random();
+        private long publishedMessages;
 
         @Override
-        public void run() {
-            long iteration = 0;
-            long count = 0;
-            while (!testContext.isStopped()) {
-                sleepRandomNanos(random, maxPublicationDelayNanos);
+        protected void timeStep() {
+            sleepRandomNanos(getRandom(), maxPublicationDelayNanos);
 
-                ITopic topic = getRandomTopic();
+            long msg = nextMessage();
+            getRandomTopic().publish(msg);
 
-                long msg = nextMessage();
-                count += msg;
-                topic.publish(msg);
-
-                iteration++;
-                if (iteration % logFrequency == 0) {
-                    LOGGER.info(Thread.currentThread().getName() + " At iteration: " + iteration);
-                }
-                if (iteration % performanceUpdateFrequency == 0) {
-                    operations.addAndGet(performanceUpdateFrequency);
-                }
-            }
-            operations.addAndGet(iteration % performanceUpdateFrequency);
-            totalExpectedCounter.addAndGet(count);
+            publishedMessages += msg;
         }
 
-        private ITopic getRandomTopic() {
-            int index = random.nextInt(topics.length);
+        @Override
+        protected void afterRun() {
+            totalExpectedCounter.addAndGet(publishedMessages);
+        }
+
+        private ITopic<Long> getRandomTopic() {
+            int index = randomInt(topics.length);
             return topics[index];
         }
 
         private long nextMessage() {
-            long msg = random.nextLong() % 1000;
+            long msg = getRandom().nextLong() % 1000;
             if (msg < 0) {
                 msg = -msg;
             }
@@ -165,13 +168,15 @@ public class ITopicTest {
     }
 
     private class TopicListener implements MessageListener<Long> {
+
         private final Random random = new Random();
-        private volatile long count;
+
+        private long receivedMessages;
 
         @Override
         public void onMessage(Message<Long> message) {
             sleepRandomNanos(random, maxProcessingDelayNanos);
-            count += message.getMessageObject();
+            receivedMessages += message.getMessageObject();
         }
     }
 
