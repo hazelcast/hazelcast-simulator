37de9de21b3689580ffb68fbdc0fc1b3c6c21cb3
Donnerbart <github@thunderphreak.de>
2015-04-27 14:34:53 +0200

    Increased code coverage of CommonUtils.

--- utils/src/test/java/com/hazelcast/simulator/utils/CommonUtils_SystemExitTest.java
+++ utils/src/test/java/com/hazelcast/simulator/utils/CommonUtils_SystemExitTest.java
@@ -3,32 +3,41 @@ package com.hazelcast.simulator.utils;
 import com.hazelcast.simulator.utils.helper.ExitExceptionSecurityManager;
 import com.hazelcast.simulator.utils.helper.ExitStatusOneException;
 import org.apache.log4j.Logger;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static com.hazelcast.simulator.utils.CommonUtils.exitWithError;
 
 public class CommonUtils_SystemExitTest {
 
-    private SecurityManager oldSecurityManager;
-    private Logger logger;
+    private static SecurityManager oldSecurityManager;
 
-    @Before
-    public void setUp() {
+    private final Logger logger = Logger.getLogger(CommonUtils_SystemExitTest.class.getSimpleName());
+
+    @BeforeClass
+    public static void setUp() {
         oldSecurityManager = System.getSecurityManager();
         System.setSecurityManager(new ExitExceptionSecurityManager());
-
-        logger = Logger.getLogger(CommonUtils_SystemExitTest.class.getSimpleName());
     }
 
-    @After
-    public void tearDown() {
+    @AfterClass
+    public static void tearDown() {
         System.setSecurityManager(oldSecurityManager);
     }
 
     @Test(expected = ExitStatusOneException.class)
-    public void testExitWithErrorWithThrowable() {
+    public void testExitWithError() {
+        exitWithError();
+    }
+
+    @Test(expected = ExitStatusOneException.class)
+    public void testExitWithError_withCommandLineExitException() {
+        exitWithError(logger, "expected failure", new CommandLineExitException("expected CommandLineExitException"));
+    }
+
+    @Test(expected = ExitStatusOneException.class)
+    public void testExitWithError_withThrowable() {
         exitWithError(logger, "expected failure", new Throwable("expected throwable"));
     }
 }
