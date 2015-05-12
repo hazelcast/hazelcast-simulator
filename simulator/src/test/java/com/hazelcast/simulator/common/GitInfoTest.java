6dde8d581d726021da748f499628911e2905abf8
Donnerbart <github@thunderphreak.de>
2015-05-12 17:15:52 +0200

    Fixed NPE in GitInfo when properties file is missing. Increased code coverage.

--- simulator/src/test/java/com/hazelcast/simulator/common/GitInfoTest.java
+++ simulator/src/test/java/com/hazelcast/simulator/common/GitInfoTest.java
@@ -2,7 +2,12 @@ package com.hazelcast.simulator.common;
 
 import org.junit.Test;
 
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class GitInfoTest {
 
@@ -29,5 +34,32 @@ public class GitInfoTest {
     @Test
     public void testGetRemoteOriginUrl() throws Exception {
         assertNotNull(GitInfo.getRemoteOriginUrl());
+    }
+
+    @Test
+    public void testLoadProperties() {
+        Properties properties = GitInfo.loadGitProperties(GitInfo.GIT_INFO_FILE);
+        assertNotNull(properties);
+        assertFalse(properties instanceof GitInfo.DummyProperties);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testLoadProperties_null() {
+        GitInfo.loadGitProperties(null);
+    }
+
+    @Test
+    public void testLoadProperties_notExists() {
+        Properties properties = GitInfo.loadGitProperties("notExists");
+        assertNotNull(properties);
+        assertTrue(properties instanceof GitInfo.DummyProperties);
+
+        assertEquals(GitInfo.UNKNOWN, properties.getProperty(GitInfo.GIT_COMMIT_ID_AABREV));
+        assertEquals(GitInfo.UNKNOWN, properties.getProperty(GitInfo.GIT_COMMIT_ID));
+        assertEquals(GitInfo.UNKNOWN, properties.getProperty(GitInfo.GIT_COMMIT_TIME));
+        assertEquals(GitInfo.UNKNOWN, properties.getProperty(GitInfo.GIT_BUILD_TIME));
+        assertEquals(GitInfo.UNKNOWN, properties.getProperty(GitInfo.GIT_REMOTE_ORIGIN_URL));
+
+        assertEquals("default", properties.getProperty(GitInfo.GIT_COMMIT_ID_AABREV, "default"));
     }
 }
