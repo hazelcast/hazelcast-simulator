6bda4cce46ebe2c3944962b6e11c67a4c636db74
Donnerbart <github@thunderphreak.de>
2015-05-17 04:00:17 +0200

    Ensure shutdown of all Hazelcast instances after TestRunner tests.

--- tests/src/test/java/com/hazelcast/simulator/tests/map/MapRaceTestTest.java
+++ tests/src/test/java/com/hazelcast/simulator/tests/map/MapRaceTestTest.java
@@ -1,9 +1,16 @@
 package com.hazelcast.simulator.tests.map;
 
+import com.hazelcast.core.Hazelcast;
 import com.hazelcast.simulator.test.TestRunner;
+import org.junit.After;
 import org.junit.Test;
 
 public class MapRaceTestTest {
+
+    @After
+    public void tearDown() {
+        Hazelcast.shutdownAll();
+    }
 
     @Test(expected = AssertionError.class)
     public void test() throws Exception {
