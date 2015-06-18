171cdd4007bdf184e68f282c33230206cb1872d1
Donnerbart <github@thunderphreak.de>
2015-06-19 00:54:04 +0300

    Added tests for external clients.

---
+++ tests/src/main/java/com/hazelcast/simulator/tests/external/ExternalClientResponseTest.java
@@ -0,0 +1,35 @@
+package com.hazelcast.simulator.tests.external;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IAtomicLong;
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
+import com.hazelcast.simulator.test.TestContext;
+import com.hazelcast.simulator.test.annotations.Run;
+import com.hazelcast.simulator.test.annotations.Setup;
+
+import static com.hazelcast.simulator.utils.CommonUtils.sleepSeconds;
+
+public class ExternalClientResponseTest {
+
+    private static final ILogger LOGGER = Logger.getLogger(ExternalClientResponseTest.class);
+
+    // properties
+    public String basename = "externalClientsFinished";
+    public int delaySeconds = 60;
+
+    private IAtomicLong clientsFinished;
+
+    @Setup
+    public void setUp(TestContext testContext) throws Exception {
+        HazelcastInstance hazelcastInstance = testContext.getTargetInstance();
+        this.clientsFinished = hazelcastInstance.getAtomicLong(basename);
+    }
+
+    @Run
+    public void run() {
+        sleepSeconds(delaySeconds);
+        clientsFinished.incrementAndGet();
+        LOGGER.info("Client response sent!");
+    }
+}
