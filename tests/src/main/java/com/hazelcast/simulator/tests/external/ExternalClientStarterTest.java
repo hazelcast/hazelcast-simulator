c517f73a49f279757da2ea73a833ba4e97d3e93c
Donnerbart <github@thunderphreak.de>
2015-06-19 22:42:21 +0200

    Added test to start external client.

---
+++ tests/src/main/java/com/hazelcast/simulator/tests/external/ExternalClientStarterTest.java
@@ -0,0 +1,37 @@
+package com.hazelcast.simulator.tests.external;
+
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
+import com.hazelcast.simulator.common.SimulatorProperties;
+import com.hazelcast.simulator.provisioner.Bash;
+import com.hazelcast.simulator.test.TestContext;
+import com.hazelcast.simulator.test.annotations.Run;
+import com.hazelcast.simulator.test.annotations.Setup;
+
+import java.io.File;
+
+import static com.hazelcast.simulator.utils.FileUtils.deleteQuiet;
+
+public class ExternalClientStarterTest {
+
+    private static final ILogger LOGGER = Logger.getLogger(ExternalClientStarterTest.class);
+
+    // properties
+    public String binaryName = "binaryName";
+    public String arguments = "";
+
+    private final SimulatorProperties props = new SimulatorProperties();
+    private final Bash bash = new Bash(props);
+
+    @Setup
+    public void setUp(TestContext testContext) throws Exception {
+        // delete the local binary, so it won't get downloaded
+        deleteQuiet(new File(binaryName));
+    }
+
+    @Run
+    public void run() {
+        LOGGER.info("Starting external client: " + binaryName);
+        bash.execute("../upload/" + binaryName + " " + arguments + " &");
+    }
+}
