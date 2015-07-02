77b553bd15747aa42ee817c842b3f946a628f87c
Donnerbart <github@thunderphreak.de>
2015-07-02 13:02:40 +0200

    Pulled out some methods from ExternalClientTest.

--- tests/src/main/java/com/hazelcast/simulator/tests/external/ExternalClientTest.java
+++ tests/src/main/java/com/hazelcast/simulator/tests/external/ExternalClientTest.java
@@ -2,7 +2,6 @@ package com.hazelcast.simulator.tests.external;
 
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.ICountDownLatch;
-import com.hazelcast.core.IList;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
 import com.hazelcast.simulator.probes.probes.IntervalProbe;
@@ -17,10 +16,11 @@ import com.hazelcast.util.EmptyStatement;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
+import static com.hazelcast.simulator.tests.external.ExternalClientHelper.getLatencyResults;
+import static com.hazelcast.simulator.tests.external.ExternalClientHelper.getThroughputResults;
+import static com.hazelcast.simulator.tests.external.ExternalClientHelper.setCountDownLatch;
 import static com.hazelcast.simulator.tests.helpers.HazelcastTestUtils.isMemberNode;
-import static com.hazelcast.simulator.utils.CommonUtils.sleepSeconds;
 import static java.lang.String.format;
-import static org.junit.Assert.fail;
 
 public class ExternalClientTest {
 
@@ -31,34 +31,25 @@ public class ExternalClientTest {
     public int waitForClientsCount = 0;
     public int waitIntervalSeconds = 60;
     public int expectedResultSize = 0;
-    public int expectedResultsMaxRetries = 60;
 
     SimpleProbe externalClientThroughput;
     IntervalProbe<HdrLatencyDistributionResult, HdrLatencyDistributionProbe> externalClientLatency;
 
-    private TestContext testContext;
     private HazelcastInstance hazelcastInstance;
     private boolean isExternalResultsCollectorInstance;
     private ICountDownLatch clientsRunning;
 
     @Setup
     public void setUp(TestContext testContext) throws Exception {
-        this.testContext = testContext;
         hazelcastInstance = testContext.getTargetInstance();
 
         if (isMemberNode(hazelcastInstance)) {
             return;
         }
 
-        if (waitForClientsCount < 1) {
-            fail("waitForClientsCount must be positive, but was: " + waitForClientsCount);
-        }
-
+        // init ICountDownLatch with waitForClientsCount
         clientsRunning = hazelcastInstance.getCountDownLatch(basename);
-        if (!clientsRunning.trySetCount(waitForClientsCount)) {
-            LOGGER.severe("Could not set ICountDownLatch value of " + waitForClientsCount);
-            fail("Could not set ICountDownLatch value of " + waitForClientsCount);
-        }
+        setCountDownLatch(clientsRunning, waitForClientsCount);
 
         // determine one instance per cluster
         if (hazelcastInstance.getMap(basename).putIfAbsent(basename, true) == null) {
@@ -99,97 +90,13 @@ public class ExternalClientTest {
             externalClientLatency.disable();
 
             LOGGER.info("Stopping non result collecting ExternalClientTest");
-            testContext.stop();
             return;
         }
 
         // get probe results
         LOGGER.info("Collecting results from external clients...");
-        getThroughputResults();
-        getLatencyResults();
+        getThroughputResults(hazelcastInstance, externalClientThroughput, expectedResultSize);
+        getLatencyResults(hazelcastInstance, externalClientLatency, expectedResultSize);
         LOGGER.info("Result collecting ExternalClientTest done!");
-
-        testContext.stop();
-    }
-
-    private void getThroughputResults() {
-        IList<String> throughputResults = getResultList("throughput", "externalClientsThroughputResults");
-        int resultSize = throughputResults.size();
-
-        LOGGER.info(format("Collecting %d throughput results (expected %d)...", resultSize, expectedResultSize));
-        int totalInvocations = 0;
-        double totalDuration = 0;
-        for (String throughputString : throughputResults) {
-            String[] throughput = throughputString.split("\\|");
-            int operationCount = Integer.parseInt(throughput[0]);
-            long duration = TimeUnit.NANOSECONDS.toMillis(Long.parseLong(throughput[1]));
-
-            String publisherId = "n/a";
-            if (throughput.length > 2) {
-                publisherId = throughput[2];
-            }
-            LOGGER.info(format("External client executed %d operations in %d ms (%s)", operationCount, duration, publisherId));
-
-            totalInvocations += operationCount;
-            totalDuration += duration;
-        }
-        LOGGER.info("Done!");
-
-        if (resultSize == 0 || totalInvocations == 0 || totalDuration == 0) {
-            LOGGER.info(format("No valid throughput probe data collected! results: %d, totalInvocations: %d, totalDuration: %.0f",
-                    resultSize, totalInvocations, totalDuration));
-            return;
-        }
-
-        long avgDuration = Math.round(totalDuration / resultSize);
-        externalClientThroughput.setValues(avgDuration, totalInvocations);
-        double performance = ((double) totalInvocations / avgDuration) * 1000;
-        LOGGER.info(format("All external clients executed %d operations in %d ms (%.3f ops/s)",
-                totalInvocations, avgDuration, performance));
-    }
-
-    private void getLatencyResults() {
-        IList<String> latencyLists = getResultList("latency", "externalClientsLatencyResults");
-
-        LOGGER.info(format("Collecting %d latency result lists...", latencyLists.size()));
-        for (String key : latencyLists) {
-            IList<Long> values = hazelcastInstance.getList(key);
-            LOGGER.info(format("Adding %d latency results...", values.size()));
-            for (Long latency : values) {
-                externalClientLatency.recordValue(latency);
-            }
-        }
-        LOGGER.info("Done!");
-    }
-
-    private IList<String> getResultList(String typeName, String listName) {
-        int lastSize = 0;
-        int retries = 0;
-        IList<String> resultList = hazelcastInstance.getList(listName);
-
-        // wait for all throughput results to arrive
-        while (expectedResultSize > 0) {
-            int listSize = resultList.size();
-
-            // check if we should stop
-            if (listSize >= expectedResultSize) {
-                break;
-            }
-
-            // check if there is progress
-            if (lastSize == listSize) {
-                retries++;
-            } else {
-                retries = 0;
-            }
-            if (retries > expectedResultsMaxRetries) {
-                break;
-            }
-            lastSize = listSize;
-
-            LOGGER.info(format("Waiting for %d/%d %s results...", resultList.size(), expectedResultSize, typeName));
-            sleepSeconds(1);
-        }
-        return resultList;
     }
 }
