879d0b7b4db6a9913f1123a045546859b098d33d
alarmnummer <peter.veentjer@hazelcast.com>
2015-08-22 18:42:30 +0300

    Converted to executeTx to see if problem still happens

--- tests/src/main/java/com/hazelcast/simulator/tests/map/MapTransactionContextTest.java
+++ tests/src/main/java/com/hazelcast/simulator/tests/map/MapTransactionContextTest.java
@@ -1,16 +1,12 @@
 package com.hazelcast.simulator.tests.map;
 
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IList;
-import com.hazelcast.core.IMap;
 import com.hazelcast.core.TransactionalMap;
 import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
 import com.hazelcast.simulator.test.TestContext;
 import com.hazelcast.simulator.test.annotations.RunWithWorker;
 import com.hazelcast.simulator.test.annotations.Setup;
-import com.hazelcast.simulator.test.annotations.Verify;
-import com.hazelcast.simulator.test.annotations.Warmup;
 import com.hazelcast.simulator.worker.tasks.AbstractMonotonicWorker;
 import com.hazelcast.transaction.TransactionContext;
 import com.hazelcast.transaction.TransactionException;
@@ -25,33 +21,20 @@ import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.util.concurrent.ThreadLocalRandom;
 
-import static org.junit.Assert.assertEquals;
-
 public class MapTransactionContextTest {
 
-    private static final ILogger LOGGER = Logger.getLogger(MapTransactionTest.class);
+    private static final ILogger LOGGER = Logger.getLogger(MapTransactionContextTest.class);
 
     // properties
-    public String basename = getClass().getSimpleName();
-    public int keyCount = 1000;
     public TransactionType transactionType = TransactionType.TWO_PHASE;
     public int durability = 1;
     public int range = 1000;
 
     private HazelcastInstance hz;
-    private IMap<Integer, Long> map;
 
     @Setup
     public void setup(TestContext testContext) throws Exception {
         hz = testContext.getTargetInstance();
-        map = hz.getMap(basename);
-     }
-
-    @Warmup(global = true)
-    public void warmup() throws Exception {
-        for (int i = 0; i < keyCount; i++) {
-            map.put(i, 0L);
-        }
     }
 
     @RunWithWorker
@@ -63,40 +46,28 @@ public class MapTransactionContextTest {
 
         @Override
         protected void timeStep() {
-            int key = nextRandom(0, range / 2);
+            TransactionOptions transactionOptions = new TransactionOptions().setTransactionType(transactionType).setDurability(durability);
 
-            TransactionOptions txOpts = new TransactionOptions().setTransactionType(transactionType).setDurability(durability);
+            hz.executeTransaction(transactionOptions, new TransactionalTask<Object>() {
+                @Override
+                public Object execute(TransactionalTaskContext context) throws TransactionException {
+                    int key = nextRandom(0, range / 2);
 
-            TransactionContext tCtx = hz.newTransactionContext(txOpts);
+                    TransactionalMap<Object, Object> txMap = context.getMap("map");
 
-            tCtx.beginTransaction();
+                    Object val = txMap.getForUpdate(key);
 
-            TransactionalMap<Object, Object> txMap = tCtx.getMap("map");
+                    if (val != null) {
+                        key = nextRandom(range / 2, range);
+                    }
 
-            try {
-                Object val = txMap.getForUpdate(key);
+                    txMap.put(key, new SampleValue(key));
 
-                if (val != null) {
-                    key = nextRandom(range / 2, range);
+                    return null;
                 }
-
-                txMap.put(key, new SampleValue(key));
-
-                tCtx.commitTransaction();
-            } catch (Exception e) {
-//                System.out.println(cfg, "Yardstick transaction will be rollback.");
-//
-//                e.printStackTrace(cfg.error());
-
-                e.printStackTrace();
-
-                tCtx.rollbackTransaction();
-            }
+            });
         }
 
-        protected int nextRandom(int max) {
-            return ThreadLocalRandom.current().nextInt(max);
-        }
         protected int nextRandom(int min, int max) {
             return ThreadLocalRandom.current().nextInt(max - min) + min;
         }
@@ -128,29 +99,28 @@ public class MapTransactionContextTest {
             return id;
         }
 
-        /** {@inheritDoc} */
-        @Override public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
             id = in.readInt();
         }
 
-        /** {@inheritDoc} */
-        @Override public void writeExternal(ObjectOutput out) throws IOException {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
             out.writeInt(id);
         }
 
-        /** {@inheritDoc} */
-        @Override public String toString() {
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String toString() {
             return "Value [id=" + id + ']';
         }
     }
-
-
-    /*
-   @Override public boolean test(Map<Object, Object> ctx) throws Exception {
-        int key = nextRandom(0, args.range() / 2);
-
-        // Repeatable read isolation level is always used.
-
-    }
-     */
 }
