99350065d6d3597a2224a25ef6c393201efee0e8
Donnerbart <github@thunderphreak.de>
2015-08-28 11:59:42 +0200

    Created constants to improve readability of SimulatorFrameDecoder.

--- simulator/src/main/java/com/hazelcast/simulator/protocol/handler/SimulatorFrameDecoder.java
+++ simulator/src/main/java/com/hazelcast/simulator/protocol/handler/SimulatorFrameDecoder.java
@@ -9,7 +9,11 @@ import static com.hazelcast.simulator.protocol.core.BaseCodec.INT_SIZE;
  */
 public class SimulatorFrameDecoder extends LengthFieldBasedFrameDecoder {
 
+    private static final int MAX_FRAME_SIZE = Integer.MAX_VALUE;
+    private static final int LENGTH_FIELD_OFFSET = 0;
+    private static final int LENGTH_FIELD_SIZE = INT_SIZE;
+
     public SimulatorFrameDecoder() {
-        super(Integer.MAX_VALUE, 0, INT_SIZE);
+        super(MAX_FRAME_SIZE, LENGTH_FIELD_OFFSET, LENGTH_FIELD_SIZE);
     }
 }
