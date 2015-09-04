95f810356eaf5c22505c9702186a50d68b434bd8
alarmnummer <peter.veentjer@hazelcast.com>
2015-09-04 07:44:33 +0300

    age sql will not select anything now

--- tests/src/main/java/com/hazelcast/simulator/tests/map/MapPredicateTest.java
+++ tests/src/main/java/com/hazelcast/simulator/tests/map/MapPredicateTest.java
@@ -18,6 +18,7 @@ import com.hazelcast.simulator.test.annotations.Verify;
 import com.hazelcast.simulator.test.annotations.Warmup;
 import com.hazelcast.simulator.tests.map.helpers.Employee;
 import com.hazelcast.simulator.tests.map.helpers.PredicateOperationCounter;
+
 import com.hazelcast.simulator.worker.loadsupport.Streamer;
 import com.hazelcast.simulator.worker.loadsupport.StreamerFactory;
 import com.hazelcast.simulator.worker.selector.OperationSelectorBuilder;
@@ -27,7 +28,6 @@ import java.util.Collection;
 
 import static java.lang.String.format;
 import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -47,31 +47,12 @@ public class MapPredicateTest {
         DESTROY_MAP
     }
 
-    /**
-     * Used to construct an SQL predicate and to compare the returned values.
-     */
-    private enum Comparison {
-        EQUALS("="),
-        LESS_THAN("<"),
-        GREATER_THAN(">");
-
-        private final String sqlOperator;
-
-        Comparison(String sqlOperator) {
-            this.sqlOperator = sqlOperator;
-        }
-    }
-
     private static final ILogger LOGGER = Logger.getLogger(MapPredicateTest.class);
 
     public String basename = MapPredicateTest.class.getSimpleName();
     public int threadCount = 3;
     public int keyCount = 100;
     public int pageSize = 5;
-
-    public boolean useFixedAge = false;
-    public int fixedAge = 100;
-    public Comparison ageComparison = Comparison.GREATER_THAN;
 
     public double predicateBuilderProb = 0.2;
     public double sqlStringProb = 0.2;
@@ -130,10 +111,10 @@ public class MapPredicateTest {
         private final PredicateOperationCounter operationCounter = new PredicateOperationCounter();
 
         private long lastUpdateMs = System.currentTimeMillis();
-        private long spentTimeMs = 0;
+        private long iterationsLastMinute = 0;
         private long maxLastMinute = Long.MIN_VALUE;
         private long minLastMinute = Long.MAX_VALUE;
-        private long iterationsLastMinute = 0;
+        private long spendTimeMs = 0;
 
         public Worker() {
             super(operationSelectorBuilder);
@@ -145,36 +126,36 @@ public class MapPredicateTest {
         public void timeStep(Operation operation) {
             long startMs = System.currentTimeMillis();
 
-            switch (operation) {
-                case PREDICATE_BUILDER:
-                    predicateBuilder();
-                    break;
-                case SQL_STRING:
-                    sqlString();
-                    break;
-                case PAGING_PREDICATE:
-                    pagingPredicate();
-                    break;
-                case UPDATE_EMPLOYEE:
-                    updateEmployee();
-                    break;
-                case DESTROY_MAP:
-                    destroyMap();
-                    break;
-                default:
-                    throw new UnsupportedOperationException();
-            }
+//            switch (operation) {
+//                case PREDICATE_BUILDER:
+//                    predicateBuilder();
+//                    break;
+//                case SQL_STRING:
+                   sqlString();
+//                    break;
+//                case PAGING_PREDICATE:
+//                    pagingPredicate();
+//                    break;
+//                case UPDATE_EMPLOYEE:
+//                    updateEmployee();
+//                    break;
+//                case DESTROY_MAP:
+//                    destroyMap();
+//                    break;
+//                default:
+//                    throw new UnsupportedOperationException();
+//            }
 
             long nowMs = System.currentTimeMillis();
             long durationMs = nowMs - startMs;
-            spentTimeMs += durationMs;
             maxLastMinute = Math.max(durationMs, maxLastMinute);
             minLastMinute = Math.min(durationMs, minLastMinute);
             iterationsLastMinute++;
+            spendTimeMs += durationMs;
 
             if (lastUpdateMs + SECONDS.toMillis(60) < nowMs) {
-                double avg = spentTimeMs / (double) iterationsLastMinute;
-                double perf = (iterationsLastMinute * 1000d) / (double) spentTimeMs;
+                double avg = spendTimeMs / (double) iterationsLastMinute;
+                double perf = (iterationsLastMinute * 1000d) / (double) spendTimeMs;
 
                 LOGGER.info(format("last minute: iterations=%d, min=%d ms, max=%d ms, avg=%.2f ms, perf=%.2f predicates/second",
                         iterationsLastMinute, minLastMinute, maxLastMinute, avg, perf));
@@ -192,34 +173,33 @@ public class MapPredicateTest {
         }
 
         private void predicateBuilder() {
-            int age = (useFixedAge ? fixedAge : randomInt(Employee.MAX_AGE));
+            int age = randomInt(Employee.MAX_AGE);
             String name = Employee.getRandomName();
 
-            // TODO: still broken because it relies on reflection which is dog slow, so we need an explicit AgeNamePredicate
+            // TODO: Still broken because it relies on reflection which is dog slow, so we need an explicit AgeNamePredicate
             EntryObject entryObject = new PredicateBuilder().getEntryObject();
-            Predicate agePredicate = getPredicate(entryObject, "age", age, ageComparison);
+            Predicate agePredicate = entryObject.get("age").equal(age);
             Predicate ageNamePredicate = entryObject.get("name").equal(name).and(agePredicate);
 
             Collection<Employee> employees = map.values(ageNamePredicate);
             for (Employee emp : employees) {
-                String assertMessage = basename + ": " + emp + " not matching " + ageNamePredicate;
-                assertInteger(assertMessage, age, emp.getAge(), ageComparison);
-                assertEquals(assertMessage, name, emp.getName());
+                assertTrue(basename + ": " + emp + " not matching " + ageNamePredicate, emp.getAge() == age);
+                assertTrue(basename + ": " + emp + " not matching " + ageNamePredicate, emp.getName().equals(name));
             }
             operationCounter.predicateBuilderCount++;
         }
 
         private void sqlString() {
             boolean active = getRandom().nextBoolean();
-            int age = (useFixedAge ? fixedAge : randomInt(Employee.MAX_AGE));
+            int age = -1;//randomInt(Employee.MAX_AGE);
 
-            SqlPredicate predicate = new SqlPredicate("active=" + active + " AND age" + ageComparison.sqlOperator + age);
-
+           // SqlPredicate predicate = new SqlPredicate("active=" + active + " AND age =" + age);
+            SqlPredicate predicate = new SqlPredicate("age = 100000");
             Collection<Employee> employees = map.values(predicate);
+
             for (Employee emp : employees) {
-                String assertMessage = basename + ": " + emp + " not matching " + predicate;
-                assertEquals(assertMessage, active, emp.isActive());
-                assertInteger(assertMessage, age, emp.getAge(), ageComparison);
+                assertTrue(basename + ": " + emp + " not matching " + predicate, emp.isActive() == active);
+                assertTrue(basename + ": " + emp + " not matching " + predicate, emp.getAge() > age);
             }
             operationCounter.sqlStringCount++;
         }
@@ -256,35 +236,6 @@ public class MapPredicateTest {
             map.destroy();
             initMap();
             operationCounter.destroyCount++;
-        }
-    }
-
-    private static Predicate getPredicate(EntryObject entryObject, String fieldName, int value, Comparison comparison) {
-        switch (comparison) {
-            case EQUALS:
-                return entryObject.get(fieldName).equal(value);
-            case LESS_THAN:
-                return entryObject.get(fieldName).lessThan(value);
-            case GREATER_THAN:
-                return entryObject.get(fieldName).greaterThan(value);
-            default:
-                throw new UnsupportedOperationException("Unsupported comparison: " + comparison);
-        }
-    }
-
-    private static void assertInteger(String message, int expected, int actual, Comparison comparison) {
-        switch (comparison) {
-            case EQUALS:
-                assertEquals(message, expected, actual);
-                break;
-            case LESS_THAN:
-                assertTrue(message, actual < expected);
-                break;
-            case GREATER_THAN:
-                assertTrue(message, actual > expected);
-                break;
-            default:
-                throw new UnsupportedOperationException("Unsupported comparison: " + comparison);
         }
     }
 }
