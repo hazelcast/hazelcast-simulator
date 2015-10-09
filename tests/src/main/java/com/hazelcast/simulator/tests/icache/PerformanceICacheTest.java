872975a73eba9a5002575cf6bf901cd1fe1c7e94
Donnerbart <github@thunderphreak.de>
2015-10-09 14:47:34 +0200

    Cleanup of PerformanceICacheTest.

--- tests/src/main/java/com/hazelcast/simulator/tests/icache/PerformanceICacheTest.java
+++ tests/src/main/java/com/hazelcast/simulator/tests/icache/PerformanceICacheTest.java
@@ -6,22 +6,18 @@ import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
 import com.hazelcast.simulator.test.TestContext;
 import com.hazelcast.simulator.test.TestRunner;
-import com.hazelcast.simulator.test.annotations.Performance;
-import com.hazelcast.simulator.test.annotations.Run;
+import com.hazelcast.simulator.test.annotations.RunWithWorker;
 import com.hazelcast.simulator.test.annotations.Setup;
 import com.hazelcast.simulator.test.annotations.Teardown;
 import com.hazelcast.simulator.test.annotations.Warmup;
-import com.hazelcast.simulator.utils.ThreadSpawner;
 import com.hazelcast.simulator.worker.loadsupport.Streamer;
 import com.hazelcast.simulator.worker.loadsupport.StreamerFactory;
+import com.hazelcast.simulator.worker.selector.OperationSelectorBuilder;
+import com.hazelcast.simulator.worker.tasks.AbstractWorker;
 
 import javax.cache.Cache;
 import javax.cache.CacheException;
 import javax.cache.CacheManager;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
 
 import static com.hazelcast.simulator.tests.icache.helpers.CacheUtils.createCacheManager;
 
@@ -32,31 +28,23 @@ public class PerformanceICacheTest {
 
     private static final ILogger LOGGER = Logger.getLogger(PerformanceICacheTest.class);
 
+    private enum Operation {
+        PUT,
+        GET
+    }
+
     // properties
     public String basename = PerformanceICacheTest.class.getSimpleName();
-    public int threadCount = 10;
     public int keyCount = 1000000;
-    public int logFrequency = 10000;
-    public int performanceUpdateFrequency = 10000;
-    public int writePercentage = 10;
+    public double putProb = 0.1;
+
+    private final OperationSelectorBuilder<Operation> operationSelectorBuilder = new OperationSelectorBuilder<Operation>();
 
     private Cache<Object, Object> cache;
-    private final AtomicLong operations = new AtomicLong();
-    private TestContext testContext;
 
     @Setup
     public void setup(TestContext testContext) throws Exception {
-        if (writePercentage < 0) {
-            throw new IllegalArgumentException("Write percentage can't be smaller than 0");
-        }
-
-        if (writePercentage > 100) {
-            throw new IllegalArgumentException("Write percentage can't be larger than 100");
-        }
-
-        this.testContext = testContext;
         HazelcastInstance hazelcastInstance = testContext.getTargetInstance();
-
         CacheManager cacheManager = createCacheManager(hazelcastInstance);
 
         CacheConfig<Integer, Integer> config = new CacheConfig<Integer, Integer>();
@@ -69,6 +57,9 @@ public class PerformanceICacheTest {
             LOGGER.severe(hack);
         }
         cache = cacheManager.getCache(basename);
+
+        operationSelectorBuilder.addOperation(Operation.PUT, putProb)
+                .addDefaultOperation(Operation.GET);
     }
 
     @Teardown
@@ -79,67 +70,37 @@ public class PerformanceICacheTest {
     @Warmup(global = true)
     public void warmup() throws Exception {
         Streamer<Object, Object> streamer = StreamerFactory.getInstance(cache);
-        for (int k = 0; k < keyCount; k++) {
-            streamer.pushEntry(k, 0);
-
-            if (k % 10000 == 0) {
-                LOGGER.info("Warmup: " + k);
-            }
+        for (int i = 0; i < keyCount; i++) {
+            streamer.pushEntry(i, 0);
         }
         streamer.await();
     }
 
-    @Run
-    public void run() {
-        ThreadSpawner spawner = new ThreadSpawner(testContext.getTestId());
-        for (int k = 0; k < threadCount; k++) {
-            spawner.spawn(new Worker());
+    @RunWithWorker
+    public Worker createWorker() {
+        return new Worker();
+    }
+
+    private class Worker extends AbstractWorker<Operation> {
+
+        private int value;
+
+        public Worker() {
+            super(operationSelectorBuilder);
         }
-        spawner.awaitCompletion();
-    }
-
-    @Performance
-    public long getOperationCount() {
-        return operations.get();
-    }
-
-    private class Worker implements Runnable {
-        private final Random random = new Random();
-        private final Map<Integer, Long> result = new HashMap<Integer, Long>();
 
         @Override
-        public void run() {
-            for (int k = 0; k < keyCount; k++) {
-                result.put(k, 0L);
-            }
-
-            long iteration = 0;
-            while (!testContext.isStopped()) {
-                Integer key = random.nextInt(keyCount);
-                if (shouldWrite(iteration)) {
-                    cache.put(key, (int) iteration);
-                } else {
+        public void timeStep(Operation operation) {
+            Integer key = randomInt(keyCount);
+            switch (operation) {
+                case PUT:
+                    cache.put(key, value++);
+                    break;
+                case GET:
                     cache.get(key);
-                }
-
-                iteration++;
-                if (iteration % logFrequency == 0) {
-                    LOGGER.info(Thread.currentThread().getName() + " At iteration: " + iteration);
-                }
-                if (iteration % performanceUpdateFrequency == 0) {
-                    operations.addAndGet(performanceUpdateFrequency);
-                }
-            }
-            operations.addAndGet(iteration % performanceUpdateFrequency);
-        }
-
-        private boolean shouldWrite(long iteration) {
-            if (writePercentage == 0) {
-                return false;
-            } else if (writePercentage == 100) {
-                return true;
-            } else {
-                return (iteration % 100) < writePercentage;
+                    break;
+                default:
+                    throw new UnsupportedOperationException();
             }
         }
     }
