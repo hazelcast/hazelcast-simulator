38b9ef46a3ffc655966033db40b34a2fe3909a14
Donnerbart <github@thunderphreak.de>
2015-10-09 15:32:40 +0200

    Cleanup of BatchingICacheTest.

--- tests/src/main/java/com/hazelcast/simulator/tests/icache/BatchingICacheTest.java
+++ tests/src/main/java/com/hazelcast/simulator/tests/icache/BatchingICacheTest.java
@@ -9,32 +9,29 @@ import com.hazelcast.logging.Logger;
 import com.hazelcast.simulator.test.TestContext;
 import com.hazelcast.simulator.test.TestException;
 import com.hazelcast.simulator.test.TestRunner;
-import com.hazelcast.simulator.test.annotations.Performance;
-import com.hazelcast.simulator.test.annotations.Run;
+import com.hazelcast.simulator.test.annotations.RunWithWorker;
 import com.hazelcast.simulator.test.annotations.Setup;
 import com.hazelcast.simulator.test.annotations.Teardown;
 import com.hazelcast.simulator.test.annotations.Warmup;
-import com.hazelcast.simulator.utils.ThreadSpawner;
 import com.hazelcast.simulator.worker.loadsupport.Streamer;
 import com.hazelcast.simulator.worker.loadsupport.StreamerFactory;
-import com.hazelcast.simulator.worker.selector.OperationSelector;
 import com.hazelcast.simulator.worker.selector.OperationSelectorBuilder;
+import com.hazelcast.simulator.worker.tasks.AbstractWorker;
 
 import javax.cache.CacheException;
 import javax.cache.CacheManager;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
 
 import static com.hazelcast.simulator.tests.icache.helpers.CacheUtils.createCacheManager;
 
 /**
- * This test demonstrates effect of batching. It uses async methods to invoke operation and wait for future
- * to complete every {@code batchSize} invocations. Hence setting batchSize to 1 is effectively the same as
- * using sync operations.
+ * Demonstrates the effect of batching.
  *
- * {@code batchSize > 1} causes batch-effect to kick-in, pipe-lines are utilized better
+ * It uses async methods to invoke operation and wait for future to complete every {@code batchSize} invocations.
+ * Hence setting {@link #batchSize} to 1 is effectively the same as using sync operations.
+ *
+ * Setting {@link #batchSize} to values greater than 1 causes the batch-effect to kick-in, pipe-lines are utilized better
  * and overall throughput goes up.
  */
 public class BatchingICacheTest {
@@ -47,23 +44,17 @@ public class BatchingICacheTest {
     private static final ILogger LOGGER = Logger.getLogger(PerformanceICacheTest.class);
 
     // properties
-    public int threadCount = 10;
-    public String basename = BatchingICacheTest.class.getSimpleName();
     public int keyCount = 1000000;
-    public int logFrequency = 10000;
-    public int performanceUpdateFrequency = 10000;
+    public String basename = BatchingICacheTest.class.getSimpleName();
     public double writeProb = 0.1;
     public int batchSize = 1;
 
+    private final OperationSelectorBuilder<Operation> operationSelectorBuilder = new OperationSelectorBuilder<Operation>();
+
     private ICache<Object, Object> cache;
-    private final AtomicLong operations = new AtomicLong();
-    private TestContext testContext;
-    private OperationSelectorBuilder<Operation> operationSelectorBuilder = new OperationSelectorBuilder<Operation>();
 
     @Setup
     public void setup(TestContext testContext) throws Exception {
-        this.testContext = testContext;
-
         HazelcastInstance hazelcastInstance = testContext.getTargetInstance();
         CacheManager cacheManager = createCacheManager(hazelcastInstance);
 
@@ -89,74 +80,52 @@ public class BatchingICacheTest {
     @Warmup(global = true)
     public void warmup() throws Exception {
         Streamer<Object, Object> streamer = StreamerFactory.getInstance(cache);
-        for (int k = 0; k < keyCount; k++) {
-            streamer.pushEntry(k, 0);
-
-            if (k % 10000 == 0) {
-                LOGGER.info("Warmup: " + k);
-            }
+        for (int i = 0; i < keyCount; i++) {
+            streamer.pushEntry(i, 0);
         }
         streamer.await();
     }
 
-    @Run
-    public void run() {
-        ThreadSpawner spawner = new ThreadSpawner(testContext.getTestId());
-        for (int k = 0; k < threadCount; k++) {
-            spawner.spawn(new Worker());
-        }
-        spawner.awaitCompletion();
+    @RunWithWorker
+    public Worker createWorker() {
+        return new Worker();
     }
 
-    @Performance
-    public long getOperationCount() {
-        return operations.get();
-    }
+    private class Worker extends AbstractWorker<Operation> {
 
-    private class Worker implements Runnable {
-        private final OperationSelector<Operation> selector = operationSelectorBuilder.build();
-        private final Random random = new Random();
         private final List<ICompletableFuture<?>> futureList = new ArrayList<ICompletableFuture<?>>(batchSize);
 
-        @Override
-        public void run() {
-            long iteration = 0;
-            while (!testContext.isStopped()) {
-                ICompletableFuture<?> future = selectAndInvokeOperation();
-                futureList.add(future);
+        private long iteration;
 
-                iteration++;
-                if (iteration % logFrequency == 0) {
-                    LOGGER.info(Thread.currentThread().getName() + " At iteration: " + iteration);
-                }
-                if (iteration % performanceUpdateFrequency == 0) {
-                    operations.addAndGet(performanceUpdateFrequency);
-                }
-
-                syncIfNecessary(iteration);
-            }
-            operations.addAndGet(iteration % performanceUpdateFrequency);
+        public Worker() {
+            super(operationSelectorBuilder);
         }
 
-        private ICompletableFuture<?> selectAndInvokeOperation() {
-            Integer key = random.nextInt(keyCount);
-            Operation operation = selector.select();
+        @Override
+        public void timeStep(Operation operation) {
+            Integer key = randomInt(keyCount);
+            ICompletableFuture<?> future;
             switch (operation) {
                 case PUT:
-                    Integer value = random.nextInt();
-                    return cache.putAsync(key, value);
+                    Integer value = randomInt();
+                    future = cache.putAsync(key, value);
+                    break;
                 case GET:
-                    return cache.getAsync(key);
+                    future = cache.getAsync(key);
+                    break;
                 default:
                     throw new UnsupportedOperationException("Unknown operation " + operation);
             }
+            futureList.add(future);
+
+            syncIfNecessary(iteration++);
         }
 
         private void syncIfNecessary(long iteration) {
             if (iteration % batchSize == 0) {
-                for (ICompletableFuture<?> f : futureList) {
+                for (ICompletableFuture<?> future : futureList) {
                     try {
-                        f.get();
+                        future.get();
                     } catch (RuntimeException e) {
                         throw e;
                     } catch (Exception e) {
