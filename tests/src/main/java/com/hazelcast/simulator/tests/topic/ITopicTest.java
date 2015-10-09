c64434a92159e99e326ed92e88467aaa2aa2380b
Donnerbart <github@thunderphreak.de>
2015-10-09 13:23:04 +0200

    Cleanup of topic tests.

--- tests/src/main/java/com/hazelcast/simulator/tests/topic/ITopicTest.java
+++ tests/src/main/java/com/hazelcast/simulator/tests/topic/ITopicTest.java
@@ -5,45 +5,35 @@ import com.hazelcast.core.IAtomicLong;
 import com.hazelcast.core.ITopic;
 import com.hazelcast.core.Message;
 import com.hazelcast.core.MessageListener;
-import com.hazelcast.logging.ILogger;
-import com.hazelcast.logging.Logger;
 import com.hazelcast.simulator.test.TestContext;
 import com.hazelcast.simulator.test.TestRunner;
-import com.hazelcast.simulator.test.annotations.Performance;
-import com.hazelcast.simulator.test.annotations.Run;
+import com.hazelcast.simulator.test.annotations.RunWithWorker;
 import com.hazelcast.simulator.test.annotations.Setup;
 import com.hazelcast.simulator.test.annotations.Teardown;
 import com.hazelcast.simulator.test.annotations.Verify;
 import com.hazelcast.simulator.utils.AssertTask;
-import com.hazelcast.simulator.utils.ThreadSpawner;
+import com.hazelcast.simulator.worker.tasks.AbstractMonotonicWorker;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicLong;
 
 import static com.hazelcast.simulator.utils.CommonUtils.sleepRandomNanos;
 import static com.hazelcast.simulator.utils.TestUtils.assertTrueEventually;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Tests the ITopic. It test creates a number of topic and a number of listeners per topic and each member
- * publishes messages to every topic.
+ * Creates a number of {@link ITopic} and a number of listeners per topic. Each member publishes messages to every topic.
  *
- * This test is inherently unreliable because the ITopic relies on the event system which is unreliable.
+ * This test is inherently unreliable because the {@link ITopic} relies on the event system which is unreliable.
  * When messages are published with a too high rate, eventually the event system will drop incoming events.
  */
 public class ITopicTest {
 
-    private static final ILogger LOGGER = Logger.getLogger(ITopicTest.class);
-
     // properties
     public String basename = ITopicTest.class.getSimpleName();
     public int topicCount = 1000;
-    public int threadCount = 5;
     public int listenersPerTopic = 1;
-    public int logFrequency = 100000;
-    public int performanceUpdateFrequency = 100000;
     public int maxProcessingDelayNanos = 0;
     public int maxPublicationDelayNanos = 1000;
     // the maximum period the verification process is going to wait till the correct number of messags
@@ -53,17 +43,15 @@ public class ITopicTest {
     private IAtomicLong totalExpectedCounter;
     private IAtomicLong totalFoundCounter;
     private ITopic[] topics;
-    private AtomicLong operations = new AtomicLong();
-    private TestContext testContext;
     private List<TopicListener> listeners;
 
     @Setup
     public void setup(TestContext testContext) throws Exception {
-        this.testContext = testContext;
         HazelcastInstance targetInstance = testContext.getTargetInstance();
 
         totalExpectedCounter = targetInstance.getAtomicLong(testContext.getTestId() + ":TotalExpectedCounter");
         totalFoundCounter = targetInstance.getAtomicLong(testContext.getTestId() + ":TotalFoundCounter");
+
         topics = new ITopic[topicCount];
         listeners = new LinkedList<TopicListener>();
         for (int topicIndex = 0; topicIndex < topics.length; topicIndex++) {
@@ -78,13 +66,13 @@ public class ITopicTest {
         }
     }
 
-    @Run
-    public void run() {
-        ThreadSpawner spawner = new ThreadSpawner(testContext.getTestId());
-        for (int i = 0; i < threadCount; i++) {
-            spawner.spawn(new Worker());
+    @Teardown
+    public void teardown() throws Exception {
+        for (ITopic topic : topics) {
+            topic.destroy();
         }
-        spawner.awaitCompletion();
+        totalExpectedCounter.destroy();
+        totalFoundCounter.destroy();
     }
 
     @Verify(global = true)
@@ -94,7 +82,6 @@ public class ITopicTest {
         }
 
         final long expectedCount = totalExpectedCounter.get();
-
         assertTrueEventually(new AssertTask() {
             @Override
             public void run() throws Exception {
@@ -107,64 +94,47 @@ public class ITopicTest {
         }, maxVerificationTimeSeconds);
     }
 
-    @Teardown
-    public void teardown() throws Exception {
-        for (ITopic topic : topics) {
-            topic.destroy();
-        }
-        totalExpectedCounter.destroy();
-        totalFoundCounter.destroy();
+    @RunWithWorker
+    public Worker createWorker() {
+        return new Worker();
     }
 
-    @Performance
-    public long getOperationCount() {
-        return operations.get();
-    }
+    private class Worker extends AbstractMonotonicWorker {
 
-    private class Worker implements Runnable {
-        private final Random random = new Random();
+        private long count;
 
         @Override
-        public void run() {
-            long iteration = 0;
-            long count = 0;
-            while (!testContext.isStopped()) {
-                sleepRandomNanos(random, maxPublicationDelayNanos);
+        public void timeStep() {
+            sleepRandomNanos(getRandom(), maxPublicationDelayNanos);
 
-                ITopic topic = getRandomTopic();
+            long msg = nextMessage();
+            count += msg;
 
-                long msg = nextMessage();
-                count += msg;
-                topic.publish(msg);
+            ITopic<Long> topic = getRandomTopic();
+            topic.publish(msg);
+        }
 
-                iteration++;
-                if (iteration % logFrequency == 0) {
-                    LOGGER.info(Thread.currentThread().getName() + " At iteration: " + iteration);
-                }
-                if (iteration % performanceUpdateFrequency == 0) {
-                    operations.addAndGet(performanceUpdateFrequency);
-                }
-            }
-            operations.addAndGet(iteration % performanceUpdateFrequency);
+        @Override
+        protected void afterRun() {
             totalExpectedCounter.addAndGet(count);
         }
 
-        private ITopic getRandomTopic() {
-            int index = random.nextInt(topics.length);
-            return topics[index];
+        @SuppressWarnings("unchecked")
+        private ITopic<Long> getRandomTopic() {
+            int index = randomInt(topics.length);
+            return (ITopic<Long>) topics[index];
         }
 
         private long nextMessage() {
-            long msg = random.nextLong() % 1000;
-            if (msg < 0) {
-                msg = -msg;
-            }
-            return msg;
+            long msg = getRandom().nextLong() % 1000;
+            return (msg < 0) ? -msg : msg;
         }
     }
 
     private class TopicListener implements MessageListener<Long> {
+
         private final Random random = new Random();
+
         private volatile long count;
 
         @Override
