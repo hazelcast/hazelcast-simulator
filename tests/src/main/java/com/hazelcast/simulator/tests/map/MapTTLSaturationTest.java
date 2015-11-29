7ac77c834954b698d94ce8003966ab57a0411b4d
Donnerbart <github@thunderphreak.de>
2015-11-29 23:59:26 +0100

    Refactored MapTTLSaturationTest.

--- tests/src/main/java/com/hazelcast/simulator/tests/map/MapTTLSaturationTest.java
+++ tests/src/main/java/com/hazelcast/simulator/tests/map/MapTTLSaturationTest.java
@@ -24,15 +24,14 @@ import com.hazelcast.logging.ILogger;
 import com.hazelcast.logging.Logger;
 import com.hazelcast.simulator.test.TestContext;
 import com.hazelcast.simulator.test.TestRunner;
-import com.hazelcast.simulator.test.annotations.Run;
+import com.hazelcast.simulator.test.annotations.RunWithWorker;
 import com.hazelcast.simulator.test.annotations.Setup;
 import com.hazelcast.simulator.test.annotations.Verify;
-import com.hazelcast.simulator.utils.ThreadSpawner;
+import com.hazelcast.simulator.worker.tasks.AbstractMonotonicWorker;
 
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-import static com.hazelcast.simulator.utils.CommonUtils.sleepMillis;
+import static com.hazelcast.simulator.utils.CommonUtils.sleepSeconds;
 import static com.hazelcast.simulator.utils.FormatUtils.humanReadableByteCount;
 
 public class MapTTLSaturationTest {
@@ -44,95 +43,77 @@ public class MapTTLSaturationTest {
     public int threadCount = 3;
     public double maxHeapUsagePercentage = 80;
 
-    private TestContext testContext;
     private IMap<Long, Long> map;
 
     @Setup
     public void setup(TestContext testContext) {
-        this.testContext = testContext;
         map = testContext.getTargetInstance().getMap(basename);
     }
 
-    private double heapUsedPercentage() {
-        long total = Runtime.getRuntime().totalMemory();
-        long max = Runtime.getRuntime().maxMemory();
-        return (100d * total) / max;
+    @Verify(global = false)
+    public void localVerify() {
+        logHeapUsage("localVerify()");
     }
 
-    @Run
-    public void run() {
-        ThreadSpawner spawner = new ThreadSpawner(basename);
-        for (int i = 0; i < threadCount; i++) {
-            spawner.spawn(new Worker());
-        }
-        spawner.awaitCompletion();
+    @RunWithWorker
+    public Worker createWorker() {
+        return new Worker();
     }
 
-    private class Worker implements Runnable {
+    private class Worker extends AbstractMonotonicWorker {
+
+        private int counter = 1;
+
         @Override
-        public void run() {
-            long free = Runtime.getRuntime().freeMemory();
-            long total = Runtime.getRuntime().totalMemory();
-            long baseLineUsed = total - free;
-            long maxBytes = Runtime.getRuntime().maxMemory();
-            double usedOfMax = 100.0 * ((double) baseLineUsed / (double) maxBytes);
+        protected void beforeRun() {
+            logHeapUsage("beforeRun()");
+        }
 
-            LOGGER.info(basename + " before Init");
-            LOGGER.info(basename + " free = " + humanReadableByteCount(free, true) + " = " + free);
-            LOGGER.info(basename + " used = " + humanReadableByteCount(baseLineUsed, true) + " = " + baseLineUsed);
-            LOGGER.info(basename + " max = " + humanReadableByteCount(maxBytes, true) + " = " + maxBytes);
-            LOGGER.info(basename + " usedOfMax = " + usedOfMax + '%');
+        @Override
+        protected void timeStep() throws Exception {
+            double usedPercentage = heapUsedPercentage();
+            if (usedPercentage >= maxHeapUsagePercentage) {
+                LOGGER.info(basename + " heap used: " + usedPercentage + " %, map size: " + map.size());
 
-            int counter = 1;
-            Random random = new Random();
-
-            while (!testContext.isStopped()) {
-                double usedPercentage = heapUsedPercentage();
-                if (usedPercentage >= maxHeapUsagePercentage) {
-                    LOGGER.info("heap used: " + usedPercentage + " % map.size:" + map.size());
-
-                    sleepMillis(10000);
-                } else {
-                    for (int i = 0; i < 1000; i++) {
-                        counter++;
-                        if (counter % 100000 == 0) {
-                            LOGGER.info("at:" + counter + " heap used: " + usedPercentage + " % map.size:" + map.size());
-                        }
-                        long key = random.nextLong();
-                        map.put(key, 0L, 24, TimeUnit.HOURS);
+                sleepSeconds(10);
+            } else {
+                for (int i = 0; i < 1000; i++) {
+                    counter++;
+                    if (counter % 100000 == 0) {
+                        LOGGER.info(basename + " at: " + counter + ", heap used: " + usedPercentage
+                                + " %, map size: " + map.size());
                     }
+                    long key = getRandom().nextLong();
+                    map.put(key, 0L, 24, TimeUnit.HOURS);
                 }
             }
+        }
 
-            free = Runtime.getRuntime().freeMemory();
-            total = Runtime.getRuntime().totalMemory();
-            long nowUsed = total - free;
-            maxBytes = Runtime.getRuntime().maxMemory();
-            usedOfMax = 100.0 * ((double) nowUsed / (double) maxBytes);
-
-            LOGGER.info(basename + " After Init");
-            LOGGER.info(basename + " map = " + map.size());
-            LOGGER.info(basename + " free = " + humanReadableByteCount(free, true) + " = " + free);
-            LOGGER.info(basename + " used = " + humanReadableByteCount(nowUsed, true) + " = " + nowUsed);
-            LOGGER.info(basename + " max = " + humanReadableByteCount(maxBytes, true) + " = " + maxBytes);
-            LOGGER.info(basename + " usedOfMax = " + usedOfMax + '%');
-            LOGGER.info(basename + " map size:" + map.size());
+        @Override
+        protected void afterRun() {
+            logHeapUsage("afterRun()");
         }
     }
 
-    @Verify(global = false)
-    public void globalVerify() {
+    private void logHeapUsage(String header) {
         long free = Runtime.getRuntime().freeMemory();
         long total = Runtime.getRuntime().totalMemory();
         long used = total - free;
-        long maxBytes = Runtime.getRuntime().maxMemory();
-        double usedOfMax = 100.0 * ((double) used / (double) maxBytes);
+        long max = Runtime.getRuntime().maxMemory();
+        double usedOfMax = 100.0 * ((double) used / (double) max);
 
-        LOGGER.info(basename + " map = " + map.size());
-        LOGGER.info(basename + "free = " + humanReadableByteCount(free, true) + " = " + free);
-        LOGGER.info(basename + "used = " + humanReadableByteCount(used, true) + " = " + used);
-        LOGGER.info(basename + "max = " + humanReadableByteCount(maxBytes, true) + " = " + maxBytes);
-        LOGGER.info(basename + "usedOfMax = " + usedOfMax + '%');
+        LOGGER.info(basename + ' ' + header);
+        LOGGER.info(basename + " map size: " + map.size());
+        LOGGER.info(basename + " free: " + humanReadableByteCount(free, true) + " = " + free);
+        LOGGER.info(basename + " used: " + humanReadableByteCount(used, true) + " = " + used);
+        LOGGER.info(basename + " max: " + humanReadableByteCount(max, true) + " = " + max);
+        LOGGER.info(basename + " usedOfMax: " + usedOfMax + '%');
+    }
+
+    private static double heapUsedPercentage() {
+        long total = Runtime.getRuntime().totalMemory();
+        long max = Runtime.getRuntime().maxMemory();
+        return (100d * total) / max;
     }
 
     public static void main(String[] args) throws Exception {
