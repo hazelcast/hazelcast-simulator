027a1d1412d88ab0a9f67dfc515cc9e180d747ff
alarmnummer <peter.veentjer@hazelcast.com>
2015-12-21 08:51:40 +0200

    Tweaks in map serializer test to include different serializer strategies

--- tests/src/main/java/com/hazelcast/simulator/tests/map/MapSerializationTest.java
+++ tests/src/main/java/com/hazelcast/simulator/tests/map/MapSerializationTest.java
@@ -17,6 +17,9 @@ package com.hazelcast.simulator.tests.map;
 
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.DataSerializable;
 import com.hazelcast.simulator.probes.Probe;
 import com.hazelcast.simulator.test.TestContext;
 import com.hazelcast.simulator.test.TestRunner;
@@ -29,21 +32,35 @@ import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
+import java.io.Serializable;
 
 /**
  * A test that checks how fast externalizable values can be put in a map. The key is an integer.
+ *
+ * todo:
+ * add the following serializer
+ * - identified data-serializable
+ * - kryo
+ * - fast-serialization
  */
-public class MapExternalizableTest {
+public class MapSerializationTest {
+
+    public enum Serializer {
+        Serializable,
+        Externalizable,
+        DataSerializable
+    }
 
     // properties
-    public String basename = MapExternalizableTest.class.getSimpleName();
+    public String basename = MapSerializationTest.class.getSimpleName();
     public int threadCount = 10;
     public int keyCount = 1000000;
+    public Serializer serializer = Serializer.Serializable;
 
     // probes
     public Probe probe;
 
-    private IMap<Integer, Value> map;
+    private IMap<Integer, Object> map;
 
     @Setup
     public void setUp(TestContext testContext) {
@@ -65,22 +82,34 @@ public class MapExternalizableTest {
         @Override
         protected void timeStep() throws Exception {
             int key = randomInt(keyCount);
-            map.put(key, new Value(key));
+            switch (serializer) {
+                case Serializable:
+                    map.put(key, new SerializableValue(key));
+                    break;
+                case Externalizable:
+                    map.put(key, new ExternalizableValue(key));
+                    break;
+                case DataSerializable:
+                    map.put(key, new DataSerializableValue(key));
+                    break;
+                default:
+                    throw new IllegalStateException("Unrecognized serializer:" + serializer);
+            }
         }
     }
 
     public static void main(String[] args) throws Exception {
-        MapExternalizableTest test = new MapExternalizableTest();
-        new TestRunner<MapExternalizableTest>(test).run();
+        MapSerializationTest test = new MapSerializationTest();
+        new TestRunner<MapSerializationTest>(test).run();
     }
 
-    public static class Value implements Externalizable {
+    public static class ExternalizableValue implements Externalizable {
         private int v;
 
-        public Value() {
+        public ExternalizableValue() {
         }
 
-        public Value(int v) {
+        public ExternalizableValue(int v) {
             this.v = v;
         }
 
@@ -96,6 +125,45 @@ public class MapExternalizableTest {
         @Override
         public void writeExternal(ObjectOutput out) throws IOException {
             out.writeInt(v);
+        }
+    }
+
+    public static class SerializableValue implements Serializable {
+        private int v;
+
+        public SerializableValue(int v) {
+            this.v = v;
+        }
+
+        public int id() {
+            return v;
+        }
+    }
+
+
+    public static class DataSerializableValue implements DataSerializable {
+        private int v;
+
+        public DataSerializableValue() {
+        }
+
+
+        public DataSerializableValue(int v) {
+            this.v = v;
+        }
+
+        public int id() {
+            return v;
+        }
+
+        @Override
+        public void writeData(ObjectDataOutput out) throws IOException {
+            out.writeInt(v);
+        }
+
+        @Override
+        public void readData(ObjectDataInput in) throws IOException {
+            v = in.readInt();
         }
     }
 }
