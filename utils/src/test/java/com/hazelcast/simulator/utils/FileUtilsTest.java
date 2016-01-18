62de15a6a3cbabb6516bff4af10bf6cdcaaed549
Donnerbart <github@thunderphreak.de>
2016-01-18 15:35:41 +0100

    Increased coverage of FileUtils.

--- utils/src/test/java/com/hazelcast/simulator/utils/FileUtilsTest.java
+++ utils/src/test/java/com/hazelcast/simulator/utils/FileUtilsTest.java
@@ -5,14 +5,22 @@ import org.junit.Test;
 import java.io.File;
 
 import static com.hazelcast.simulator.utils.FileUtils.USER_HOME;
+import static com.hazelcast.simulator.utils.FileUtils.appendText;
+import static com.hazelcast.simulator.utils.FileUtils.deleteQuiet;
+import static com.hazelcast.simulator.utils.FileUtils.fileAsText;
 import static com.hazelcast.simulator.utils.FileUtils.isValidFileName;
 import static com.hazelcast.simulator.utils.FileUtils.newFile;
+import static com.hazelcast.simulator.utils.FileUtils.readObject;
+import static com.hazelcast.simulator.utils.FileUtils.writeObject;
+import static com.hazelcast.simulator.utils.FileUtils.writeText;
 import static com.hazelcast.simulator.utils.ReflectionUtils.invokePrivateConstructor;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class FileUtilsTest {
+
+    private static final File INVALID_FILE = new File(":\\//?&");
 
     @Test
     public void testConstructor() throws Exception {
@@ -46,5 +54,99 @@ public class FileUtilsTest {
     public void testNewFileUserHomeBased() {
         File actual = newFile("~" + File.separator + "foobar");
         assertEquals(USER_HOME + File.separator + "foobar", actual.getPath());
+    }
+
+    @Test
+    public void testWriteAndReadObject() {
+        File file = new File("testWriteAndReadObject");
+        try {
+            String expected = "Hello World!";
+
+            writeObject(expected, file);
+            String actual = readObject(file);
+
+            assertEquals(expected, actual);
+        } finally {
+            deleteQuiet(file);
+        }
+    }
+
+    @Test(expected = FileUtilsException.class)
+    public void testWriteObject_withInvalidFilename() {
+        writeObject(null, INVALID_FILE);
+    }
+
+    @Test(expected = FileUtilsException.class)
+    public void testReadObject_withInvalidFilename() {
+        readObject(INVALID_FILE);
+    }
+
+    @Test
+    public void testWriteText() {
+        File file = new File("testWriteText");
+        try {
+            String expected = "write test";
+            writeText(expected, file);
+
+            String actual = fileAsText(file);
+            assertEquals(expected, actual);
+        } finally {
+            deleteQuiet(file);
+        }
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testWriteText_withNullText() {
+        writeText(null, new File("ignored"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testWriteText_withNullFile() {
+        writeText("ignored", null);
+    }
+
+    @Test(expected = FileUtilsException.class)
+    public void testWriteText_withInvalidFilename() {
+        writeText("ignored", INVALID_FILE);
+    }
+
+    @Test
+    public void testAppendText() {
+        File file = new File("testAppendText");
+        try {
+            String expected = "write test";
+            appendText(expected, file);
+            appendText(expected, file);
+
+            String actual = fileAsText(file);
+            assertEquals(expected + expected, actual);
+        } finally {
+            deleteQuiet(file);
+        }
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAppendText_withNullText() {
+        appendText(null, new File("ignored"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAppendText_withNullFile() {
+        appendText("ignored", (File) null);
+    }
+
+    @Test(expected = FileUtilsException.class)
+    public void testAppendText_withInvalidFilename() {
+        appendText("ignored", INVALID_FILE);
+    }
+
+    @Test(expected = FileUtilsException.class)
+    public void testFileAsText_withInvalidFilename() {
+        fileAsText(INVALID_FILE);
+    }
+
+    @Test
+    public void testDeleteQuiet_withInvalidFilename() {
+        deleteQuiet(new File("/dev/null"));
     }
 }
