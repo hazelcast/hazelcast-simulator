5a7de6e4fbcbdbc4a4afea05751940d2edc3675a
Donnerbart <github@thunderphreak.de>
2016-01-26 13:50:58 +0100

    Increased code coverage of AbstractServerConnector.

--- simulator/src/test/java/com/hazelcast/simulator/protocol/connector/AbstractServerConnectorTest.java
+++ simulator/src/test/java/com/hazelcast/simulator/protocol/connector/AbstractServerConnectorTest.java
@@ -1,18 +1,30 @@
 package com.hazelcast.simulator.protocol.connector;
 
 import com.hazelcast.simulator.protocol.core.AddressLevel;
+import com.hazelcast.simulator.protocol.core.Response;
 import com.hazelcast.simulator.protocol.core.ResponseFuture;
+import com.hazelcast.simulator.protocol.core.ResponseType;
 import com.hazelcast.simulator.protocol.core.SimulatorAddress;
+import com.hazelcast.simulator.protocol.operation.IntegrationTestOperation;
+import com.hazelcast.simulator.protocol.operation.SimulatorOperation;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.group.ChannelGroup;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import static com.hazelcast.simulator.protocol.core.ResponseFuture.getMessageIdFromFutureKey;
+import static com.hazelcast.simulator.protocol.core.ResponseType.EXCEPTION_DURING_OPERATION_EXECUTION;
+import static com.hazelcast.simulator.protocol.core.ResponseType.SUCCESS;
+import static com.hazelcast.simulator.protocol.core.SimulatorAddress.COORDINATOR;
+import static com.hazelcast.simulator.utils.CommonUtils.sleepMillis;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.mock;
@@ -21,21 +33,35 @@ import static org.mockito.Mockito.when;
 
 public class AbstractServerConnectorTest {
 
+    private boolean shutdownAfterTest = true;
+
+    private SimulatorAddress connectorAddress;
+    private ConcurrentMap<String, ResponseFuture> futureMap;
     private ExecutorService executorService;
+
     private TestServerConnector testServerConnector;
 
     @Before
     public void setUp() {
+        futureMap = new ConcurrentHashMap<String, ResponseFuture>();
+        connectorAddress = new SimulatorAddress(AddressLevel.WORKER, 1, 1, 0);
         executorService = mock(ExecutorService.class);
-        ConcurrentMap<String, ResponseFuture> futureMap = new ConcurrentHashMap<String, ResponseFuture>();
-        SimulatorAddress address = new SimulatorAddress(AddressLevel.WORKER, 1, 1, 0);
 
-        testServerConnector = new TestServerConnector(futureMap, address, 9000, executorService);
-        testServerConnector.start();
+        testServerConnector = new TestServerConnector(futureMap, connectorAddress, 9000, executorService);
+    }
+
+    @After
+    public void tearDown() {
+        if (shutdownAfterTest) {
+            testServerConnector.shutdown();
+        }
     }
 
     @Test
     public void testShutdown() throws Exception {
+        shutdownAfterTest = false;
+        testServerConnector.start();
+
         testServerConnector.shutdown();
 
         verify(executorService).shutdown();
@@ -44,7 +70,9 @@ public class AbstractServerConnectorTest {
 
     @Test
     public void testShutdown_withInterruptedException() throws Exception {
+        shutdownAfterTest = false;
         when(executorService.awaitTermination(anyLong(), any(TimeUnit.class))).thenThrow(new InterruptedException("expected"));
+        testServerConnector.start();
 
         testServerConnector.shutdown();
 
@@ -52,7 +80,77 @@ public class AbstractServerConnectorTest {
         verify(executorService).awaitTermination(anyLong(), any(TimeUnit.class));
     }
 
+    @Test
+    public void testShutdown_withMessageOnQueue() throws Exception {
+        shutdownAfterTest = false;
+        SimulatorOperation operation = new IntegrationTestOperation(null);
+        testServerConnector.start();
+
+        testServerConnector.submit(COORDINATOR, operation);
+        ResponseFuture future = testServerConnector.submit(COORDINATOR, operation);
+
+        Thread responseSetter = new Thread() {
+            @Override
+            public void run() {
+                sleepMillis(300);
+                setResponse(SUCCESS, 2);
+            }
+        };
+
+        responseSetter.start();
+        testServerConnector.shutdown();
+        responseSetter.join();
+
+        Response response = future.get();
+        assertEquals(SUCCESS, response.getFirstErrorResponseType());
+    }
+
+    @Test
+    public void testGetMessageQueueSizeInternal() throws Exception {
+        SimulatorOperation operation = new IntegrationTestOperation(null);
+
+        testServerConnector.submit(COORDINATOR, operation);
+        testServerConnector.submit(COORDINATOR, operation);
+        ResponseFuture future = testServerConnector.submit(COORDINATOR, operation);
+
+        assertEquals(3, testServerConnector.getMessageQueueSizeInternal());
+
+        testServerConnector.start();
+        setResponse(SUCCESS, 3);
+
+        Response response = future.get();
+        assertEquals(SUCCESS, response.getFirstErrorResponseType());
+    }
+
+    @Test
+    public void testSubmit_withFailureResponse() throws Exception {
+        SimulatorOperation operation = new IntegrationTestOperation(null);
+        testServerConnector.start();
+
+        ResponseFuture future = testServerConnector.submit(COORDINATOR, operation);
+        setResponse(EXCEPTION_DURING_OPERATION_EXECUTION, 1);
+        Response response = future.get();
+
+        assertEquals(EXCEPTION_DURING_OPERATION_EXECUTION, response.getFirstErrorResponseType());
+    }
+
+    private void setResponse(ResponseType responseType, int expectedMessageCount) {
+        int responseSetCounter = 0;
+        int tries = 0;
+        do {
+            for (Map.Entry<String, ResponseFuture> entry : futureMap.entrySet()) {
+                long messageId = getMessageIdFromFutureKey(entry.getKey());
+                Response response = new Response(messageId, connectorAddress, COORDINATOR, responseType);
+                entry.getValue().set(response);
+                responseSetCounter++;
+            }
+            sleepMillis(50);
+        } while (responseSetCounter < expectedMessageCount && tries++ < 50);
+    }
+
     private class TestServerConnector extends AbstractServerConnector {
+
+        private final ChannelGroup channelGroup = mock(ChannelGroup.class);
 
         TestServerConnector(ConcurrentMap<String, ResponseFuture> futureMap, SimulatorAddress localAddress, int port,
                             ExecutorService executorService) {
@@ -69,7 +167,7 @@ public class AbstractServerConnectorTest {
 
         @Override
         ChannelGroup getChannelGroup() {
-            return null;
+            return channelGroup;
         }
     }
 }
