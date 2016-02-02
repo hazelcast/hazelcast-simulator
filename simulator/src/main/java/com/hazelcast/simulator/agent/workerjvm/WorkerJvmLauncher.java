3420fa164d61937b3a6c13c682cb49eb84876fd5
Donnerbart <github@thunderphreak.de>
2016-02-02 14:37:19 +0100

    Simplified classpath for worker.sh.

--- simulator/src/main/java/com/hazelcast/simulator/agent/workerjvm/WorkerJvmLauncher.java
+++ simulator/src/main/java/com/hazelcast/simulator/agent/workerjvm/WorkerJvmLauncher.java
@@ -49,7 +49,6 @@ public class WorkerJvmLauncher {
 
     private static final int WAIT_FOR_WORKER_STARTUP_INTERVAL_MILLIS = 500;
 
-    private static final String CLASSPATH = System.getProperty("java.class.path");
     private static final String CLASSPATH_SEPARATOR = System.getProperty("path.separator");
 
     private static final Logger LOGGER = Logger.getLogger(WorkerJvmLauncher.class);
@@ -281,10 +280,11 @@ public class WorkerJvmLauncher {
     }
 
     private String getClasspath() {
+        String simulatorHome = getSimulatorHome().getAbsolutePath();
         String hzVersionDirectory = directoryForVersionSpec(workerJvmSettings.getHazelcastVersionSpec());
-        return CLASSPATH
-                + CLASSPATH_SEPARATOR + getSimulatorHome() + "/hz-lib/" + hzVersionDirectory + "/*"
-                + CLASSPATH_SEPARATOR + getSimulatorHome() + "/user-lib/*"
+        return simulatorHome + "/lib/*"
+                + CLASSPATH_SEPARATOR + simulatorHome + "/hz-lib/" + hzVersionDirectory + "/*"
+                + CLASSPATH_SEPARATOR + simulatorHome + "/user-lib/*"
                 + CLASSPATH_SEPARATOR + new File(agent.getTestSuiteDir(), "lib/*").getAbsolutePath();
     }
 
