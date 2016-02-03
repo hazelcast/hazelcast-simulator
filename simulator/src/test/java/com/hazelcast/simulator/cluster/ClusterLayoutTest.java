b4982b32bd7d369788221e35c8625230d48ce09e
Donnerbart <github@thunderphreak.de>
2016-02-03 18:14:33 +0100

    Increased code coverage of ClusterLayout.

--- simulator/src/test/java/com/hazelcast/simulator/cluster/ClusterLayoutTest.java
+++ simulator/src/test/java/com/hazelcast/simulator/cluster/ClusterLayoutTest.java
@@ -8,6 +8,9 @@ import com.hazelcast.simulator.protocol.registry.ComponentRegistry;
 import com.hazelcast.simulator.utils.jars.HazelcastJARs;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.util.Collections.singleton;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
@@ -20,8 +23,24 @@ public class ClusterLayoutTest {
     public void testGetVersionSpecs() {
         AgentData agentData = new AgentData(1, "127.0.0.1", "127.0.0.1");
 
+        testGetVersionSpecs(singletonList(agentData), 1, 0);
+    }
+
+    @Test
+    public void testGetVersionSpecs_noWorkersOnSecondAgent() {
+        AgentData agentData1 = new AgentData(1, "172.16.16.1", "127.0.0.1");
+        AgentData agentData2 = new AgentData(2, "172.16.16.2", "127.0.0.1");
+
+        List<AgentData> agents = new ArrayList<AgentData>(2);
+        agents.add(agentData1);
+        agents.add(agentData2);
+
+        testGetVersionSpecs(agents, 1, 0);
+    }
+
+    private void testGetVersionSpecs(List<AgentData> agents, int memberCount, int clientCount) {
         ComponentRegistry componentRegistry = mock(ComponentRegistry.class);
-        when(componentRegistry.getAgents()).thenReturn(singletonList(agentData));
+        when(componentRegistry.getAgents()).thenReturn(agents);
 
         WorkerParameters workerParameters = mock(WorkerParameters.class);
         when(workerParameters.getProfiler()).thenReturn(JavaProfiler.NONE);
@@ -29,8 +48,8 @@ public class ClusterLayoutTest {
 
         ClusterLayoutParameters clusterLayoutParameters = mock(ClusterLayoutParameters.class);
         when(clusterLayoutParameters.getDedicatedMemberMachineCount()).thenReturn(0);
-        when(clusterLayoutParameters.getMemberWorkerCount()).thenReturn(1);
-        when(clusterLayoutParameters.getClientWorkerCount()).thenReturn(0);
+        when(clusterLayoutParameters.getMemberWorkerCount()).thenReturn(memberCount);
+        when(clusterLayoutParameters.getClientWorkerCount()).thenReturn(clientCount);
 
         ClusterLayout clusterLayout = new ClusterLayout(componentRegistry, workerParameters, clusterLayoutParameters);
 
