f7c8c840f2ebd6e0a5bf9b2fb6cff0a67433cd8a
Donnerbart <github@thunderphreak.de>
2016-02-09 21:10:39 +0100

    Added missing @Override to AbstractWorker. Set explicit name for workerProbe via a constant in AbstractWorker.

--- simulator/src/main/java/com/hazelcast/simulator/worker/tasks/AbstractWorker.java
+++ simulator/src/main/java/com/hazelcast/simulator/worker/tasks/AbstractWorker.java
@@ -39,13 +39,15 @@ import static com.hazelcast.simulator.utils.CommonUtils.rethrow;
  */
 public abstract class AbstractWorker<O extends Enum<O>> implements IWorker {
 
+    public static final String DEFAULT_WORKER_PROBE_NAME = "workerProbe";
+
     protected static final ILogger LOGGER = Logger.getLogger(AbstractWorker.class);
 
     final OperationSelector<O> selector;
 
     @InjectTestContext
     TestContext testContext;
-    @InjectProbe(useForThroughput = true)
+    @InjectProbe(name = DEFAULT_WORKER_PROBE_NAME, useForThroughput = true)
     Probe workerProbe;
 
     private final Random random = new Random();
@@ -133,6 +135,7 @@ public abstract class AbstractWorker<O extends Enum<O>> implements IWorker {
      *
      * @see IWorker
      */
+    @Override
     public void afterCompletion() throws Exception {
     }
 
