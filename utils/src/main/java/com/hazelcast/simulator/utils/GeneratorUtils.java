55bc61a179ce003de986cd9e1b2c77af9a82ca0e
Donnerbart <github@thunderphreak.de>
2016-02-18 18:04:39 +0100

    Cleanup of GeneratorUtils.

--- utils/src/main/java/com/hazelcast/simulator/utils/GeneratorUtils.java
+++ utils/src/main/java/com/hazelcast/simulator/utils/GeneratorUtils.java
@@ -34,15 +34,15 @@ public final class GeneratorUtils {
      */
     public static String[] generateStrings(int count, int length) {
         String[] keys = new String[count];
-        for (int k = 0; k < keys.length; k++) {
-            keys[k] = generateString(length);
+        for (int i = 0; i < keys.length; i++) {
+            keys[i] = generateString(length);
         }
         return keys;
     }
 
     public static String generateString(int length) {
         StringBuilder sb = new StringBuilder();
-        for (int k = 0; k < length; k++) {
+        for (int i = 0; i < length; i++) {
             char c = ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length()));
             sb.append(c);
         }
