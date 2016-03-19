087f60bf95215b502ec5d8ee9ccee4bf19a149c9
alarmnummer <peter.veentjer@hazelcast.com>
2016-03-19 09:05:30 +0200

    Added new test that tests putasync using an completionhandler

---
+++ tests/src/main/java/com/hazelcast/simulator/tests/map/PutAsyncAndThenTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2008-2016, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hazelcast.simulator.tests.map;
+
+import com.hazelcast.core.ExecutionCallback;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ICompletableFuture;
+import com.hazelcast.core.IMap;
+import com.hazelcast.simulator.test.TestContext;
+import com.hazelcast.simulator.test.annotations.RunWithWorker;
+import com.hazelcast.simulator.test.annotations.Setup;
+import com.hazelcast.simulator.test.annotations.Teardown;
+import com.hazelcast.simulator.utils.ExceptionReporter;
+import com.hazelcast.simulator.worker.tasks.AbstractMonotonicWorker;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class PutAsyncAndThenTest {
+
+    // properties
+    public int keyCount = 1000;
+    public int maxConcurrentCallsPerWorker = 1000;
+    public String basename = PutAsyncAndThenTest.class.getSimpleName();
+    public long acquireTimeoutMs = 2 * 60 * 1000;
+
+    private IMap<Integer, Integer> map;
+    private TestContext context;
+
+    @Setup
+    public void setUp(TestContext testContext) {
+        this.context = testContext;
+        HazelcastInstance targetInstance = testContext.getTargetInstance();
+        map = targetInstance.getMap(basename);
+    }
+
+    @Teardown
+    public void tearDown() {
+        map.destroy();
+    }
+
+    @RunWithWorker
+    public Worker createWorker() {
+        return new Worker();
+    }
+
+    private class Worker extends AbstractMonotonicWorker implements ExecutionCallback {
+        private final Semaphore semaphore = new Semaphore(maxConcurrentCallsPerWorker);
+
+        @Override
+        protected void timeStep() throws Exception {
+            if (semaphore.tryAcquire(1, acquireTimeoutMs, TimeUnit.MILLISECONDS)) {
+                throw new RuntimeException();
+            }
+
+            Integer key = randomInt(keyCount);
+            ICompletableFuture f = (ICompletableFuture) map.putAsync(key, key);
+            f.andThen(this);
+        }
+
+        @Override
+        public void onResponse(Object o) {
+            semaphore.release();
+        }
+
+        @Override
+        public void onFailure(Throwable throwable) {
+            ExceptionReporter.report(context.getTestId(), throwable);
+        }
+    }
+}
