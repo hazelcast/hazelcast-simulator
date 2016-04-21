fe9686f5d19ea7a2bf93c5b25b62248cf1b65566
hasancelik <hasan@hazelcast.com>
2016-04-21 13:55:18 +0300

    added new constructor with concurrencyLevel variable

--- simulator/src/main/java/com/hazelcast/simulator/worker/loadsupport/AsyncMapStreamer.java
+++ simulator/src/main/java/com/hazelcast/simulator/worker/loadsupport/AsyncMapStreamer.java
@@ -28,6 +28,11 @@ final class AsyncMapStreamer<K, V> extends AbstractAsyncStreamer<K, V> {
 
     private final IMap<K, V> map;
 
+    public AsyncMapStreamer(int concurrencyLevel, IMap<K, V> map) {
+        super(concurrencyLevel);
+        this.map = map;
+    }
+
     AsyncMapStreamer(IMap<K, V> map) {
         this.map = map;
     }
