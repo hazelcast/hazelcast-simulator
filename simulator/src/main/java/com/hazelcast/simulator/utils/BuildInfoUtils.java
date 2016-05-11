f9a5e46f11ed416a0dd9445db84a3c253e9ad50f
Donnerbart <github@thunderphreak.de>
2016-05-11 19:46:32 +0200

    Added methods to retrieve Hazelcast version from JAR files.

--- simulator/src/main/java/com/hazelcast/simulator/utils/BuildInfoUtils.java
+++ simulator/src/main/java/com/hazelcast/simulator/utils/BuildInfoUtils.java
@@ -18,29 +18,37 @@ package com.hazelcast.simulator.utils;
 import com.hazelcast.instance.BuildInfo;
 import com.hazelcast.instance.BuildInfoProvider;
 
+import java.io.File;
+import java.util.List;
+import java.util.jar.Attributes;
+import java.util.jar.Attributes.Name;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+import static com.hazelcast.simulator.utils.FileUtils.getFilesFromClassPath;
 import static com.hazelcast.simulator.utils.VersionUtils.parseVersionString;
+import static java.lang.String.format;
 
 public final class BuildInfoUtils {
 
-    static final int DEFAULT_MINOR_VERSION = 5;
+    static final int DEFAULT_MAJOR_VERSION = 3;
+    static final int DEFAULT_MINOR_VERSION = 6;
+
+    static final int DEFAULT_FALLBACK_MAJOR_VERSION = 3;
+    static final int DEFAULT_FALLBACK_MINOR_VERSION = 5;
 
     private BuildInfoUtils() {
     }
 
-    public static int getMinorVersion() {
-        return getMinorVersion(getVersion());
-    }
-
-    static int getMinorVersion(String version) {
-        if (version == null) {
-            return DEFAULT_MINOR_VERSION;
-        }
-        String[] versions = parseVersionString(version);
-        return Integer.parseInt(versions[1]);
-    }
-
     public static boolean isMinVersion(String minVersion) {
         return isMinVersion(minVersion, getVersion());
+    }
+
+    public static String getHazelcastVersionFromJAR(String classPath) {
+        String hzVersion = getHazelcastVersionFromJarOrNull(classPath);
+        int majorVersion = (hzVersion != null) ? getMajorVersion(hzVersion) : DEFAULT_MAJOR_VERSION;
+        int minorVersion = (hzVersion != null) ? getMinorVersion(hzVersion) : DEFAULT_MINOR_VERSION;
+        return format("%d%d", majorVersion, minorVersion);
     }
 
     static boolean isMinVersion(String minVersion, String version) {
@@ -50,12 +58,75 @@ public final class BuildInfoUtils {
         return VersionUtils.isMinVersion(minVersion, getVersion());
     }
 
+    static String getHazelcastVersionFromJarOrNull(String classPath) {
+        try {
+            List<File> jarFiles = getFilesFromClassPath(classPath);
+            for (File jarFile : jarFiles) {
+                String version = getVersion(jarFile);
+                if (version != null) {
+                    return version;
+                }
+            }
+        } catch (FileUtilsException ignored) {
+            EmptyStatement.ignore(ignored);
+        }
+        return null;
+    }
+
+    static int getMajorVersion() {
+        return getMajorVersion(getVersion());
+    }
+
+    static int getMinorVersion() {
+        return getMinorVersion(getVersion());
+    }
+
+    static int getMajorVersion(String version) {
+        if (version == null) {
+            return DEFAULT_FALLBACK_MAJOR_VERSION;
+        }
+        String[] versions = parseVersionString(version);
+        return Integer.parseInt(versions[0]);
+    }
+
+    static int getMinorVersion(String version) {
+        if (version == null) {
+            return DEFAULT_FALLBACK_MINOR_VERSION;
+        }
+        String[] versions = parseVersionString(version);
+        return Integer.parseInt(versions[1]);
+    }
+
     private static String getVersion() {
         try {
             BuildInfo buildInfo = BuildInfoProvider.getBuildInfo();
             return buildInfo.getVersion();
         } catch (NoClassDefFoundError e) {
             // it's Hazelcast 3.2 or older
+            return null;
+        }
+    }
+
+    private static String getVersion(File jarFile) {
+        try {
+            JarFile jar = new JarFile(jarFile);
+            Manifest manifest = jar.getManifest();
+
+            String versionNumber = null;
+            Attributes attributes = manifest.getMainAttributes();
+            if (attributes != null) {
+                for (Object attributeKey : attributes.keySet()) {
+                    Name key = (Name) attributeKey;
+                    String keyword = key.toString();
+                    if (keyword.equals("Implementation-Version") || keyword.equals("Bundle-Version")) {
+                        versionNumber = (String) attributes.get(key);
+                        break;
+                    }
+                }
+            }
+            jar.close();
+            return versionNumber;
+        } catch (Exception ignored) {
             return null;
         }
     }
--- simulator/src/test/java/com/hazelcast/simulator/utils/BuildInfoUtilsTest.java
+++ simulator/src/test/java/com/hazelcast/simulator/utils/BuildInfoUtilsTest.java
@@ -2,29 +2,31 @@ package com.hazelcast.simulator.utils;
 
 import org.junit.Test;
 
+import java.io.File;
+
+import static com.hazelcast.simulator.utils.BuildInfoUtils.DEFAULT_FALLBACK_MAJOR_VERSION;
+import static com.hazelcast.simulator.utils.BuildInfoUtils.DEFAULT_FALLBACK_MINOR_VERSION;
+import static com.hazelcast.simulator.utils.BuildInfoUtils.DEFAULT_MAJOR_VERSION;
 import static com.hazelcast.simulator.utils.BuildInfoUtils.DEFAULT_MINOR_VERSION;
+import static com.hazelcast.simulator.utils.BuildInfoUtils.getHazelcastVersionFromJAR;
+import static com.hazelcast.simulator.utils.BuildInfoUtils.getHazelcastVersionFromJarOrNull;
+import static com.hazelcast.simulator.utils.BuildInfoUtils.getMajorVersion;
 import static com.hazelcast.simulator.utils.BuildInfoUtils.getMinorVersion;
 import static com.hazelcast.simulator.utils.BuildInfoUtils.isMinVersion;
 import static com.hazelcast.simulator.utils.ReflectionUtils.invokePrivateConstructor;
+import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class BuildInfoUtilsTest {
 
+    private static final String RESOURCE_PATH = "simulator/src/main/resources/";
+
     @Test
     public void testConstructor() throws Exception {
         invokePrivateConstructor(BuildInfoUtils.class);
-    }
-
-    @Test
-    public void testGetMinorVersion() {
-        assertEquals(6, getMinorVersion());
-    }
-
-    @Test
-    public void testGetMinorVersion_whenVersionCannotBeFound_thenReturnDefaultVersion() {
-        assertEquals(DEFAULT_MINOR_VERSION, getMinorVersion(null));
     }
 
     @Test
@@ -35,5 +37,59 @@ public class BuildInfoUtilsTest {
     @Test
     public void testIsMinVersion_whenVersionCannotBeFound_thenReturnFalse() {
         assertFalse(isMinVersion("3.6", null));
+    }
+
+    @Test
+    public void testGetHazelcastVersionFromJAR() {
+        assertEquals("2342", getHazelcastVersionFromJAR(RESOURCE_PATH + "build-info.jar"));
+    }
+
+    @Test
+    public void testGetHazelcastVersionFromJAR_whenNoJarFound_thenReturnDefault() {
+        assertEquals(format("%d%d", DEFAULT_MAJOR_VERSION, DEFAULT_MINOR_VERSION), getHazelcastVersionFromJAR(RESOURCE_PATH));
+    }
+
+    @Test
+    public void testGetMajorVersion() {
+        assertEquals(DEFAULT_MAJOR_VERSION, getMajorVersion());
+    }
+
+    @Test
+    public void testGetMajorVersion_whenVersionCannotBeFound_thenReturnDefaultVersion() {
+        assertEquals(DEFAULT_FALLBACK_MAJOR_VERSION, getMajorVersion(null));
+    }
+
+    @Test
+    public void testGetMinorVersion() {
+        assertEquals(DEFAULT_MINOR_VERSION, getMinorVersion());
+    }
+
+    @Test
+    public void testGetMinorVersion_whenVersionCannotBeFound_thenReturnDefaultVersion() {
+        assertEquals(DEFAULT_FALLBACK_MINOR_VERSION, getMinorVersion(null));
+    }
+
+    @Test
+    public void testGetHazelcastVersionFromJarOrNull() {
+        File file = new File(RESOURCE_PATH + "build-info.jar").getAbsoluteFile();
+        assertEquals("23.42-SNAPSHOT", getHazelcastVersionFromJarOrNull(file.getAbsolutePath()));
+    }
+
+    @Test
+    public void testGetHazelcastVersionFromJarOrNull_whenJarContainsNoVersion_thenReturnNull() {
+        File file = new File(RESOURCE_PATH + "build-info-no-version.jar").getAbsoluteFile();
+        assertNull(getHazelcastVersionFromJarOrNull(file.getAbsolutePath()));
+    }
+
+    @Test
+    public void testGetHazelcastVersionFromJarOrNull_whenNoJarFound_thenReturnNull() {
+        File file = new File("simulator/src/main/java/*").getAbsoluteFile();
+        assertNull(getHazelcastVersionFromJarOrNull(file.getAbsolutePath()));
+    }
+
+    @Test
+    public void testGetHazelcastVersionFromJarOrNull_whenInvalidPath_thenReturnNull() {
+        File file = new File("notExists/*").getAbsoluteFile();
+        assertNull(getHazelcastVersionFromJarOrNull(file.getAbsolutePath()));
     }
 }
