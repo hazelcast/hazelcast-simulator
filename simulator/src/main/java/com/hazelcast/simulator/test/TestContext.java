ace17f433115f1f0489ebfc7ca9a18d4abcfcedb
Donnerbart <github@thunderphreak.de>
2016-07-20 18:04:43 +0200

    Fixed JavaDoc of TestContext.

--- simulator/src/main/java/com/hazelcast/simulator/test/TestContext.java
+++ simulator/src/main/java/com/hazelcast/simulator/test/TestContext.java
@@ -25,8 +25,6 @@ public interface TestContext {
     String LOCALHOST = "127.0.0.1";
 
     /**
-     *
-     * @return
      * @deprecated since 0.8. Use {@link com.hazelcast.simulator.test.annotations.InjectHazelcastInstance}.
      */
     HazelcastInstance getTargetInstance();
