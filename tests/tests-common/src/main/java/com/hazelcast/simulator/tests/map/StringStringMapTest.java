851896f1e2c92bcf70827b55d2a749d7e1785156
pveentjer <alarmnummer@gmail.com>
2016-07-20 09:25:49 +0300

    Converted StringStringMapTest to new Timestep approach

--- tests/tests-common/src/main/java/com/hazelcast/simulator/tests/map/StringStringMapTest.java
+++ tests/tests-common/src/main/java/com/hazelcast/simulator/tests/map/StringStringMapTest.java
@@ -16,17 +16,15 @@
 package com.hazelcast.simulator.tests.map;
 
 import com.hazelcast.core.IMap;
-import com.hazelcast.simulator.probes.Probe;
-import com.hazelcast.simulator.test.annotations.RunWithWorker;
+import com.hazelcast.simulator.test.AbstractTest;
+import com.hazelcast.simulator.test.BaseThreadContext;
 import com.hazelcast.simulator.test.annotations.Setup;
 import com.hazelcast.simulator.test.annotations.Teardown;
+import com.hazelcast.simulator.test.annotations.TimeStep;
 import com.hazelcast.simulator.test.annotations.Warmup;
-import com.hazelcast.simulator.test.AbstractTest;
 import com.hazelcast.simulator.tests.helpers.KeyLocality;
 import com.hazelcast.simulator.worker.loadsupport.Streamer;
 import com.hazelcast.simulator.worker.loadsupport.StreamerFactory;
-import com.hazelcast.simulator.worker.selector.OperationSelectorBuilder;
-import com.hazelcast.simulator.worker.tasks.AbstractWorkerWithMultipleProbes;
 
 import java.util.Random;
 
@@ -37,12 +35,6 @@ import static com.hazelcast.simulator.utils.GeneratorUtils.generateStrings;
 
 public class StringStringMapTest extends AbstractTest {
 
-    private enum Operation {
-        PUT,
-        SET,
-        GET
-    }
-
     // properties
     public int keyCount = 10000;
     public int valueCount = 10000;
@@ -50,23 +42,14 @@ public class StringStringMapTest extends AbstractTest {
     public int valueLength = 10;
     public KeyLocality keyLocality = KeyLocality.SHARED;
     public int minNumberOfMembers = 0;
-    public double putProb = 0.1;
-    public double setProb = 0.0;
-
-    private final OperationSelectorBuilder<Operation> operationSelectorBuilder = new OperationSelectorBuilder<Operation>();
 
     private IMap<String, String> map;
-
     private String[] keys;
     private String[] values;
 
     @Setup
     public void setUp() {
         map = targetInstance.getMap(name);
-
-        operationSelectorBuilder.addOperation(Operation.PUT, putProb)
-                .addOperation(Operation.SET, setProb)
-                .addDefaultOperation(Operation.GET);
     }
 
     @Warmup(global = false)
@@ -88,44 +71,27 @@ public class StringStringMapTest extends AbstractTest {
         streamer.await();
     }
 
-    @RunWithWorker
-    public Worker createWorker() {
-        return new Worker();
+    @TimeStep(prob = -1)
+    public void get(ThreadContext threadContext) {
+        String key = threadContext.randomKey();
+        map.get(key);
     }
 
-    private class Worker extends AbstractWorkerWithMultipleProbes<Operation> {
+    @TimeStep(prob = 0.1)
+    public void put(ThreadContext threadContext) {
+        String key = threadContext.randomKey();
+        String value = threadContext.randomValue();
+        map.put(key, value);
+    }
 
-        public Worker() {
-            super(operationSelectorBuilder);
-        }
+    @TimeStep(prob = 0)
+    public void set(ThreadContext threadContext) {
+        String key = threadContext.randomKey();
+        String value = threadContext.randomValue();
+        map.set(key, value);
+    }
 
-        @Override
-        protected void timeStep(Operation operation, Probe probe) throws Exception {
-            String key = randomKey();
-            long started;
-
-            switch (operation) {
-                case PUT:
-                    String value = randomValue();
-                    started = System.nanoTime();
-                    map.put(key, value);
-                    probe.done(started);
-                    break;
-                case SET:
-                    value = randomValue();
-                    started = System.nanoTime();
-                    map.set(key, value);
-                    probe.done(started);
-                    break;
-                case GET:
-                    started = System.nanoTime();
-                    map.get(key);
-                    probe.done(started);
-                    break;
-                default:
-                    throw new UnsupportedOperationException();
-            }
-        }
+    public class ThreadContext extends BaseThreadContext {
 
         private String randomKey() {
             return keys[randomInt(keys.length)];
@@ -141,6 +107,4 @@ public class StringStringMapTest extends AbstractTest {
         map.destroy();
         logger.info(getOperationCountInformation(targetInstance));
     }
-
-
 }
