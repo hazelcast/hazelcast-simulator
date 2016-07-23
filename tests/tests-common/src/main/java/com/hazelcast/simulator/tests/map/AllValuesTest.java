98b84ed5b45f66e216fc2c52eb660924e748aedd
pveentjer <alarmnummer@gmail.com>
2016-07-23 11:19:13 +0300

    Converted AllValuesTest to new timestep approach

--- tests/tests-common/src/main/java/com/hazelcast/simulator/tests/map/AllValuesTest.java
+++ tests/tests-common/src/main/java/com/hazelcast/simulator/tests/map/AllValuesTest.java
@@ -17,14 +17,13 @@ package com.hazelcast.simulator.tests.map;
 
 import com.hazelcast.core.IMap;
 import com.hazelcast.query.TruePredicate;
-import com.hazelcast.simulator.test.annotations.RunWithWorker;
+import com.hazelcast.simulator.test.AbstractTest;
 import com.hazelcast.simulator.test.annotations.Setup;
 import com.hazelcast.simulator.test.annotations.Teardown;
+import com.hazelcast.simulator.test.annotations.TimeStep;
 import com.hazelcast.simulator.test.annotations.Warmup;
-import com.hazelcast.simulator.test.AbstractTest;
 import com.hazelcast.simulator.worker.loadsupport.Streamer;
 import com.hazelcast.simulator.worker.loadsupport.StreamerFactory;
-import com.hazelcast.simulator.worker.tasks.AbstractMonotonicWorker;
 
 import java.util.Collection;
 
@@ -65,24 +64,17 @@ public class AllValuesTest extends AbstractTest {
         streamer.await();
     }
 
-    @RunWithWorker
-    public Worker createWorker() {
-        return new Worker();
-    }
-
-    private class Worker extends AbstractMonotonicWorker {
-
-        @Override
-        protected void timeStep() throws Exception {
-            Collection<String> result;
-            if (usePredicate) {
-                result = map.values(TruePredicate.INSTANCE);
-            } else {
-                result = map.values();
-            }
-
-            assertEquals(entryCount, result.size());
+    @TimeStep
+    public void timeStep() throws Exception {
+        // todo: it would be better to have 2 timestep methods so that optimized code can be generated.
+        Collection<String> result;
+        if (usePredicate) {
+            result = map.values(TruePredicate.INSTANCE);
+        } else {
+            result = map.values();
         }
+
+        assertEquals(entryCount, result.size());
     }
 
     @Teardown
