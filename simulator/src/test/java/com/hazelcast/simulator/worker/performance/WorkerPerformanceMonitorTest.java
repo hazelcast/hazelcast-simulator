c66aeee72c0a14d26c71e06d0b642dcaf69c253e
Donnerbart <github@thunderphreak.de>
2016-07-27 10:56:59 +0200

    Removed @Ignore from test which was failing due to the broken probe generation.

--- simulator/src/test/java/com/hazelcast/simulator/worker/performance/WorkerPerformanceMonitorTest.java
+++ simulator/src/test/java/com/hazelcast/simulator/worker/performance/WorkerPerformanceMonitorTest.java
@@ -16,7 +16,6 @@ import com.hazelcast.simulator.tests.SuccessTest;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -131,7 +130,6 @@ public class WorkerPerformanceMonitorTest {
     }
 
     @Test
-    @Ignore("Fails randomly when the probeMap is empty during TestPerformanceTracker init -> NPE on histogramLogWriterMap.get()")
     public void test_whenTestWithProbeWhichIsRunningWithDelay_thenSendPerformanceStates() {
         PerformanceMonitorProbeTest test = new PerformanceMonitorProbeTest();
         addTest(test, 200);
