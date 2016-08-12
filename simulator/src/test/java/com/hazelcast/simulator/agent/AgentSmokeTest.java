b5b0b4c751c401b93a507b553a85d5c694f2a1f5
pveentjer <alarmnummer@gmail.com>
2016-08-12 11:23:25 +0300

    Tests are now closing remote clients; since they now always have a running thread; they can hog resources.. memory retention + consuming cpu.. + I have no ide of the pending ping messages are going to pile up

--- simulator/src/test/java/com/hazelcast/simulator/agent/AgentSmokeTest.java
+++ simulator/src/test/java/com/hazelcast/simulator/agent/AgentSmokeTest.java
@@ -107,7 +107,6 @@ public class AgentSmokeTest implements FailureListener {
 
         remoteClient = new RemoteClient(coordinatorConnector, componentRegistry, (int) SECONDS.toMillis(10));
 
-
         failureCollector.addListener(true, new FailureListener() {
             @Override
             public void onFailure(FailureOperation failure, boolean isFinishedFailure, boolean isCritical) {
