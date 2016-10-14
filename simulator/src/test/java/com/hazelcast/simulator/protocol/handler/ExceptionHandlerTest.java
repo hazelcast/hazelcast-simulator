7c7abc232180ef7c98a1ce3fd53daefb055bb509
Donnerbart <github@thunderphreak.de>
2016-10-14 11:18:00 +0200

    Fixed ExceptionHandlerTest.

--- simulator/src/test/java/com/hazelcast/simulator/protocol/handler/ExceptionHandlerTest.java
+++ simulator/src/test/java/com/hazelcast/simulator/protocol/handler/ExceptionHandlerTest.java
@@ -4,6 +4,8 @@ import com.hazelcast.simulator.protocol.connector.ServerConnector;
 import com.hazelcast.simulator.protocol.core.SimulatorAddress;
 import com.hazelcast.simulator.protocol.operation.FailureOperation;
 import com.hazelcast.simulator.test.TestException;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,11 +18,16 @@ import static org.mockito.Mockito.when;
 
 public class ExceptionHandlerTest {
 
+    private ChannelHandlerContext ctx;
+
     private ServerConnector serverConnector;
     private ExceptionHandler exceptionHandler;
 
     @Before
     public void setUp() {
+        ctx = mock(ChannelHandlerContext.class);
+        when(ctx.channel()).thenReturn(mock(Channel.class));
+
         serverConnector = mock(ServerConnector.class);
         when(serverConnector.getAddress()).thenReturn(SimulatorAddress.COORDINATOR);
 
@@ -31,7 +38,7 @@ public class ExceptionHandlerTest {
     public void testExceptionCaught() throws Exception {
         Exception cause = new TestException("expected");
 
-        exceptionHandler.exceptionCaught(null, cause);
+        exceptionHandler.exceptionCaught(ctx, cause);
 
         verify(serverConnector).getAddress();
         verify(serverConnector).submit(eq(SimulatorAddress.COORDINATOR), any(FailureOperation.class));
