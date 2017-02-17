010345a2e3cf3fd730cd226edb711251af2b4ddb
Donnerbart <github@thunderphreak.de>
2017-02-17 19:39:37 +0100

    Cleanup of install bash scripts

--- dist/src/main/dist/conf/install-hazelcast-enterprise.sh
+++ dist/src/main/dist/conf/install-hazelcast-enterprise.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#
+# Script to install Hazelcast Enterprise
+# 
+# The installation can be modified by copying it into the working directory.
+# All properties from the 'simulator.properties' are passed as environment variables.
+#
 
-. $SIMULATOR_HOME/conf/install-hazelcast-support.sh
+. ${SIMULATOR_HOME}/conf/install-hazelcast-support.sh
 
 session_id=$1
 version_spec=$2
@@ -8,27 +14,27 @@ public_ips=$3
 
 prepare()
 {
-    echo VERSION_SPEC $version_spec
+    echo VERSION_SPEC ${version_spec}
 
-    if  [[ $version_spec == maven* ]] ; then
+    if  [[ ${version_spec} == maven* ]] ; then
         version=${version_spec#*=}
 
-        echo Maven using $version
+        echo "Maven using $version"
 
         snapshot_repo="https://repository-hazelcast-l337.forge.cloudbees.com/snapshot"
         release_repo="https://repository-hazelcast-l337.forge.cloudbees.com/release"
 
-        prepare_using_maven "hazelcast-enterprise-all" "$version" $release_repo $snapshot_repo
-    elif [[ $version_spec == git* ]] ; then
+        prepare_using_maven "hazelcast-enterprise-all" "$version" ${release_repo} ${snapshot_repo}
+    elif [[ ${version_spec} == git* ]] ; then
         git_branch=${version_spec#*=}
 
-        echo Git using $git_branch
+        echo "Git using $git_branch"
 
-        prepare_using_git $git_branch $git_build_dir/hazelcast-enterprise git@github.com:hazelcast/hazelcast-enterprise.git
-    elif [[ $version_spec == bringmyown ]] ; then
-        echo Bring my own
-        # we don't need to do anything.
-    elif [[ $version_spec == outofthebox ]] ; then
+        prepare_using_git ${git_branch} ${git_build_dir}/hazelcast-enterprise git@github.com:hazelcast/hazelcast-enterprise.git
+    elif [[ ${version_spec} == bringmyown ]] ; then
+        echo "Bring my own"
+        # we don't need to do anything
+    elif [[ ${version_spec} == outofthebox ]] ; then
         echo "Aborting, VERSION_SPEC 'outofthebox' is not supported for hazelcast-enterprise"
         exit 1
     else
