16757141e34f02b9b1be46c29ca1a39da0101d22
Donnerbart <github@thunderphreak.de>
2017-02-17 19:44:33 +0100

    Cleanup of after-completion.sh

--- dist/src/main/dist/conf/after-completion.sh
+++ dist/src/main/dist/conf/after-completion.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
-
-# This script is executed after the coordinator completes executing a test-suite and optionally has downloaded the artifacts.
+#
+# This script is executed after the Coordinator completes executing a test-suite and optionally has downloaded the artifacts.
 # It will be executed 'locally'; on the machine that ran the executor, and not remotely.
 #
-# This script can be used for post processing e.g. HDR file conversion.
-#
+# This script can be used for post processing, e.g. HDR file conversion.
 # This script can be copied into the working directory and modified.
-
+#
 
 # exit on failure
 set -e
-
-# Printing the command being executed (useful for debugging)
+# printing the command being executed (useful for debugging)
 #set -x
 
 dir=$1
 if [ ! -d "${dir}" ]; then
-    echo Directory $dir does not exist. Exiting silently.
+    echo "Directory $dir does not exist. Exiting silently."
     exit 0
 fi
 
-# Merge all hdr files of each member into a hdr file which gets stored in the dir.
-probes=($(ls -R $dir | grep .hdr | sort | uniq))
+# merge all hdr files of each member into a hdr file which gets stored in the dir
+probes=($(ls -R ${dir} | grep .hdr | sort | uniq))
 for probe in "${probes[@]}"
 do
-    echo Processing $probe
+    echo "Processing $probe"
 
-    hdr_files=($(find $dir | grep $probe))
+    hdr_files=($(find ${dir} | grep ${probe}))
 
    java -cp "${SIMULATOR_HOME}/lib/*" com.hazelcast.simulator.utils.HistogramLogMerger "${dir}/${probe}" "${hdr_files[@]}"
 done
 
-
-# Convert all hdr files to hgrm files so they can easily be plot usingdist/src/main/dist/conf/after-completion.sh:31
+# convert all hdr files to hgrm files so they can easily be plot using
 # http://hdrhistogram.github.io/HdrHistogram/plotFiles.html
-hdr_files=($(find $dir -name *.hdr))
+hdr_files=($(find ${dir} -name *.hdr))
 for hdr_file in "${hdr_files[@]}"
 do
     file_name="${hdr_file%.*}"
