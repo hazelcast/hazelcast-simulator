fc105995a67d5df6c8dcaf91f9d8af4a1b7d24c4
Donnerbart <github@thunderphreak.de>
2017-02-17 19:39:37 +0100

    Cleanup of install bash scripts

--- dist/src/main/dist/conf/install-hazelcast-support.sh
+++ dist/src/main/dist/conf/install-hazelcast-support.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 #
-# Script to install Hazelcast. The installation can be modified by copying it into the working directory.
+# Support script to install Hazelcast.
 #
+# The installation can be modified by copying it into the working directory.
 # All properties from the 'simulator.properties' are passed as environment variables.
 #
 
@@ -12,44 +12,43 @@ set -e
 #set -x
 
 local_upload_dir=upload
-ssh_options=$SSH_OPTIONS
-user=$USER
+ssh_options=${SSH_OPTIONS}
+user=${USER}
 
 # we limit the number of concurrent uploads
 max_current_uploads=2
 
 # setting the right maven executable
-eval git_build_dir=$GIT_BUILD_DIR
+eval git_build_dir=${GIT_BUILD_DIR}
 
 local_build_cache=${git_build_dir}/build-cache
 
-# we create a tmp directory for all the artifacts
-# for more info about this command see:
+# we create a tmp directory for all the artifacts, for more info about this command see:
 # http://unix.stackexchange.com/questions/30091/fix-or-alternative-for-mktemp-in-os-x
 tmp_dir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
 local_install_dir="$tmp_dir/lib"
 
 function real_path {
     local r=$1; local t=$(readlink ${r})
-    while [ $t ]; do
+    while [ ${t} ]; do
         r=$(cd $(dirname ${r}) && cd $(dirname ${t}) && pwd -P)/$(basename ${t})
         t=$(readlink ${r})
     done
     echo ${r}
 }
 
-simulator_basename=($(real_path ${SIMULATOR_HOME} | xargs basename))
+simulator_basename=$(real_path ${SIMULATOR_HOME} | xargs basename)
 
 download() {
     url=$1 # the url of the artifact to download
     dir=$2 # the directory the file should be downloaded
 
     if type "wget" >> /dev/null 2>&1; then
-        wget --no-verbose --directory-prefix=$dir $url
+        wget --no-verbose --directory-prefix=${dir} ${url}
     else
         pushd .
-        cd $dir
-        curl -O $url
+        cd ${dir}
+        curl -O ${url}
         popd
     fi
 }
@@ -62,94 +61,90 @@ prepare_using_maven() {
 
     maven_repo="$HOME/.m2/repository"
 
-    destination=$local_install_dir/maven-$version
-    mkdir -p $destination
+    destination=${local_install_dir}/maven-${version}
+    mkdir -p ${destination}
 
     hazelcast_jar="$maven_repo/com/hazelcast/${artifact_id}/${version}/${artifact_id}-${version}.jar"
     if [[ -f "${hazelcast_jar}" ]] ; then
         # first we look in the local repo
         echo "Found $hazelcast_jar in local repo, to $destination"
-        cp $hazelcast_jar $destination
+        cp ${hazelcast_jar} ${destination}
         return
     fi
 
-    # The artifact is not found in the local repo; so now we need to download it.
-
+    # The artifact is not found in the local repo; so now we need to download it
     echo "$hazelcast_jar is not found in local repo; downloading"
     url=""
-    if  [[ $version == *SNAPSHOT ]] ; then
-        # it is a snapshot. These are a bit more complex because we need to download maven-metadata first
-        # to figure out the correct url
+    if  [[ ${version} == *SNAPSHOT ]] ; then
+        # snapshots are a bit more complex because we need to download maven-metadata first to figure out the correct url
+        download ${snapshot_repo}/com/hazelcast/${artifact_id}/${version}/maven-metadata.xml ${local_install_dir}
 
-        download $snapshot_repo/com/hazelcast/$artifact_id/$version/maven-metadata.xml $local_install_dir
-
-        version_no_snapshot=${version%-SNAPSHOT}
-        buildNumber=($(grep -o -P '(?<=buildNumber>).*(?=</buildNumber)' $local_install_dir/maven-metadata.xml))
-        timeStamp=($(grep -o -P '(?<=timestamp>).*(?=</timestamp)' $local_install_dir/maven-metadata.xml))
+        baseVersion=${version%-SNAPSHOT}
+        buildNumber=$(grep -o -P '(?<=buildNumber>).*(?=</buildNumber)' ${local_install_dir}/maven-metadata.xml)
+        timeStamp=$(grep -o -P '(?<=timestamp>).*(?=</timestamp)' ${local_install_dir}/maven-metadata.xml)
 
         # cleanup the garbage
-        rm $local_install_dir/maven-metadata.xml
+        rm ${local_install_dir}/maven-metadata.xml
 
-        url=$snapshot_repo/com/hazelcast/$artifact_id/$version/$artifact_id-$version_no_snapshot-$timeStamp-$buildNumber.jar
+        url=${snapshot_repo}/com/hazelcast/${artifact_id}/${version}/${artifact_id}-${baseVersion}-${timeStamp}-${buildNumber}.jar
     else
-        url=$release_repo/com/hazelcast/$artifact_id/$version/$artifact_id-$version.jar
+        url=${release_repo}/com/hazelcast/${artifact_id}/${version}/${artifact_id}-${version}.jar
     fi
 
     echo "Copying $url to $destination"
-    download $url  $destination
+    download ${url} ${destination}
 }
 
 prepare_using_git() {
-    git_branch=$1       # the branch to check out
-    local_repo=$2       # the local directory to check out to
-    repo_url=$3         # the remote repository
+    git_branch=$1 # the branch to check out
+    local_repo=$2 # the local directory to check out to
+    repo_url=$3   # the remote repository
 
-    echo Git using $git_branch
+    echo "Git using $git_branch"
 
-    #first we make sure the repo is cloned.
-    if [ -d $local_repo ]; then
+    # first we make sure the repo is cloned
+    if [ -d ${local_repo} ]; then
         echo "Local git repo $local_repo exist skipping clone"
     else
         echo "Local git repo $local_repo does not exist, cloning $repo_url"
-        mkdir -p $git_build_dir
-        git clone $repo_url $local_repo
+        mkdir -p ${git_build_dir}
+        git clone ${repo_url} ${local_repo}
     fi
 
     pushd .
 
-    cd $local_repo
+    cd ${local_repo}
 
-    # todo: for enterprise you need different set or repo's
-    # make sure all remote repo's are added.
+    # TODO: for enterprise you need different set or repo
+    # make sure all remote repos are added
     for remote in ${GIT_CUSTOM_REPOSITORIES//,/ } ; do
         name=${remote%%=*}
         url=${remote#*=}
-
     done
 
     git fetch --all --tags
     git reset --hard
-    git checkout $git_branch
+    git checkout ${git_branch}
 
-    hash=($(git rev-parse HEAD))
+    hash=$(git rev-parse HEAD)
 
-    #getting a cached artifact, or else build it
-    if [ -d $local_build_cache/$hash ]; then
+    # getting a cached artifact, or else build it
+    if [ -d ${local_build_cache}/${hash} ]; then
         echo "$hash cached version available, skipping build"
     else
         echo "$hash cached version not available, building"
 
-        if [ -z "$MVN_EXECUTABLE" ]; then mvn="mvn"; else eval mvn=$MVN_EXECUTABLE; fi
+        if [ -z "$MVN_EXECUTABLE" ]; then mvn="mvn"; else eval mvn=${MVN_EXECUTABLE}; fi
 
-        $mvn --quiet clean package -DskipTests
-        mkdir -p $local_build_cache/$hash
+        ${mvn} --quiet clean package -DskipTests
+        mkdir -p ${local_build_cache}/${hash}
 
         jars=($(find . -name *.jar | grep --invert-match 'sources\|tests\|original\|build-utils/'))
         for jar in "${jars[@]}" ; do
-            cp $jar $local_build_cache/$hash
+            cp ${jar} ${local_build_cache}/${hash}
         done
 
-        $mvn clean
+        ${mvn} clean
 
         echo "$hash build and cached successfully"
     fi
@@ -160,19 +155,21 @@ prepare_using_git() {
     encoded_git_branch=${git_branch//\//\\\\}
 
     # copy from the cache into the local_install_dir
-    destination=$local_install_dir/git-${encoded_git_branch}
-    mkdir -p $destination
-    jars=($(find $local_build_cache/$hash -name *.jar))
+    destination=${local_install_dir}/git-${encoded_git_branch}
+    mkdir -p ${destination}
+    searchPath=${local_build_cache}/${hash}
+    jars=($(find ${searchPath} -name *.jar))
     for jar in "${jars[@]}" ; do
         echo "Copying file $jar to $destination"
-        cp $jar $destination
+        cp ${jar} ${destination}
     done
 }
 
 # throttles the number of concurrent uploads by limiting the number of child processes
 throttle_concurrent_uploads() {
     while test $(jobs -p | wc -w) -ge "$max_current_uploads"; do
-        wait -n || true # -n is not supported on older bash versions; so we can't throttle there
+        # -n is not supported on older bash versions; so we can't throttle there
+        wait -n || true
     done
 }
 
@@ -180,47 +177,47 @@ throttle_concurrent_uploads() {
 upload_to_single_agent() {
     public_ip=$1
 
-    remote_hz_lib=$simulator_basename/hz-lib
-    remote_run_dir=$simulator_basename/workers/$session_id
-    remote_lib_dir=$remote_run_dir/lib/
-    remote_upload_dir=$remote_run_dir/upload
+    remote_hz_lib=${simulator_basename}/hz-lib
+    remote_run_dir=${simulator_basename}/workers/${session_id}
+    remote_lib_dir=${remote_run_dir}/lib/
+    remote_upload_dir=${remote_run_dir}/upload
 
     # if the local upload directory exist, it needs to be uploaded
-    if [ -d $local_upload_dir ]; then
-        echo Uploading upload directory $local_upload_dir to $public_ip:remote_upload_dir
-        ssh $ssh_options $user@$public_ip "mkdir -p $remote_upload_dir"
-        scp $ssh_options -r $local_upload_dir simulator@${public_ip}:${remote_upload_dir}
+    if [ -d ${local_upload_dir} ]; then
+        echo "Uploading upload directory $local_upload_dir to $public_ip:remote_upload_dir"
+        ssh ${ssh_options} ${user}@${public_ip} "mkdir -p $remote_upload_dir"
+        scp ${ssh_options} -r ${local_upload_dir} simulator@${public_ip}:${remote_upload_dir}
     fi
 
-    echo Uploading Hazelcast $local_install_dir to $public_ip:$remote_hz_lib
-    ssh $ssh_options $user@$public_ip "mkdir -p $remote_hz_lib"
+    echo "Uploading Hazelcast $local_install_dir to $public_ip:$remote_hz_lib"
+    ssh ${ssh_options} ${user}@${public_ip} "mkdir -p $remote_hz_lib"
 
     # in the local_install_dir multiple directories could be created e.g. git=master, maven=3.8. Each of these
     # directories we want to upload; but we do not want to remove other non conflicting directories.
-    for dir in $(find $local_install_dir -maxdepth 1 -type d); do
+    for dir in $(find ${local_install_dir} -maxdepth 1 -type d); do
         # sync the directory and put the task in the background
-         rsync --checksum -avv --delete -L -e "ssh $ssh_options" $local_install_dir/* $user@$public_ip:$remote_hz_lib
+         rsync --checksum -avv --delete -L -e "ssh $ssh_options" ${local_install_dir}/* ${user}@${public_ip}:${remote_hz_lib}
     done
 }
 
 # uploads the installation files to all agents
 upload() {
-    # if there are no provided public ip's, then it is a local install
+    # if there are no provided public ips, then it is a local install
     if [ -z "$public_ips" ] ; then
         echo "Local install"
-        mkdir -p $SIMULATOR_HOME/workers/$session_id/lib
-        cp -r $local_install_dir $SIMULATOR_HOME/workers/$session_id/lib
+        mkdir -p ${SIMULATOR_HOME}/workers/${session_id}/lib
+        cp -r ${local_install_dir} ${SIMULATOR_HOME}/workers/${session_id}/lib
         return
     fi
 
-    # it is a remote install; so upload to each of the public ip's
+    # it is a remote install; so upload to each of the public ips
     # the public_ips is a comma separated list
     # we execute the uploading in parallel
     for public_ip in ${public_ips//,/ } ; do
-        #throttle_concurrent_uploads
-        upload_to_single_agent $public_ip &
+        # throttle_concurrent_uploads
+        upload_to_single_agent ${public_ip} &
     done
 
-    # wait for all uploads to complete.
+    # wait for all uploads to complete
     wait
 }
--- dist/src/main/dist/conf/install-hazelcast.sh
+++ dist/src/main/dist/conf/install-hazelcast.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #
-# Script to install Hazelcast. The installation can be modified by copying it into the working directory.
-#
+# Script to install Hazelcast
+# 
+# The installation can be modified by copying it into the working directory.
 # All properties from the 'simulator.properties' are passed as environment variables.
 #
 # TODO:
-# -
-# - selecting the right os jars
-# - selecting the right enterprise jars
 # - use rsync instead of scp for upload
 # - if downloading of maven artifacts fails, no error
 # - variables to be used in quotes
@@ -20,42 +18,41 @@
 # - instead of download artifacts to local machine and then uploading; download them on the target machine
 # - instead of uploading to each machine, upload to a single machine and then download from that machine
 
-. $SIMULATOR_HOME/conf/install-hazelcast-support.sh
+. ${SIMULATOR_HOME}/conf/install-hazelcast-support.sh
 
 session_id=$1
 version_spec=$2
 public_ips=$3
 
-# building of hazelcast using mvn/git if needed
 prepare()
 {
-     if  [[ $version_spec == maven* ]] ; then
+     if  [[ ${version_spec} == maven* ]] ; then
         maven_version=${version_spec#*=}
 
-        echo Maven using $maven_version
+        echo "Maven using $maven_version"
 
         snapshot_repo="https://oss.sonatype.org/content/repositories/snapshots"
         release_repo="https://oss.sonatype.org/content/repositories/releases"
 
-        prepare_using_maven "hazelcast" "$maven_version" $release_repo $snapshot_repo
-        prepare_using_maven "hazelcast-client" "$maven_version" $release_repo $snapshot_repo
-    elif [[ $version_spec == git* ]] ; then
+        prepare_using_maven "hazelcast" "$maven_version" ${release_repo} ${snapshot_repo}
+        prepare_using_maven "hazelcast-client" "$maven_version" ${release_repo} ${snapshot_repo}
+    elif [[ ${version_spec} == git* ]] ; then
         git_branch=${version_spec#*=}
 
-        echo Git using $git_branch
+        echo Git using ${git_branch}
 
-        prepare_using_git $git_branch $git_build_dir/hazelcast-os https://github.com/hazelcast/hazelcast.git
-    elif [[ $version_spec == bringmyown ]] ; then
-        echo Bring my own
-        # we don't need to do anything.
-    elif [[ $version_spec == outofthebox ]] ; then
-        echo Out of the box
+        prepare_using_git ${git_branch} ${git_build_dir}/hazelcast-os https://github.com/hazelcast/hazelcast.git
+    elif [[ ${version_spec} == bringmyown ]] ; then
+        echo "Bring my own"
+        # we don't need to do anything
+    elif [[ ${version_spec} == outofthebox ]] ; then
+        echo "Out of the box"
 
-        jars=($(find $SIMULATOR_HOME/lib -name hazelcast*.jar))
-        mkdir -p $local_install_dir/outofthebox
+        jars=($(find ${SIMULATOR_HOME}/lib -name hazelcast*.jar))
+        mkdir -p ${local_install_dir}/outofthebox
         for jar in "${jars[@]}" ; do
             echo "Copying file $jar"
-            cp $jar $local_install_dir/outofthebox
+            cp ${jar} ${local_install_dir}/outofthebox
         done
     else
         echo "Aborting, invalid VERSION_SPEC [$version_spec]"
