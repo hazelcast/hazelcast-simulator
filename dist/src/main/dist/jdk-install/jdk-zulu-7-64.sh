3025297dc983cf265743bb7d643754fd5cb2782b
Donnerbart <github@thunderphreak.de>
2017-02-28 10:58:03 +0100

    Fixed IBM and Zulu JDK install scripts

--- dist/src/main/dist/jdk-install/jdk-zulu-7-64.sh
+++ dist/src/main/dist/jdk-install/jdk-zulu-7-64.sh
@@ -3,11 +3,4 @@
 set -e
 source jdk-support.sh
 
-installPackage wget
-installPackage tar
-
-cd ~
-wget --no-verbose --referer=http://www.azulsystems.com/products/zulu/downloads http://cdn.azulsystems.com/zulu/bin/zulu1.7.0_91-7.12.0.3-x86lx64.zip -O zulu-7.zip
-unzip zulu-7.zip
-
-addJavaHome "~/zulu1.7.0_91-7.12.0.3-x86lx64"
+installZuluJdk 7 "http://cdn.azul.com/zulu/bin/zulu7.17.0.5-jdk7.0.131-linux_x64.tar.gz" "~/zulu7.17.0.5-jdk7.0.131-linux_x64"
