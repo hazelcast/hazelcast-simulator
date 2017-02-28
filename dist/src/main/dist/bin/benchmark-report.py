800b4db737389cc950497330e0f57f86364b8649
Peter Veentjer <alarmnummer@gmail.com>
2017-02-28 21:08:44 +0200

    Added a plot with throughput of all workers

--- dist/src/main/dist/bin/benchmark-report.py
+++ dist/src/main/dist/bin/benchmark-report.py
@@ -15,6 +15,7 @@
 #
 # done:
 # - better commandline help
+# - throughput per worker in a single plot
 #
 # backlog
 # - google chart option
@@ -26,16 +27,16 @@
 # - option not to make low part of graph shrink
 # - option to show real time
 
+import argparse
 import csv
 import os
 import re
 import tempfile
-import argparse
 
 parser = argparse.ArgumentParser(description='Creating a benchmark report from one or more benchmarks.')
 parser.add_argument('benchmarks', metavar='B', nargs='+',
                     help='a benchmark to be used in the comparison')
-#parser.add_argument('-r', '--realtime', default='report', help='print the real time of the datapoints.')
+# parser.add_argument('-r', '--realtime', default='report', help='print the real time of the datapoints.')
 parser.add_argument('-o', '--output', nargs=1,
                     help='The output directory for the report. By default a report directory in the working directory is created.')
 
@@ -48,6 +49,7 @@ else:
     output_dir = x.output[0]
 
 print("output directory '" + output_dir + "'")
+
 
 # ================ html ========================
 
@@ -86,7 +88,6 @@ def dump(obj):
         print "obj.%s = %s" % (attr, getattr(obj, attr))
 
 
-
 def ensure_dir(file_path):
     if not os.path.exists(file_path):
         os.makedirs(file_path)
@@ -101,12 +102,13 @@ class Gnuplot:
     ylabel = None
     is_bytes = None
 
-    def __init__(self, directory, title):
+    def __init__(self, directory, title, basefilename=None):
         self.tmp = tempfile.NamedTemporaryFile(delete=False)
         self.title = title
         self.directory = directory
         self.ts_list = []
         self.titles = {}
+        self.basefilename = basefilename
 
     def _complete(self):
         self.tmp.flush()
@@ -129,29 +131,34 @@ class Gnuplot:
                 break
 
         if empty:
-            print("Skipping plot of " + self.title + "; timeseries are empty")
+            # print("Skipping plot of " + self.title + "; timeseries are empty")
             return
 
         ts_first = self.ts_list[0]
         self.ylabel = ts_first.ylabel
-        self.filepath = os.path.join(self.directory, ts_first.name + ".png")
+
+        if self.basefilename:
+            self.filepath = os.path.join(self.directory, self.basefilename + ".png")
+        else:
+            self.filepath = os.path.join(self.directory, ts_first.name + ".png")
+
         self.is_bytes = ts_first.is_bytes
 
         ensure_dir(self.directory)
-        self.do_plot()
+        self._plot()
 
         print(self.filepath)
 
-    def do_plot(self):
+    def _plot(self):
         raise NotImplementedError("Please Implement this method")
 
 
 class TimeseriesGnuplot(Gnuplot):
-    def __init__(self, directory, title):
-        Gnuplot.__init__(self, directory, title)
+    def __init__(self, directory, title, basefilename=None):
+        Gnuplot.__init__(self, directory, title, basefilename)
 
-    def do_plot(self):
-        #self._write("unset autoscale y")
+    def _plot(self):
+        # self._write("unset autoscale y")
         self._write("set title '" + self.title + "'")
         self._write("set style data lines")
         self._write('set datafile separator ","')
@@ -170,7 +177,7 @@ class TimeseriesGnuplot(Gnuplot):
             # the problem here is that this is 1000 based; not 1024
             self._write("set format y '%.1s%cB'")
 
-        #else:
+        # else:
         #    self._write("set format y '%.0f'")
 
         self._write("set output '" + self.filepath + "'")
@@ -190,11 +197,12 @@ class TimeseriesGnuplot(Gnuplot):
             self._write("   \'" + ts_file.name + "\' using (t0(timecolumn(1))):2 " + title_str + ", \\")
         self._complete()
 
+
 class LatencyDistributionGnuplot(Gnuplot):
     def __init__(self, directory, title):
         Gnuplot.__init__(self, directory, title)
 
-    def do_plot(self):
+    def _plot(self):
         self._write("set datafile separator \",\"")
         self._write("set title '" + self.title + "'")
         self._write("set terminal png size " + str(self.image_width) + "," + str(self.image_height))
@@ -333,6 +341,7 @@ class Series:
                 result = item.value
         return result
 
+
 # A key/value in a series
 class KeyValue:
     time = None
@@ -346,7 +355,6 @@ class KeyValue:
 # A handle to a series. With a handle you can refer to a series, without needing to pull it into memory. Since we could have
 # a lot of measured data, we want to prevent getting it all in memory.
 class SeriesHandle:
-
     def __init__(self, src, name, title, ylabel, load_method, args=None, is_bytes=False):
         if not args:
             args = []
@@ -477,7 +485,7 @@ class Worker:
                 for x in range(0, 8):
                     next(csvreader)
                 for row in csvreader:
-                    result.append(KeyValue(row[0], float(row[5])+float(row[6])))
+                    result.append(KeyValue(row[0], float(row[5]) + float(row[6])))
         return result
 
 
@@ -507,7 +515,7 @@ class Benchmark:
 
         # making sure there are workers; otherwise it is an invalid benchmark
         if len(self.workers) == 0:
-            print("Invalid Benchmark "+self.name+" from directory ["+self.src_dir+"]; no workers found")
+            print("Invalid Benchmark " + self.name + " from directory [" + self.src_dir + "]; no workers found")
             exit(1)
 
         # look for all latency info
@@ -524,7 +532,7 @@ class Benchmark:
             file_path = os.path.join(self.src_dir, file_name)
             print(file_path)
 
-            name=file_name.split('-')[1]
+            name = file_name.split('-')[1]
 
             refs.append(SeriesHandle("latency", "latency_interval_25_" + name, "Interval 25%", "Latency (μs)",
                                      self.load_latency_ts, args=[file_path, 3]))
@@ -544,17 +552,19 @@ class Benchmark:
                                      self.load_latency_ts, args=[file_path, 10]))
 
             refs.append(SeriesHandle("latency", "latency_interval_min_" + name, "Interval Min", "Latency (μs)",
-                                     self.load_latency_ts,args=[file_path, 11]))
+                                     self.load_latency_ts, args=[file_path, 11]))
             refs.append(SeriesHandle("latency", "latency_interval_max_" + name, "Interval Max", "Latency (μs)",
                                      self.load_latency_ts, args=[file_path, 12]))
             refs.append(SeriesHandle("latency", "latency_interval_mean_" + name, "Interval Mean", "Latency (μs)",
-                                     self.load_latency_ts,args=[file_path, 13]))
-            refs.append(SeriesHandle("latency", "latency_interval_std_deviation_" + name, "Interval Standard Deviation", "Latency (μs)",
-                                     self.load_latency_ts, args=[file_path, 14]))
+                                     self.load_latency_ts, args=[file_path, 13]))
+            refs.append(
+                SeriesHandle("latency", "latency_interval_std_deviation_" + name, "Interval Standard Deviation", "Latency (μs)",
+                             self.load_latency_ts, args=[file_path, 14]))
 
             hgrm_path = os.path.join(src_dir, file_name + ".hgrm")
-            refs.append(SeriesHandle("latency-distribution", "latency_distribution_" + name, "Latency distribution", "Latency (μs)",
-                                     self.load_latency_distribution_ts, args=[hgrm_path]))
+            refs.append(
+                SeriesHandle("latency-distribution", "latency_distribution_" + name, "Latency distribution", "Latency (μs)",
+                             self.load_latency_distribution_ts, args=[hgrm_path]))
 
         agents = {}
         for worker in self.workers:
@@ -565,7 +575,8 @@ class Benchmark:
         for agent, worker in agents.iteritems():
             for ref in worker.ts_references:
                 if ref.src == "dstat":
-                    refs.append(SeriesHandle("dstat",ref.name+"_"+agent,ref.title, ref.ylabel,self.x, args=[ref], is_bytes=ref.is_bytes))
+                    refs.append(SeriesHandle("dstat", ref.name + "_" + agent, ref.title, ref.ylabel, self.x, args=[ref],
+                                             is_bytes=ref.is_bytes))
 
     def x(self, ref):
         return ref.load().items
@@ -623,7 +634,6 @@ class Benchmark:
 
 
 class Comparison:
-
     def __init__(self):
         benchmark_dirs = []
         benchmark_names = {}
@@ -635,16 +645,16 @@ class Comparison:
         for arg in args:
             if arg.startswith("[") and arg.endswith("]"):
                 if not last_benchmark:
-                    print("Benchmark name "+arg+" must be preceded with a benchmark directory.")
+                    print("Benchmark name " + arg + " must be preceded with a benchmark directory.")
                     exit()
-                benchmark_names[last_benchmark] = arg[1:len(arg)-1]
+                benchmark_names[last_benchmark] = arg[1:len(arg) - 1]
                 last_benchmark = None
             else:
                 benchmark_dir = arg
                 if not os.path.exists(benchmark_dir):
                     print("benchmark directory '" + benchmark_dir + "' does not exist!")
                     exit(1)
-                last_benchmark=arg
+                last_benchmark = arg
                 benchmark_dirs.append(benchmark_dir)
                 name = os.path.basename(os.path.normpath(benchmark_dir))
                 benchmark_names[benchmark_dir] = name
@@ -659,7 +669,7 @@ class Comparison:
 
         for benchmark in self.benchmarks:
             if len(benchmark.ts_references) == 0:
-                print(" benchmark ["+benchmark.name+"] benchmark.dir ["+benchmark.src_dir+"] has no data")
+                print(" benchmark [" + benchmark.name + "] benchmark.dir [" + benchmark.src_dir + "] has no data")
                 exit(1)
 
             for ref in benchmark.ts_references:
@@ -674,16 +684,29 @@ class Comparison:
 
                 plot.add(ref.load(), title=benchmark.name)
 
+        for benchmark in self.benchmarks:
+            for worker in benchmark.workers:
+                for ref in worker.ts_references:
+                    if ref.src == "throughput":
+                        plot = plots.get("throughput_per_worker")
+                        if not plot:
+                            plot = TimeseriesGnuplot(output_dir,
+                                                     "Throughput per member",
+                                                     basefilename="throughput_per_worker")
+                            plots["throughput_per_worker"] = plot
+
+                        if len(self.benchmarks)>1:
+                            plot.add(ref.load(), benchmark.name + "_" + worker.name)
+                        else:
+                            plot.add(ref.load(), worker.name)
+
         for plot in plots.values():
             plot.plot()
 
         print("Done writing report [" + output_dir + "]")
         for benchmark in self.benchmarks:
-            print(" benchmark ["+benchmark.name+"] benchmark.dir ["+benchmark.src_dir+"]")
-
+            print(" benchmark [" + benchmark.name + "] benchmark.dir [" + benchmark.src_dir + "]")
 
 
 comparison = Comparison()
 comparison.compare()
-
-
