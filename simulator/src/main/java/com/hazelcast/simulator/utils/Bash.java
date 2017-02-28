2b4cc230f3c24c87c4c136fc166ac7fe9ec6d8e9
Donnerbart <github@thunderphreak.de>
2017-02-28 18:19:33 +0100

    Filtered -t from SCP options

--- simulator/src/main/java/com/hazelcast/simulator/utils/Bash.java
+++ simulator/src/main/java/com/hazelcast/simulator/utils/Bash.java
@@ -24,12 +24,14 @@ import static java.lang.String.format;
 
 public class Bash {
 
-    private final String sshOptions;
     private final String user;
+    private final String sshOptions;
+    private final String scpOptions;
 
     public Bash(SimulatorProperties simulatorProperties) {
-        this.sshOptions = simulatorProperties.getSshOptions();
         this.user = simulatorProperties.getUser();
+        this.sshOptions = simulatorProperties.getSshOptions();
+        this.scpOptions = sshOptions.replace("-t ", "");
     }
 
     public String execute(String command) {
@@ -64,8 +66,7 @@ public class Bash {
     }
 
     public void scpToRemote(String ip, File src, String target) {
-        String command = format("scp -r %s %s %s@%s:%s", sshOptions, src.getAbsolutePath(), user, ip, target);
+        String command = format("scp -r %s %s %s@%s:%s", scpOptions, src.getAbsolutePath(), user, ip, target);
         execute(command);
     }
 }
-
