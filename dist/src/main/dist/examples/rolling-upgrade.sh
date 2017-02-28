4ffd8d295d72a89f37c315a8e180ee25e5d43781
Donnerbart <github@thunderphreak.de>
2017-02-28 12:27:02 +0100

    Updated rolling-upgrade.sh with HZ 3.8 and 3.9-SNAPSHOT versions

--- dist/src/main/dist/examples/rolling-upgrade.sh
+++ dist/src/main/dist/examples/rolling-upgrade.sh
@@ -1,55 +1,58 @@
 #!/bin/bash
-
 #
-# This test verifies that rolling upgrade works. We begin with a cluster with an old version, and then
-# we shutdown member with old version, and restart it with a new version.
+# Verifies that rolling upgrade for members works.
+# This is supported since Hazelcast EE 3.8 (for minor versions only).
+
+# We begin with a cluster with old version.
+# Then we shutdown each member with an old version and restart it with a new version.
+#
 
 set -e
 
-# The number of members we are going to start with.
+# the number of members we are going to start with
 members=10
 clients=2
-old_version=maven=3.7
-new_version=maven=3.8
+old_version=maven=3.8
+new_version=maven=3.9-SNAPSHOT
 
-# start the coordinator in remote mode
-coordinator --monitorPerformance &
+# start the Coordinator in remote mode
+coordinator &
 
 coordinator-remote install ${old_version}
 coordinator-remote install ${new_version}
 
-echo starting members
+echo "Starting members..."
 coordinator-remote worker-start --count ${members} --versionSpec ${old_version}
 
-echo starting client drivers
+echo "Starting client drivers..."
 coordinator-remote worker-start --count ${clients} --workerType javaclient --versionSpec ${old_version}
 
-echo starting the test
-test_id=$(coordinator-remote test-start --duration 0s atomiclong.properties)
-echo test_id $test_id
+echo "Starting the test..."
+test_id=$(coordinator-remote test-start --duration 0s rolling.properties)
+echo "test_id: $test_id"
 
-echo waiting for test to run
+echo "Waiting for the test to run..."
 sleep 30s
 
 for i in {1..$members}
 do
-   echo iteration $i
+   echo iteration ${i}
 
-   echo killing member
+   echo "Killing member with old version ($old_version)"
    coordinator-remote worker-kill --versionSpec ${old_version}
 
-   echo starting new member
+   echo "Starting member with new version ($new_version)"
    coordinator-remote worker-start --versionSpec ${new_version}
 
    sleep 10s
 
-   status=$(coordinator-remote test-status $test_id)
-   echo test_status $status
-   if [ $status == "failure" ]; then
-       echo Error detected!!!!!!!
+   status=$(coordinator-remote test-status ${test_id})
+   echo "test_status: $status"
+   if [ ${status} == "failure" ]; then
+       echo "Error detected!!!"
        break
    fi
 done
 
-coordinator-remote test-stop $test_id
+coordinator-remote test-stop ${test_id}
 coordinator-remote stop
