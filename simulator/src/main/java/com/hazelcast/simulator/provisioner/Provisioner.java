6209281633a2564f6fb59c23028cd94e6260300e
Donnerbart <github@thunderphreak.de>
2017-03-01 15:57:09 +0100

    Added distinct method Bash::sshTTY to Bash enforce TTY

--- simulator/src/main/java/com/hazelcast/simulator/provisioner/Provisioner.java
+++ simulator/src/main/java/com/hazelcast/simulator/provisioner/Provisioner.java
@@ -377,7 +377,7 @@ class Provisioner {
         File initFile = newFile("init-" + newUnsecureUuidString() + ".sh");
         writeText(loadInitScript(), initFile);
         bash.scpToRemote(ip, initFile, "init.sh");
-        bash.ssh(ip, "bash init.sh");
+        bash.sshTTY(ip, "bash init.sh");
         deleteQuiet(initFile);
     }
 
