1ae766173f058469258690e3a34bff1780392403
pveentjer <alarmnummer@gmail.com>
2017-03-08 16:02:39 +0200

    Removed unused metod in ExecutorFactory

--- simulator/src/main/java/com/hazelcast/simulator/utils/ExecutorFactory.java
+++ simulator/src/main/java/com/hazelcast/simulator/utils/ExecutorFactory.java
@@ -35,10 +35,6 @@ public final class ExecutorFactory {
         return Executors.newFixedThreadPool(poolSize, createThreadFactory(namePrefix));
     }
 
-    public static ExecutorService createCachedThreadPool(String namePrefix) {
-        return Executors.newCachedThreadPool(createThreadFactory(namePrefix));
-    }
-
     public static ScheduledExecutorService createScheduledThreadPool(int poolSize, String namePrefix) {
         return Executors.newScheduledThreadPool(poolSize, createThreadFactory(namePrefix));
     }
