f14ef659815111d3525c9e2e00de238f70996d61
pveentjer <alarmnummer@gmail.com>
2017-04-18 19:28:46 +0300

    Added unit tests for unused probability

---
+++ simulator/src/test/java/com/hazelcast/simulator/worker/testcontainer/TestContainer_TimeStep_UnusedProbabilityTest.java
@@ -0,0 +1,39 @@
+package com.hazelcast.simulator.worker.testcontainer;
+
+import com.hazelcast.simulator.common.TestCase;
+import com.hazelcast.simulator.protocol.Server;
+import com.hazelcast.simulator.test.annotations.TimeStep;
+import com.hazelcast.simulator.utils.BindException;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import static com.hazelcast.simulator.common.TestPhase.SETUP;
+import static org.mockito.Mockito.mock;
+
+public class TestContainer_TimeStep_UnusedProbabilityTest extends TestContainer_AbstractTest {
+
+    @Test(expected = BindException.class)
+    public void test() throws Exception {
+        UnusedProbabilityTest testInstance = new UnusedProbabilityTest();
+        TestCase testCase = new TestCase("stopRun")
+                .setProperty("threadCount", 1)
+                .setProperty("iterations", 100)
+                .setProperty("unusedProb", "0.2")
+                .setProperty("class", testInstance.getClass());
+
+        TestContextImpl testContext = new TestContextImpl(
+                testCase.getId(), "localhost", mock(Server.class));
+        TestContainer container = new TestContainer(testContext, testInstance, testCase);
+        container.invoke(SETUP);
+    }
+
+    public static class UnusedProbabilityTest {
+        private final AtomicLong runCount = new AtomicLong(0);
+
+        @TimeStep
+        public void timeStep() {
+            runCount.incrementAndGet();
+        }
+    }
+}
