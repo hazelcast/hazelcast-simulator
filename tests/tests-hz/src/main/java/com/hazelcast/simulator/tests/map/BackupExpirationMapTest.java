c23d27458deb1bb70f9cfacc7725ead0a707e93e
Ahmet Mırçık <ahmetmircik@gmail.com>
2017-06-06 15:22:46 +0300

    Fixed BackupExpirationMapTest

--- tests/tests-hz/src/main/java/com/hazelcast/simulator/tests/map/BackupExpirationMapTest.java
+++ tests/tests-hz/src/main/java/com/hazelcast/simulator/tests/map/BackupExpirationMapTest.java
@@ -16,19 +16,71 @@
 package com.hazelcast.simulator.tests.map;
 
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import com.hazelcast.instance.Node;
 import com.hazelcast.map.impl.MapService;
 import com.hazelcast.map.impl.MapServiceContext;
 import com.hazelcast.map.impl.recordstore.RecordStore;
+import com.hazelcast.simulator.hz.HazelcastTest;
+import com.hazelcast.simulator.test.BaseThreadState;
+import com.hazelcast.simulator.test.annotations.Prepare;
+import com.hazelcast.simulator.test.annotations.Setup;
+import com.hazelcast.simulator.test.annotations.TimeStep;
 import com.hazelcast.simulator.test.annotations.Verify;
+import com.hazelcast.simulator.tests.helpers.KeyLocality;
 import com.hazelcast.simulator.utils.AssertTask;
+import com.hazelcast.simulator.worker.loadsupport.Streamer;
+import com.hazelcast.simulator.worker.loadsupport.StreamerFactory;
 import com.hazelcast.spi.impl.NodeEngineImpl;
 
+import java.util.Random;
+
 import static com.hazelcast.simulator.tests.helpers.HazelcastTestUtils.getNode;
+import static com.hazelcast.simulator.tests.helpers.HazelcastTestUtils.waitClusterSize;
+import static com.hazelcast.simulator.tests.helpers.KeyUtils.generateIntKeys;
 import static com.hazelcast.simulator.utils.TestUtils.assertTrueEventually;
 import static org.junit.Assert.assertEquals;
 
-public class BackupExpirationMapTest extends IntIntMapTest {
+public class BackupExpirationMapTest extends HazelcastTest {
+
+    // properties
+    public int minNumberOfMembers = 0;
+    public int keyCount = 10000;
+    public KeyLocality keyLocality = KeyLocality.SHARED;
+
+    private IMap<Integer, Integer> map;
+    private int[] keys;
+
+    @Setup
+    public void setUp() {
+        map = targetInstance.getMap(name);
+    }
+
+    @Prepare(global = false)
+    public void prepare() {
+        waitClusterSize(logger, targetInstance, minNumberOfMembers);
+        keys = generateIntKeys(keyCount, keyLocality, targetInstance);
+        Streamer<Integer, Integer> streamer = StreamerFactory.getInstance(map);
+        Random random = new Random();
+        for (int key : keys) {
+            int value = random.nextInt(Integer.MAX_VALUE);
+            streamer.pushEntry(key, value);
+        }
+        streamer.await();
+    }
+
+    @TimeStep(prob = 0.1)
+    public Integer put(ThreadState state) {
+        int key = state.randomKey();
+        int value = state.randomValue();
+        return map.put(key, value);
+    }
+
+    @TimeStep(prob = -1)
+    public Integer get(ThreadState state) {
+        int key = state.randomKey();
+        return map.get(key);
+    }
 
     @Verify(global = false)
     public void verify() {
@@ -59,5 +111,16 @@ public class BackupExpirationMapTest extends IntIntMapTest {
             }
         }
         return size;
+    }
+
+    public class ThreadState extends BaseThreadState {
+
+        private int randomKey() {
+            return keys[randomInt(keys.length)];
+        }
+
+        private int randomValue() {
+            return randomInt(Integer.MAX_VALUE);
+        }
     }
 }
