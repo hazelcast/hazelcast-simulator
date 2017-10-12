4a2a81ed8d5eb9f947fcb64f0a85b575a6497293
Donnerbart <github@thunderphreak.de>
2017-10-12 12:56:20 +0200

    Fixed userDir deletion in CoordinatorCliTest

--- simulator/src/test/java/com/hazelcast/simulator/coordinator/CoordinatorCliTest.java
+++ simulator/src/test/java/com/hazelcast/simulator/coordinator/CoordinatorCliTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2008-2016, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hazelcast.simulator.coordinator;
 
 import com.hazelcast.simulator.agent.workerprocess.WorkerProcessSettings;
@@ -52,7 +67,7 @@ public class CoordinatorCliTest {
     private String sessionId;
 
     @BeforeClass
-    public static void beforeClass() throws Exception {
+    public static void beforeClass() {
         setupFakeEnvironment();
 
         createAgentsFileWithLocalhost();
@@ -64,13 +79,6 @@ public class CoordinatorCliTest {
         propertiesFile = ensureExistingFile("simulator.properties");
     }
 
-    @Before
-    public void before() {
-        sessionId = "CoordinatorCliTest-" + currentTimeMillis();
-        args.add("--sessionId");
-        args.add(sessionId);
-    }
-
     @AfterClass
     public static void afterClass() {
         tearDownFakeEnvironment();
@@ -78,9 +86,16 @@ public class CoordinatorCliTest {
         deleteQuiet(propertiesFile);
     }
 
+    @Before
+    public void before() {
+        sessionId = "CoordinatorCliTest-" + currentTimeMillis();
+        args.add("--sessionId");
+        args.add(sessionId);
+    }
+
     @After
     public void after() {
-        deleteQuiet(new File(sessionId).getAbsoluteFile());
+        deleteQuiet(new File(getUserDir(), sessionId));
     }
 
     @Test
