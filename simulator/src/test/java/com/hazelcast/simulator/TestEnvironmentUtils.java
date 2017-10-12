af1c0817df998956a6dda862d540c1aa25afe6df
Donnerbart <github@thunderphreak.de>
2017-10-12 12:57:18 +0200

    Fixed deletion of user environment directory in TestEnvironmentUtils

--- simulator/src/test/java/com/hazelcast/simulator/TestEnvironmentUtils.java
+++ simulator/src/test/java/com/hazelcast/simulator/TestEnvironmentUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2008-2016, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hazelcast.simulator;
 
 import com.hazelcast.simulator.common.AgentsFile;
@@ -17,6 +32,7 @@ import static com.hazelcast.simulator.utils.FileUtils.ensureExistingFile;
 import static com.hazelcast.simulator.utils.FileUtils.getUserDir;
 import static com.hazelcast.simulator.utils.FileUtils.newFile;
 
+@SuppressWarnings("unused")
 public class TestEnvironmentUtils {
 
     private static final Logger LOGGER = Logger.getLogger(TestEnvironmentUtils.class);
@@ -71,7 +87,10 @@ public class TestEnvironmentUtils {
             System.setProperty("SIMULATOR_HOME", originalSimulatorHome);
         }
 
-        System.clearProperty("user.dir.test");
+        String userDir = System.clearProperty("user.dir.test");
+        if (userDir != null) {
+            deleteQuiet(userDir);
+        }
     }
 
     public static void setLogLevel(Level level) {
