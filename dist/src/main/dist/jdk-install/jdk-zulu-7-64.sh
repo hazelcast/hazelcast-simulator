451d38d9d456e0a244adf9928a67341d06f74374
pveentjer <alarmnummer@gmail.com>
2018-03-23 06:25:53 +0200

    Upgraded zulu 7 to latest release fix #1676

--- dist/src/main/dist/jdk-install/jdk-zulu-7-64.sh
+++ dist/src/main/dist/jdk-install/jdk-zulu-7-64.sh
@@ -3,4 +3,4 @@
 set -e
 source jdk-support.sh
 
-installZuluJdk 7 "http://cdn.azul.com/zulu/bin/zulu7.17.0.5-jdk7.0.131-linux_x64.tar.gz" "~/zulu7.17.0.5-jdk7.0.131-linux_x64"
+installZuluJdk 7 "http://cdn.azul.com/zulu/bin/zulu7.22.0.3-jdk7.0.171-linux_x64.tar.gz" "~/zulu7.22.0.3-jdk7.0.171-linux_x64"
