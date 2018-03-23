4b1a158908cedae6fe767c7b5cf8cc7cd35efded
pveentjer <alarmnummer@gmail.com>
2018-03-23 06:28:30 +0200

    Upgraded zulu 8 support to latest
    fix #1657

--- dist/src/main/dist/jdk-install/jdk-zulu-8-64.sh
+++ dist/src/main/dist/jdk-install/jdk-zulu-8-64.sh
@@ -3,4 +3,4 @@
 set -e
 source jdk-support.sh
 
-installZuluJdk 8 "http://cdn.azul.com/zulu/bin/zulu8.20.0.5-jdk8.0.121-linux_x64.tar.gz" "~/zulu8.20.0.5-jdk8.0.121-linux_x64"
+installZuluJdk 8 "http://cdn.azul.com/zulu/bin/zulu8.28.0.1-jdk8.0.163-linux_x64.tar.gz" "~/zulu8.28.0.1-jdk8.0.163-linux_x64"
