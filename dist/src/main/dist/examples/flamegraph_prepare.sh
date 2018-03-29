c4296e2c60ffafb7fb7f25baafe598535d83f2f3
pveentjer <alarmnummer@gmail.com>
2018-03-29 08:25:47 +0300

    Added an externalizable test
    Also added example script for flamegraph prepare
    of ec2 (yum based) nodes.

---
+++ dist/src/main/dist/examples/flamegraph_prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+agent-ssh "sudo yum -y install perf"
+agent-ssh "sudo yum -y install git"
+agent-ssh "sudo yum -y install cmake"
+agent-ssh "sudo yum -y install make"
+agent-ssh "sudo yum -y install gcc-c++"
+
+
