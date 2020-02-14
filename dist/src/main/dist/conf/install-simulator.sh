ee25818da37d1784db9a19481c0c01ee1a9e3df4
Peter Veentjer <alarmnummer@gmail.com>
2020-02-14 16:57:33 +0200

    Fixes dependency problems

--- dist/src/main/dist/conf/install-simulator.sh
+++ dist/src/main/dist/conf/install-simulator.sh
@@ -11,6 +11,12 @@ uploadLibraryJar(){
     rsync --checksum -avv -L -e "ssh $SSH_OPTIONS" $src $SIMULATOR_USER@$agent:hazelcast-simulator-$SIMULATOR_VERSION/lib
 }
 
+uploadTestLibJar(){
+    pattern=$1
+    src="$SIMULATOR_HOME/test-lib/tests-$VENDOR/$pattern"
+    rsync --checksum -avv -L -e "ssh $SSH_OPTIONS" $src $SIMULATOR_USER@$agent:hazelcast-simulator-$SIMULATOR_VERSION/test-lib/tests-$VENDOR/
+}
+
 uploadToRemoteSimulatorDir(){
     src=$1
     target=$2
@@ -49,78 +55,18 @@ uploadLibraryJar "slf4j-log4j12-*"
 
 # hack to get hz enterprise working
 if [ "$VENDOR" = "hazelcast-enterprise" ]; then
-    uploadLibraryJar "netty-buffer-*"
-    uploadLibraryJar "netty-codec-*"
-    uploadLibraryJar "netty-common-*"
-    uploadLibraryJar "netty-handler-*"
-    uploadLibraryJar "netty-resolver-*"
-    uploadLibraryJar "netty-tcnative-boringssl-static-*"
+    ssh $SSH_OPTIONS $SIMULATOR_USER@$agent "mkdir -p hazelcast-simulator-$SIMULATOR_VERSION/test-lib/tests-hazelcast"
+    rsync --checksum -avv -L -e "ssh $SSH_OPTIONS" $SIMULATOR_HOME/test-lib/tests-hazelcast/* \
+            $SIMULATOR_USER@$agent:hazelcast-simulator-$SIMULATOR_VERSION/test-lib/tests-hazelcast/
+else
+    ssh $SSH_OPTIONS $SIMULATOR_USER@$agent "mkdir -p hazelcast-simulator-$SIMULATOR_VERSION/test-lib/tests-$VENDOR"
+    uploadTestLibJar "*"
 fi
 
-# hack to get ignite working
-if [ "$VENDOR" = "ignite" ]; then
-    uploadLibraryJar "ignite-*"
-    uploadLibraryJar "spring-*"
-    uploadLibraryJar "commons-logging-*"
-    uploadLibraryJar "h2-*"
-fi
-
-# hack to get infinispan working
-if [ "$VENDOR" = "infinispan" ]; then
-    uploadLibraryJar "infinispan-*"
-    uploadLibraryJar "jboss*"
-    uploadLibraryJar "jgroups*"
-    uploadLibraryJar "netty*"
-    uploadLibraryJar "jackson*"
-    uploadLibraryJar "commons-pool*"
-    uploadLibraryJar "javassist*"
-    uploadLibraryJar "xstream*"
-    uploadLibraryJar "xmlpull*"
-    uploadLibraryJar "caffeine*"
-    uploadLibraryJar "reactive-streams*"
-    # specific version needed due to different versions
-    uploadLibraryJar "rxjava-2.2.4*"
-fi
-
-# hack to get couchbase working
-if [ "$VENDOR" = "couchbase" ]; then
-    uploadLibraryJar "java-client-*"
-    uploadLibraryJar "core-io-*"
-    uploadLibraryJar "rxjava-*"
-fi
-
-#hack to get hz enterprise working
-if [ "$VENDOR" = "jet" ]; then
-    uploadLibraryJar "hazelcast-jet-*"
-fi
-
-# hack to get memcached working
-if [ "$VENDOR" = "memcached" ]; then
-    uploadLibraryJar "spymemcached*"
-fi
-
-# hack to get mongodb working
-if [ "$VENDOR" = "mongodb" ]; then
-    uploadLibraryJar "mongo-*"
-fi
-
-# hack to get redis working
-if [ "$VENDOR" = "jedis" ]; then
-    uploadLibraryJar "jedis*"
-    uploadLibraryJar "commons-pool2*"
-fi
-
-# hack to get lettuce working
-if [ "$VENDOR" = "lettuce" ]; then
-    uploadLibraryJar "lettuce-core-*"
-    uploadLibraryJar "netty-*"
-    uploadLibraryJar "reactor-*"
-    uploadLibraryJar "reactive-streams-*"
-fi
+#
 
 # upload remaining files
 uploadToRemoteSimulatorDir "$SIMULATOR_HOME/bin/" "bin"
 uploadToRemoteSimulatorDir "$SIMULATOR_HOME/conf/" "conf"
 uploadToRemoteSimulatorDir "$SIMULATOR_HOME/jdk-install/" "jdk-install"
-uploadToRemoteSimulatorDir "$SIMULATOR_HOME/test-lib/" "test-lib/"
 uploadToRemoteSimulatorDir "$SIMULATOR_HOME/user-lib/" "user-lib/"
