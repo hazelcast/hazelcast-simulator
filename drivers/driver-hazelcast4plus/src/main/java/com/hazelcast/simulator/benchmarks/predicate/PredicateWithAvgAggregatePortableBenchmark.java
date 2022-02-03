fd6b04c43f4b7c509db642309b3e2c4a403e61e0
Krzysztof Ślusarski <krzysztof.slusarski@hazelcast.com>
2022-02-03 08:40:49 +0100

    SQL Benchmarks Portable Sum

---
+++ drivers/driver-hazelcast4plus/src/main/java/com/hazelcast/simulator/benchmarks/predicate/PredicateWithAvgAggregatePortableBenchmark.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2008-2016, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hazelcast.simulator.benchmarks.predicate;
+
+import com.hazelcast.aggregation.Aggregators;
+import com.hazelcast.map.IMap;
+import com.hazelcast.simulator.hz.HazelcastTest;
+import com.hazelcast.simulator.hz.IdentifiedDataWithLongPortablePojo;
+import com.hazelcast.simulator.test.annotations.Prepare;
+import com.hazelcast.simulator.test.annotations.Setup;
+import com.hazelcast.simulator.test.annotations.Teardown;
+import com.hazelcast.simulator.test.annotations.TimeStep;
+import com.hazelcast.simulator.worker.loadsupport.Streamer;
+import com.hazelcast.simulator.worker.loadsupport.StreamerFactory;
+
+
+public class PredicateWithAvgAggregatePortableBenchmark extends HazelcastTest {
+
+    // properties
+    // the number of map entries
+    public int entryCount = 10_000_000;
+
+    private long sum = 0;
+
+    //16 byte + N*(20*N
+    private IMap<Integer, IdentifiedDataWithLongPortablePojo> map;
+
+    @Setup
+    public void setup() {
+        this.map = targetInstance.getMap(name);
+    }
+
+    @Prepare(global = true)
+    public void prepare() {
+        Streamer<Integer, IdentifiedDataWithLongPortablePojo> streamer = StreamerFactory.getInstance(map);
+        Integer[] sampleArray = new Integer[20];
+        for (int i = 0; i < 20; i++) {
+            sampleArray[i] = i;
+        }
+
+        for (int i = 0; i < entryCount; i++) {
+            Integer key = i;
+            IdentifiedDataWithLongPortablePojo value = new IdentifiedDataWithLongPortablePojo(sampleArray, key.longValue());
+            sum += i;
+            streamer.pushEntry(key, value);
+        }
+        streamer.await();
+    }
+
+    @TimeStep
+    public void timeStep() throws Exception {
+        map.aggregate(Aggregators.longAvg("value")).longValue();
+    }
+
+    @Teardown
+    public void teardown() {
+        map.destroy();
+    }
+}
+
