4af3a74b9c171708a17fc9f09bb58f2aa37fa426
Peter Veentjer <alarmnummer@gmail.com>
2022-08-03 11:16:51 +0300

    WIP

--- src/simulator/repo.py
+++ src/simulator/repo.py
@@ -1,33 +1,39 @@
 import pandas as pd
 pd.options.mode.chained_assignment = None
 from matplotlib import pyplot as plt
-
+import tempfile
 
 
 def multiple_by_thousand(df, *column_names):
     for column_name in column_names:
         column = df[column_name]
         values = len(column.values)
-        print(f'fixing column {column_name}')
         for i in range(values):
             column.iloc[i] = 1000 * column.iloc[i]
     print("done")
 
-filename = '/home/eng/Hazelcast/simulator-tng/storage/runs/insert/1M/map_tiered/03-08-2022_09-04-10/A1_dstat.csv'
+filename = '/home/eng/Hazelcast/simulator-tng/storage/runs/insert/10M/map_tiered/03-08-2022_10-52-46/A1_dstat.csv'
 df = pd.read_csv(filename, skiprows=5)
 
-#for col in df.columns:
-#    print(col)
+for col in df.columns:
+    print(col)
 
 multiple_by_thousand(df, 'used', 'free', 'buf', 'cach')
 
 print(df.to_string())
 
-plt.figure()
+dirpath = tempfile.mkdtemp()
+print(f"directory {dirpath}")
+for c in range(2, len(df.columns)):
+    column_name = df.columns[c]
+    plt.figure()
+    plt.rcParams["figure.figsize"] = [10, 5]
 
-plt.rcParams["figure.figsize"] = [10, 5]
-plt.plot(df['epoch'], df['used'])
-plt.plot(df['epoch'], df['free'])
-plt.plot(df['epoch'], df['buf'])
-plt.plot(df['epoch'], df['cach'])
-plt.show()
+    df['epoch'] = pd.to_datetime(df['epoch'], unit='s')
+
+    plt.plot(df['epoch'], df[column_name])
+    filename = column_name.replace("/","_")
+
+    plt.savefig(f'{dirpath}/{filename}.png')
+    plt.close()
+
