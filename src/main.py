59a45d7bcbbd7cedc371be39222e800064f7b4eb
Serkan Özel <serkan.ozel@hazelcast.com>
2023-02-06 21:56:18 +0300

    install.txt changed and main.py fixed

--- src/main.py
+++ src/main.py
@@ -7,4 +7,5 @@ result = find_ee_commit("/Users/serkan/forked/hazelcast", "/Users/serkan/forked/
 assert result == ee_commit
 
 result = find_os_commit("/Users/serkan/forked/hazelcast", "/Users/serkan/forked/hazelcast-enterprise", ee_commit)
-assert result == os_commit
+# This is not equal to os_commit because more than one oss commits can match to a single ee commit
+assert result == "f55912b0819c576cb6cd5ed2e5f57e402ac207a4"
