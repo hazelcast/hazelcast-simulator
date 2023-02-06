fdb59c6f32aadeb384aa012b375be5ad36f1a428
Serkan Özel <serkan.ozel@hazelcast.com>
2023-02-06 22:11:58 +0300

    Update comment

--- src/main.py
+++ src/main.py
@@ -7,5 +7,5 @@ result = find_ee_commit("/Users/serkan/forked/hazelcast", "/Users/serkan/forked/
 assert result == ee_commit
 
 result = find_os_commit("/Users/serkan/forked/hazelcast", "/Users/serkan/forked/hazelcast-enterprise", ee_commit)
-# This is not equal to os_commit because more than one oss commits can match to a single ee commit
+# This is not equal to os_commit because more than one oss commits can match to a single ee commit (the same is also true for ee commits)
 assert result == "f55912b0819c576cb6cd5ed2e5f57e402ac207a4"
