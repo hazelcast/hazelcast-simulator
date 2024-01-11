47d54e8a13b4242cee8bc60a84f84141f9be2034
Granville Barnett <140408555+gbarnett-hz@users.noreply.github.com>
2024-01-11 13:06:16 +0000

    CPMap Tests [HZ-4143] (#2184)
    * CPMap Tests
    
    - required bumpt to 5.4.0-SNAPSHOT for CPMap
    - create-then-delete for IAtomicReferenceTest
    
    * return results of non-void methods
    
    * permit multiple keys to be defined; uuid as key
    
    * multiple cp-groups, maps, etc
    
    * naming
    
    * int keys in range [0,keys)
    
    * remove formatting on IAtomicRefTest
    
    * newline
    
    * use default cp group when cpGroups==0

---
+++ java/drivers/driver-hazelcast4plus/src/main/java/com/hazelcast/simulator/tests/cp/CPMapTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hazelcast.simulator.tests.cp;
+
+import com.hazelcast.cp.CPMap;
+import com.hazelcast.simulator.hz.HazelcastTest;
+import com.hazelcast.simulator.test.BaseThreadState;
+import com.hazelcast.simulator.test.annotations.Prepare;
+import com.hazelcast.simulator.test.annotations.Setup;
+import com.hazelcast.simulator.test.annotations.TimeStep;
+import com.hazelcast.simulator.utils.GeneratorUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CPMapTest extends HazelcastTest {
+    // number of cp groups to host the created maps; if (distinctMaps % maps) == 0 then there's a uniform distribution of maps
+    // over cp groups, otherwise maps are allocated per-cp group in a RR-fashion. If cpGroups == 0 then all CPMap instances will
+    // be hosted by the default CPGroup. When cpGroups > 0, we create and host CPMaps across non-default CP Groups.
+    public int cpGroups = 0;
+    // number of distinct maps to create and use during the tests
+    public int maps = 1;
+    // number of distinct keys to create and use per-map; key domain is [0, keys)
+    public int keys = 1;
+    // size in bytes for each key's associated value
+    public int valueSizeBytes = 100;
+    private List<CPMap<Integer, String>> mapReferences;
+
+    private String v; // this is always the value associated with any key; exception is remove and delete
+
+
+    @Setup
+    public void setup() {
+        v = GeneratorUtils.generateAsciiString(valueSizeBytes);
+
+        // (1) create the cp group names that will host the maps
+        String[] cpGroupNames = createCpGroupNames();
+        // (2) create the map names + associated proxies (maps aren't created until you actually interface with them)
+        mapReferences = new ArrayList<>();
+        for (int i = 0; i < maps; i++) {
+            String cpGroup = cpGroupNames[i % cpGroupNames.length];
+            String mapName = "map" + i + "@" + cpGroup;
+            mapReferences.add(targetInstance.getCPSubsystem().getMap(mapName));
+        }
+    }
+
+    private String[] createCpGroupNames() {
+        if (cpGroups == 0) {
+            return new String[]{"default"};
+        }
+
+        String[] cpGroupNames = new String[cpGroups];
+        for (int i = 0; i < cpGroups; i++) {
+            cpGroupNames[i] = "cpgroup-" + i;
+        }
+        return cpGroupNames;
+    }
+
+    @Prepare(global = true)
+    public void prepare() {
+        for (CPMap<Integer, String> mapReference : mapReferences) {
+            for (int key = 0; key < keys; key++) {
+                mapReference.set(key, v);
+            }
+        }
+    }
+
+    @TimeStep(prob = 1)
+    public void set(ThreadState state) {
+        state.randomMap().set(state.randomKey(), v);
+    }
+
+    @TimeStep(prob = 0)
+    public String put(ThreadState state) {
+        return state.randomMap().put(state.randomKey(), v);
+    }
+
+    @TimeStep(prob = 0)
+    public String get(ThreadState state) {
+        return state.randomMap().get(state.randomKey());
+    }
+
+    // 'remove' and 'delete' other than their first invocation pointless -- we're just timing the logic that underpins the
+    // retrieval of no value.
+
+    @TimeStep(prob = 0)
+    public String remove(ThreadState state) {
+        return state.randomMap().remove(state.randomKey());
+    }
+
+    @TimeStep(prob = 0)
+    public void delete(ThreadState state) {
+        state.randomMap().delete(state.randomKey());
+    }
+
+    @TimeStep(prob = 0)
+    public boolean cas(ThreadState state) {
+        // 'v' is always associated with 'k'
+        return state.randomMap().compareAndSet(state.randomKey(), v, v);
+    }
+
+    @TimeStep(prob = 0)
+    public void setThenDelete(ThreadState state) {
+        CPMap<Integer, String> map = state.randomMap();
+        int key = state.randomKey();
+        map.set(key, v);
+        map.delete(key);
+    }
+
+    public class ThreadState extends BaseThreadState {
+        public int randomKey() {
+            return randomInt(keys); // [0, keys)
+        }
+
+        public CPMap<Integer, String> randomMap() {
+            return mapReferences.get(randomInt(maps));
+        }
+    }
+}
