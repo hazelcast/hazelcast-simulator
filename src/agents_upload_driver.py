7f27fd180d5325504d4fc120beb42a70908d68f8
Peter Veentjer <alarmnummer@gmail.com>
2024-02-27 14:28:37 +0200

    Driver was not uploaded

--- src/agents_upload_driver.py
+++ src/agents_upload_driver.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
-import yaml
-import sys
+
 from simulator.util import shell, run_parallel, simulator_home
-from simulator.hosts import public_ip, ssh_user, ssh_options
 
 
-def __upload_driver(agent):
-    print(f"[INFO]     {public_ip(agent)}  Uploading")
+def __upload_driver(host, agent_yaml, driver_dir):
+    ssh_user = agent_yaml.get('ansible_user')
+    ssh_key = agent_yaml.get('ansible_ssh_private_key_file')
+    ssh_options = f"-i {ssh_key} -o StrictHostKeyChecking=no -o ConnectTimeout=60";
+
+    print(f"[INFO]     {host}  Uploading")
     shell(
-        f"""rsync --checksum -avv -L -e "ssh {ssh_options(agent)}" {simulator_home}/{driver_dir}/* {ssh_user(agent)}@{public_ip(agent)}:hazelcast-simulator/{driver_dir}/""")
-    print(f"[INFO]     {public_ip(agent)}  Uploading: done")
+        f"""rsync --checksum -avv -L -e "ssh {ssh_options}" {simulator_home}/{driver_dir}/* {ssh_user}@{host}:hazelcast-simulator/{driver_dir}/""")
+    print(f"[INFO]     {host}  Uploading: done")
 
 
-agents_yaml = yaml.safe_load(sys.argv[1])
-driver = sys.argv[2]
+def upload_driver(driver, agents_yaml):
+    driver_dir = f"drivers/driver-{driver}"
 
-driver_dir = f"drivers/driver-{driver}"
-
-print(f"[INFO]Uploading driver {driver} to {driver_dir}: starting")
-run_parallel(__upload_driver, [(agent,) for agent in agents_yaml])
-print(f"[INFO]Uploading driver {driver} to {driver_dir}: done")
+    print(f"[INFO]Uploading driver {driver} to {driver_dir}: starting")
+    run_parallel(__upload_driver, [(host, agent_yaml, driver_dir,) for host, agent_yaml in agents_yaml.items()])
+    print(f"[INFO]Uploading driver {driver} to {driver_dir}: done")
