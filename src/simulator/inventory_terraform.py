40f83ccaad7907552513b860cff9c60d33c81181
Gareth Johnston <92283680+gareth-johnston@users.noreply.github.com>
2024-03-14 14:35:43 +0000

    New redis drivers support and utils (#2207)
    - Adds package manager/linux distribution util
    - Collect output from ssh commands
    - Adds load_balancer hosts/ count
    - Inventory terraform can parse out arn/private ip from load balancers
    - Ignore load balancers when installing java/simulator
    - Add post_run step to driver

--- src/simulator/inventory_terraform.py
+++ src/simulator/inventory_terraform.py
@@ -2,6 +2,8 @@ import json
 import os
 from os import path
 import subprocess
+import boto3
+import re
 
 from simulator.util import shell, exit_with_error, read_file, write_yaml
 from simulator.log import info
@@ -47,23 +49,32 @@ def terraform_import(terraform_plan):
     inventory = {}
 
     for group_name, block in json.loads(output).items():
-        json_host_list = block['value'][0]
-        host_map = {}
-        for json_host in json_host_list:
-            host_name = json_host['public_ip']
-            host_data = {'private_ip': json_host['private_ip']}
-            tags = json_host.get("tags")
+        if 'value' in block and block['value']:  # Check if 'value' key exists and is not empty
+            json_host_list = block['value'][0]
+            host_map = {}
+            for json_host in json_host_list:
+                if group_name == 'load_balancers':
+                    host_name = json_host['dns_name']
+                    host_data = {'arn': json_host['arn']}
+                    private_ip = __get_nlb_private_ip(json_host)
+                    host_data['private_ip'] = private_ip
 
-            if tags:
-                for key, value in tags.items():
-                    if key.startswith("passthrough:"):
-                        host_data[key[len("passthrough:"):]] = value
+                else:
+                    host_name = json_host['public_ip']
+                    host_data = {'private_ip': json_host['private_ip']}
 
-            host_map[host_name] = host_data
+                tags = json_host.get("tags")
 
-        if host_map:
-            hosts = {'hosts': host_map}
-            inventory[group_name] = hosts
+                if tags:
+                    for key, value in tags.items():
+                        if key.startswith("passthrough:"):
+                            host_data[key[len("passthrough:"):]] = value
+
+                host_map[host_name] = host_data
+
+            if host_map:
+                hosts = {'hosts': host_map}
+                inventory[group_name] = hosts
 
     info("Creating [inventory.yaml]")
     write_yaml("inventory.yaml", inventory)
@@ -102,3 +113,82 @@ def __destroy(terraform_plan, force):
 def __ensure_plan_exists(terraform_plan):
     if not path.isdir(terraform_plan):
         exit_with_error(f"Directory [{terraform_plan}] does not exist.")
+
+
+def extract_aws_region_from_arn(arn):
+    # ARN format: arn:partition:service:region:account-id:resource
+    match = re.match(r'arn:[^:]+:[^:]+:([^:]+):', arn)
+    if match:
+        return match.group(1)
+    return "default_region"
+
+
+def extract_aws_region_from_arn(arn):
+    # ARN format: arn:aws:elasticloadbalancing:<region>:<account-id>:loadbalancer/<lb-name>/<lb-id>
+    match = re.match(r'arn:aws:elasticloadbalancing:([^:]+):', arn)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+
+def extract_lb_name_from_arn(arn):
+    try:
+        parts = arn.split('/')
+        index = parts.index('net')
+        return parts[index + 1] if index + 1 < len(parts) else None
+    except Exception as e:
+        print(f"Error extracting NLB name from ARN: {e}")
+        return None
+
+
+def __get_nlb_private_ip(data):
+    aws_region = extract_aws_region_from_arn(data['arn'])
+    aws_nlb_name = extract_lb_name_from_arn(data['arn'])
+
+    if aws_region is None or aws_nlb_name is None:
+        print("Error: Unable to extract AWS region or NLB name from ARN.")
+        return None
+
+    ec2 = boto3.client('ec2', region_name=aws_region)
+    response = ec2.describe_network_interfaces(
+        Filters=[
+            {
+                'Name': 'description',
+                'Values': [
+                    f"ELB net/{aws_nlb_name}/*"
+                ]
+            },
+            {
+                'Name': 'vpc-id',
+                'Values': [
+                    data['vpc_id']
+                ]
+            },
+            {
+                'Name': 'status',
+                'Values': [
+                    "in-use"
+                ]
+            },
+            {
+                'Name': 'attachment.status',
+                'Values': [
+                    "attached"
+                ]
+            }
+        ]
+    )
+    interfaces = response['NetworkInterfaces']
+
+    associations = [
+        association for interface in interfaces
+        for association in interface['PrivateIpAddresses']
+    ]
+
+    if associations:
+        return associations[0]['PrivateIpAddress']
+    else:
+        print("Error: No private IP found.")
+        return None
+
--- src/simulator/util.py
+++ src/simulator/util.py
@@ -220,3 +220,25 @@ def find_config_file(filename):
 
     raise Exception(f"Could not find a configuration file with name '{filename}'")
 
+
+def determine_package_manager(ssh):
+    distro_info = get_linux_distribution(ssh)
+    if distro_info is "debian":
+        return "apt"
+    elif distro_info is "rpm":
+        return "yum"
+    else:
+        error(f"Unsupported distribution: {distro_info}")
+        raise Exception(f"Unsupported distribution: {distro_info}")
+
+
+def get_linux_distribution(ssh):
+    distro = None
+    exitcode, output = ssh.exec("cat /etc/*release")
+    if exitcode == 0:
+        if "ubuntu" in output.lower() or "debian" in output.lower():
+            distro = 'debian'
+        elif "centos" in output.lower() or "fedora" in output.lower() or "rhel" in output.lower() or "amazon" in output.lower():
+            distro = 'rpm'
+    return distro
+
