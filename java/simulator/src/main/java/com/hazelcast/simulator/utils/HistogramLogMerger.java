f18c9e6a43062e8aa45c622e8f1a74eb9406036c
ihsan demir <ihsan@hazelcast.com>
2025-04-19 00:00:10 +0300

    Update java/simulator/src/main/java/com/hazelcast/simulator/utils/HistogramLogMerger.java
    Co-authored-by: Artem Laktionov <laktionov_artem@ukr.net>

--- java/simulator/src/main/java/com/hazelcast/simulator/utils/HistogramLogMerger.java
+++ java/simulator/src/main/java/com/hazelcast/simulator/utils/HistogramLogMerger.java
@@ -53,7 +53,7 @@ public final class HistogramLogMerger {
 
         File inputFilesListFile = new File(args[1]);
         if (!inputFilesListFile.exists()) {
-            throw new IllegalArgumentException("inputFilesListingFile File [" + inputFilesListFile + "] doesn't exist");
+            throw new IllegalArgumentException("inputFilesListingFile [" + inputFilesListFile + "] doesn't exist");
         }
 
         System.out.println("[HistogramLogMerger] Reading input files list from " + inputFilesListFile);
