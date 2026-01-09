f969f3121194149e1d25fb2b2bd08e778394396b
Thomas Ball <thomas.ball@hazelcast.com>
2026-01-09 14:44:06 +0000

    Configurable waiting for test completion

--- java/drivers/driver-hazelcast4plus/src/main/java/com/hazelcast/simulator/tests/jet/PipelineExecutionTest.java
+++ java/drivers/driver-hazelcast4plus/src/main/java/com/hazelcast/simulator/tests/jet/PipelineExecutionTest.java
@@ -1,6 +1,7 @@
 package com.hazelcast.simulator.tests.jet;
 
 import com.hazelcast.config.UserCodeNamespaceConfig;
+import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.jet.Job;
 import com.hazelcast.jet.config.JobConfig;
 import com.hazelcast.jet.pipeline.Pipeline;
@@ -11,19 +12,21 @@ import com.hazelcast.simulator.utils.CompilationUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 /**
  * Test to allow benchmarking of Jet pipelines using the simulator as a harness.
- * We are not collecting metrics here, they will be profiled from member diagnostics
+ * We are not collecting metrics here, they will be profiled from member diagnostics.
  */
 public class PipelineExecutionTest
         extends HazelcastTest {
 
-    private static final int MILLIS_IN_SECOND = 1000;
     private static final String UPLOAD_DIR = "upload";
     private static final String COMPILATION_OUTPUT_DIR = "compilation-output";
 
@@ -39,12 +42,14 @@ public class PipelineExecutionTest
      */
     public String ucnResourcesName = "SimulatorJobResources";
 
-    /**
-     * Wait after all jobs have completed for this amount of time, can be used to ensure
-     * final metric values output in diagnostics.
-     */
-    public long waitAfterJobCompletionSeconds = 0L;
+    // The pipeline context is created dynamically by first compiling a .java file uploaded to this
+    // loadgenerator which is expected to be a class implementing Supplier<Map<JobConfig, Pipeline>>.
+    // The supplier is then called to get the job definitions which are then submitted to the cluster.
+    private Object pipelineContext;
 
+    private List<Job> submittedJobs;
+
+    @SuppressWarnings("unchecked")
     @Prepare(global = true)
     public void submitJobs()
             throws Exception {
@@ -58,20 +63,26 @@ public class PipelineExecutionTest
         }
 
         File compilationOutputDir = new File(COMPILATION_OUTPUT_DIR);
-        compilationOutputDir.mkdir();
+        if (!compilationOutputDir.mkdir()) {
+            throw new IllegalStateException("Failed to create directory at " + compilationOutputDir);
+        }
 
         Class<?> compilationOutput = CompilationUtils.compile(pipelineSupplierP, getPipelineSupplierClassName(),
                 compilationOutputDir);
         targetInstance.getConfig().getNamespacesConfig()
                       .addNamespaceConfig(new UserCodeNamespaceConfig(ucnResourcesName).addClass(compilationOutput));
 
-        Method getter = compilationOutput.getDeclaredMethod("get");
-        Map<JobConfig, Pipeline> pipelines = (Map<JobConfig, Pipeline>) getter.invoke(
-                compilationOutput.getConstructor().newInstance());
+        pipelineContext = compilationOutput.getConstructor().newInstance();
 
-        for (var entry : pipelines.entrySet()) {
-            entry.getKey().setUserCodeNamespace(ucnResourcesName);
-            targetInstance.getJet().newJob(entry.getValue(), entry.getKey());
+        if (pipelineContext instanceof Supplier<?> pipelineSupplier) {
+            Map<JobConfig, Pipeline> pipelines = (Map<JobConfig, Pipeline>) pipelineSupplier.get();
+            submittedJobs = new ArrayList<>();
+            for (var entry : pipelines.entrySet()) {
+                entry.getKey().setUserCodeNamespace(ucnResourcesName);
+                submittedJobs.add(targetInstance.getJet().newJob(entry.getValue(), entry.getKey()));
+            }
+        } else {
+            throw new RuntimeException("Injected class must implement Supplier<Map<JobConfig, Pipeline>>");
         }
     }
 
@@ -79,18 +90,28 @@ public class PipelineExecutionTest
         return pipelineSupplierFileName.replace(".java", "");
     }
 
+    @SuppressWarnings("unchecked")
     @Run
     public void waitForFinish() {
-        // Simply wait until all jobs to finish
-        logger.info("Joining jobs");
-        targetInstance.getJet().getJobs().forEach(Job::join);
-        if (waitAfterJobCompletionSeconds > 0) {
-            logger.info("Waiting after jobs for {} seconds", waitAfterJobCompletionSeconds);
+        if (pipelineContext instanceof BiConsumer<?, ?> waitForStop) {
+            logger.info("Using injectedContext as wait condition");
+            ((BiConsumer<HazelcastInstance, List<Job>>) waitForStop).accept(targetInstance, submittedJobs);
+        } else {
+            logger.info("Waiting for all jobs to complete");
+            joinAllJobs();
+        }
+
+        // Cancel any jobs still running
+        for (Job job : submittedJobs) {
             try {
-                Thread.sleep(waitAfterJobCompletionSeconds * MILLIS_IN_SECOND);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+                job.cancel();
+            } catch (Exception e) {
+                logger.info("Cancellation of job " + job.getName() + " failed", e);
             }
         }
+    }
+
+    private void joinAllJobs() {
+        submittedJobs.forEach(Job::join);
     }
 }
